cbbb2e0bd3281150443577be7b634811d6df4caf
Linas Vepstas <linas@linas.org>
1998-01-14 06:07:24 +0000

    first draft of register widget
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@428 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/register/register.h
@@ -0,0 +1,34 @@
+
+/*
+ * register.h
+ */
+
+#ifndef __XACC_REGISTER_H__
+#define __XACC_REGISTER_H__
+
+#include "basiccell.h"
+#include "datecell.h"
+#include "pricecell.h"
+#include "table.h"
+#include "recncell.h"
+#include "textcell.h"
+
+typedef struct _BasicRegister {
+   Table       * table;
+   CellBlock   * cursor;
+   CellBlock   * header;
+   BasicCell  * dateCell;
+   BasicCell  * descCell;
+   BasicCell  * memoCell;
+   BasicCell  * recnCell;
+   PriceCell   * creditCell;
+   PriceCell   * debitCell;
+
+} BasicRegister;
+
+BasicRegister * xaccMallocBasicRegister (void);
+void            xaccInitBasicRegister (BasicRegister *);
+
+#endif __XACC_REGISTER_H__
+
+/* ============ END OF FILE ===================== */
