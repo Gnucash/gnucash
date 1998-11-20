dd05ce1fade4d11675d6f5dcc51b17293db5836c
Linas Vepstas <linas@linas.org>
1998-11-20 03:42:06 +0000

    patches from rob browning
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@1397 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/startup/main.scm
+++ src/scm/startup/main.scm
@@ -1,7 +1,30 @@
 
 (define (gnc:startup)
   (gnc:debug "starting up.")
-  #t)
+  (if (not (gnc:handle-command-line-args))
+      (gnc:shutdown 1))
+  
+  ;; Now we can load a bunch of files.
+  
+  (gnc:load "hooks.scm")
+  (gnc:load "doc.scm")
+  
+  ;; Load the system and user configs
+  
+  (if (not (gnc:load-system-config-if-needed))
+      (gnc:shutdown 1))
+  
+  (if (not (gnc:load-user-config-if-needed))
+      (gnc:shutdown 1))
+  
+  (gnc:hook-run-danglers gnc:*startup-hook*)
+  
+  (if (or (gnc:config-var-value-get gnc:*arg-show-usage*)
+          (gnc:config-var-value-get gnc:*arg-show-help*))
+      (begin
+        (gnc:prefs-show-usage)
+        (gnc:shutdown 0)))
+  )
 
 (define (gnc:shutdown exit-status)
   (gnc:debug "Shutdown -- exit-status: " exit-status)
@@ -10,42 +33,20 @@
   (_gnc_shutdown_ exit-status)
   (exit exit-status))
 
-;;;; Now the fun begins.
+(define (gnc:main)
+  
+  ;; Now the fun begins.
 
-(gnc:startup)
-
-(if (not (gnc:handle-command-line-args))
-    (gnc:shutdown 1))
-
-;;; Now we can load a bunch of files.
-
-(gnc:load "hooks.scm")
-(gnc:load "doc.scm")
-
-;;; Load the system and user configs
-
-(if (not (gnc:load-system-config-if-needed))
-    (gnc:shutdown 1))
-
-(if (not (gnc:load-user-config-if-needed))
-    (gnc:shutdown 1))
-
-(gnc:hook-run-danglers gnc:*startup-hook*)
-
-(if (or (gnc:config-var-value-get gnc:*arg-show-usage*)
-         (gnc:config-var-value-get gnc:*arg-show-help*))
-     (begin
-       (gnc:prefs-show-usage)
-       (gnc:shutdown 0)))
-
-(if (not (= (gnucash_lowlev_app_init) 0))
-    (gnc:shutdown 0))
-
-(if (pair? gnc:*command-line-files*)
-     ;; You can only open single files right now...
-     (gnucash_ui_open_file (car gnc:*command-line-files*))
-     (gnucash_ui_select_file))
-
-(gnucash_lowlev_app_main)
-
-(gnc:shutdown 0)
+  (gnc:startup)
+  
+  (if (not (= (gnucash_lowlev_app_init) 0))
+      (gnc:shutdown 0))
+  
+  (if (pair? gnc:*command-line-files*)
+      ;; You can only open single files right now...
+      (gnucash_ui_open_file (car gnc:*command-line-files*))
+      (gnucash_ui_select_file))
+  
+  (gnucash_lowlev_app_main)
+  
+  (gnc:shutdown 0))
