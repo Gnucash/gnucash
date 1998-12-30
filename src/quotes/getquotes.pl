172fb18f2eba675e2f3cd32b33bd9096a29a49bc
Linas Vepstas <linas@linas.org>
1998-12-30 07:05:34 +0000

    updates
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@1503 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/quotes/getquotes.pl
+++ src/quotes/getquotes.pl
@@ -1,118 +1,84 @@
-#!/usr/bin/perl
+#
+# FILE:
+# getquotes.pl
+#
+# FUNCTION:
+# perl utilities to fetch stock prices & volumes from various internet sources.
+#
+# HISTORY:
+# Contributed by C.B.Browne circa March 1998
+#
 #######################################################################
-# $Id$
-# Looks up investment prices on the web, and builds a report
-# to summarize the results
+######## &get_url_command("http://www.conline.com/~cbbrowne");#########
 #######################################################################
-$HISTORYFILE="/home/cbbrowne/financial/histprices";
-$newprices = "NO";  # Haven't found *any* new prices so far...
-
+# Build the appropriate command that accesses (in raw form) the
+# requested URL.  Probably ought to change this to use any of
+# -- the w3c "line mode" utility"
+# -- curl
+# -- lynx,
+# -- urlget
+# depending on which are installed.
 #######################################################################
-# Start by initializing the security list
-#######################################################################
-# My TSE stocks...  
-#######################################################################
-&add_stock("T", 100, 1670.5, "TSE");
-&add_stock("NVA", 100, 1345.5, "TSE");
-&add_stock("PCA", 100, 1733, "TSE");
-&add_stock("TOC", 50, 21.035*50, "TSE");
-&add_stock("RY", 50, 32.785*50, "TSE");
-
-#######################################################################
-# My Canada Trust Everest funds...
-#######################################################################
-# Original numbers of units...
-#&add_stock("CTMM", 83.951, 839.51, "CTE");  # Not strictly correct..
-#&add_stock("CTBOND", 121.747, 1315.17, "CTE");
-#&add_stock("CTSTK", 57.177, 832.73, "CTE");
-#&add_stock("CTSPEC", 28.263, 504.07, "CTE");
-#&add_stock("CTAMER", 177.723, 1983.05, "CTE");
-#&add_stock("CTUSEQ", 19.644, 294.91, "CTE");
-#&add_stock("CTASIA", 28.224, 251.99, "CTE");
-#&add_stock("CTEURO", 60.136, 530.50, "CTE");
-#&add_stock("CTEMER", 27.463, 266.67, "CTE");
-#&add_stock("CTIBND", 84.879, 907.37, "CTE");
-
-# Units as at Dec 31/97
-&add_stock("CTMM", 87.033, 839.51, "CTE"); 
-&add_stock("CTBOND", 131.818, 1315.17, "CTE");
-&add_stock("CTSTK", 57.425, 832.73, "CTE");
-&add_stock("CTSPEC", 28.263, 504.07, "CTE");
-&add_stock("CTAMER", 206.301, 1983.05, "CTE");
-&add_stock("CTUSEQ", 19.644, 294.91, "CTE");
-&add_stock("CTASIA", 28.238, 251.99, "CTE");
-&add_stock("CTEURO", 71.689, 530.50, "CTE");
-&add_stock("CTEMER", 27.463, 266.67, "CTE");
-&add_stock("CTIBND", 91.056, 907.37, "CTE");
-
-# Stuff on NYSE
-&add_stock("MOT", 15, (62+5/8)*15, "NYSE");
-#&add_stock("IFMXE", 0.1, 0.1, "NYSE");
-&add_stock("TSG", 13.0289, 43.02*5+43.75+44.48, "NYSE");
-
-
-#######################################################################
-# Working Ventures fund... Not currently updatable via the Web...
-#######################################################################
-&add_stock("WORKVE", 219.701, 3000, "OTHER");
-$PRICE{"WORKVE"} = 3000/219.701;   # Hack in a price for Working Ventures
-$OLD{"WORKVE"} = "yes";  # Indicate that this is *not* a "new" price...
-
-
-#######################################################################
-# Map CT Everest fund names to the "short" IDs 
-#######################################################################
-# I don't want to get long names; this builds an array that
-# shortens the names used in the CT Everest Funds web page to
-# the ones used above
-#######################################################################
-&ct_fund_ids;   # Get mapping of CT "long" fund names to "short" names
-
-&load_prices;   # Load old prices, so that failure to get a price
-                # means we fall back to the previously-found
-                # price
-
-$CURRNAME{"CDN"} = "Canadian";
-$CURRNAME{"USD"} = "United States";
-$CURRATE{"CDN"} = 1;
-$CURRATE{"USD"} = 1.37;
-# &get_currency("USD");
-#######################################################################
-# Now, get the current prices by sundry queries of web sites
-#######################################################################
-&search_web_for_prices();   # Find new closing prices
-&calc_variances();          # Calculate variances
-
-#######################################################################
-# Run report that details portfolio value based on the prices, costs
-# and quantities of shares...
-#######################################################################
-if ($newprices eq "YES") {
-    &show_report();     # Display a report summarizing the results...
-#    &show_variances();
-#    &hilo_portfolios();
-    &save_prices();     # Save prices...
+sub get_url_command {
+    local ($url) = @_;
+    return "lynx -source '$url'";
 }
-exit 0;   # Done
+
 
 #######################################################################
+###################### &get_quote_from_qyahoo(); ######################
 #######################################################################
+# Look up a stock via "yahoo"
+# usage: &get_yaho ("IBM", "NYSE");
+# 
+# returns values in associateive arrays:
+# $last, $lastdate,$lasttime, $change,$opening,$hi, $lo, $volume
+# adds the fetchs symbol to the indexed array $symbol
+#
+# sample usage:
+# require "getquotes.pl";
+# &get_quote_from_yahoo ("IBM", "NYSE");
+# $sym = $symbol[0];
+# printf "$sym last=$last{$sym} hi=$hi{$sym} lo=$lo{$sym} chg=$change{$sym} vol=$volume{$sym}\n";
+#
 #######################################################################
-#######################################################################
-#######################################################################
-######################## End of the main body #########################
-#######################################################################
-#######################################################################
-#######################################################################
-#######################################################################
-#######################################################################
+sub get_quote_from_yahoo {
+    local ($stock, $exchange) = @_;
+    local ($price);
 
-#######################################################################
-#######################################################################
-############################# Subroutines #############################
-#######################################################################
-#######################################################################
+    if (($exchange eq "NY") || ($exchange eq "NYSE")) {
+	$exchange = "";
+    }
+    if ($exchange eq "TSE") {
+	$exchange = ".TO";
+    }
+    
+    # build URL for quote
+    local ($page) = "http://quote.yahoo.com/download/quotes.csv?symbols=" . 
+           $stock . $exchange .
+	       "&format=sl1d1t1c1ohgv&ext=.csv";
 
+    local ($command) = &get_url_command($page);
+    # print "Will issue command: $command \n";
+
+    local ($line);
+    open(GETPAGE, "$command |");
+    while ($line = <GETPAGE>) {
+	#print "Received the following: $line";
+	local ($sym,$slast,$slastdate,$slasttime,
+               $schange,$sopening,$shi,$slo,$svolume)=split(/,/, $line);
+
+        $symbol [$#symbol+1] = $sym;
+        $last{$sym} = $slast;   
+        $lastdate{$sym} = $slastdate;   
+        $lastime{$sym} = $slastime;   
+        $change{$sym} = $schange;   
+        $opening{$sym} = $sopening;   
+        $hi{$sym} = $shi;   
+        $lo{$sym} = $slo;   
+        $volume{$sym} = $svolume;   
+    }
+}
 
 #######################################################################
 ############### &add_stock("NVA", 100, 1345.5, "TSE"); ################
@@ -137,18 +103,6 @@ sub add_stock {
         $currency = "USD";
     }
     $CURRENCY{$ticker} = $currency;
-}
-
-#######################################################################
-######## &get_url_command("http://www.conline.com/~cbbrowne");#########
-#######################################################################
-# Build the appropriate command that accesses (in raw form) the
-# requested URL.  Probably ought to change this to use the w3c
-# "line mode" utility, as it's minscule
-#######################################################################
-sub get_url_command {
-    local ($url) = @_;
-    return "lynx -source '$url'";
 }
 
 #######################################################################
@@ -239,48 +193,6 @@ sub get_TSE {
 	}
     }
     close GETPAGE;
-}
-
-#######################################################################
-############################ &get_yahoo(); ############################
-#######################################################################
-# Look up a stock via "yahoo"
-#######################################################################
-sub get_yahoo {
-    local ($stock, $exchange) = @_;
-    local ($price);
-    if (($exchange eq "NY") || ($exchange eq "NYSE")) {
-	$exchange = "";
-    }
-    if ($exchange eq "TSE") {
-	$exchange = ".TO";
-    }
-    local ($page) = "http://quote.yahoo.com/download/quotes.csv?symbols=" . 
-           $stock . $exchange .
-	       "&format=sl1d1t1c1ohgv&ext=.csv";
-    if ($OLD{$stock} eq "NO!") {  # We've already priced this stock
-	return;
-    }
-    local ($command) = &get_url_command($page);
-    local ($line);
-#    print $command, "\n";
-    open(GETPAGE, "$command |");
-    while ($line = <GETPAGE>) {
-#	print "$line";
-	local ($trash, $price)=split(/,/, $line);
-#	print "$trash, $price\n";
-#	print "Old price: ", $PRICE{$stock}, "\n";
-	if ((($price * 1.0) != $PRICE{$stock}) && ($price > 0) ) {
-	    $newprices = "YES";
-	    print "NEW PRICE: $stock, $price\n";
-	} else {
-	    $price = $PRICE{$stock};
-#	    print "Keep old price: $price\n";
-	}
-	$PRICE{$stock} = sprintf("%.4f", $price);
-	$OLD{$stock} = "NO!";  # Indicate that this is an update...
-#	print "Done - ", $stock, $PRICE{$stock}, $OLD{$stock}, "\n";
-    }
 }
 
 #######################################################################
@@ -384,7 +296,7 @@ sub save_prices {
 	    $key = $date . "" . $ticker;
 	    $price = $PRICE{$ticker};
             print HISTPRICES $date, "|", $ticker, "|", $price, "\n";;
-	}
+	} 
     }
     close HISTPRICES;
 }
@@ -411,16 +323,5 @@ sub load_prices {
     close HISTPRICES;
 }   
 
+1;
 
-#######################################################################
-############################ &show_report();###########################
-#######################################################################
-# Look up all of the securities, and build a report listing the cost,
-# value, and net profit/loss on each security, along with totals.
-########################################################################
-sub show_report {
-    local($date) = `date`;
-    chop $date;
-    local($ticker, $shares, $price, $value, $cost, $profitloss);
-    local ($tp, $tv, $tc);
-}
