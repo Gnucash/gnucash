a9bd8904ae756fd1eab6c747aa49d89dd390b77b
Linas Vepstas <linas@linas.org>
1999-01-02 04:22:30 +0000

    more fidelity things
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@1517 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/quotes/Quote.pm
+++ src/quotes/Quote.pm
@@ -28,18 +28,22 @@ require 5.000;
 require Exporter;
 use strict;
 use vars qw($VERSION @EXPORT @ISA $YAHOO_URL $FIDELITY_GANDI_URL
-            $FIDELITY_GROWTH_URL $FIDELITY_CORPBOND_URL);
+            $FIDELITY_GROWTH_URL $FIDELITY_CORPBOND_URL $FIDELITY_GLBND_URL
+            $FIDELITY_MM_URL);
 
 use LWP::UserAgent;
 use HTTP::Request::Common;
 
-$VERSION = '0.06';
+$VERSION = '0.07';
 @ISA = qw(Exporter);
 
 $YAHOO_URL = ("http://quote.yahoo.com/d?f=snl1d1t1c1p2va2bapomwerr1dyj1&s=");
 $FIDELITY_GANDI_URL = ("http://personal441.fidelity.com/gen/prices/gandi.csv");
 $FIDELITY_GROWTH_URL = ("http://personal441.fidelity.com/gen/prices/growth.csv");
 $FIDELITY_CORPBOND_URL = ("http://personal441.fidelity.com/gen/prices/corpbond.csv");
+$FIDELITY_GLBND_URL = ("http://personal441.fidelity.com/gen/prices/glbnd.csv");
+$FIDELITY_MM_URL = ("http://personal441.fidelity.com/gen/prices/mm.csv");
+$FIDELITY_ASSET_URL = ("http://personal441.fidelity.com/gen/prices/asset.csv");
 
 # Don't export; let user invoke with Quote::getquote syntax.
 # @EXPORT = qw(&quote_yahoo);
@@ -85,22 +89,35 @@ sub fidelity {
 
     # rather irritatingly, fidelity sorts its funds into different groups.
     # as a result, the fetch that we need to do depends on the group.
-    my @gandi =  ("FBALX", "FCVSX", "FEQIX", "FEQTX", "FFIDX", "FGRIX",
-                  "FIUIX", "FPURX", "FRESX", );
-    my @growth = ("FBGRX", "FCNTX", "FCONX", "FDCAX", "FDEGX", "FDEQX",
-                  "FDFFX", "FDGFX", "FDGRX", "FDSCX", "FDSSX", "FDVLX",
-                  "FEXPX", "FFTYX", "FLCSX", "FLPSX", "FMAGX", "FMCSX",
-                  "FMILX", "FOCPX", "FSLCX", "FSLSX", "FTQGX", "FTRNX",
-                  "FTXMX", );
+    my @gandi    = ("FBALX", "FCVSX", "FEQIX", "FEQTX", "FFIDX", "FGRIX",
+                    "FIUIX", "FPURX", "FRESX", );
+    my @growth   = ("FBGRX", "FCNTX", "FCONX", "FDCAX", "FDEGX", "FDEQX",
+                    "FDFFX", "FDGFX", "FDGRX", "FDSCX", "FDSSX", "FDVLX",
+                    "FEXPX", "FFTYX", "FLCSX", "FLPSX", "FMAGX", "FMCSX",
+                    "FMILX", "FOCPX", "FSLCX", "FSLSX", "FTQGX", "FTRNX",
+                    "FTXMX", );
     my @corpbond = ("FAGIX", "SPHIX", "FTHRX", "FBNDX", "FSHBX", "FSIBX", 
                     "FTBDX", "FSICX", "FTTAX", "FTTBX", "FTARX", );
-    my (%agandi, %agrowth, %acorpbond);
+    my @glbnd    = ("FGBDX", "FNMIX");
+    my @mm       = ("FDRXX", "FDTXX", "FGMXX", "FRTXX", "SPRXX", "SPAXX",
+                    "FDLXX", "FGRXX",);
+    my @asset    = ("FASMX", "FASGX", "FASIX", );
+
+    my (%agandi, %agrowth, %acorpbond, %aglbnd, %amm, %aasset);
+
     my $dgandi=0;
     my $dgrowth=0;
     my $dcorpbond=0;
+    my $dglbnd=0;
+    my $dmm=0;
+    my $dasset=0;
 
     for (@gandi) { $agandi{$_} ++; }
     for (@growth) { $agrowth{$_} ++; }
+    for (@corpbond) { $acorpbond{$_} ++; }
+    for (@glbnd) { $aglbnd{$_} ++; }
+    for (@mm) { $amm{$_} ++; }
+    for (@asset) { $aasset{$_} ++; }
    
     # for Fidelity, we get them all. 
     for (@symbols) {
@@ -122,6 +139,27 @@ sub fidelity {
           if (0 ==  $dcorpbond ) {
              %cc = &fidelity_corpbond;
              $dcorpbond = 1;
+             foreach $k (keys %cc) { $aa{$k} = $cc{$k}; }
+          }
+       }
+       if ($aglbnd {$_} ) {
+          if (0 ==  $dglbnd ) {
+             %cc = &fidelity_glbnd;
+             $dglbnd = 1;
+             foreach $k (keys %cc) { $aa{$k} = $cc{$k}; }
+          }
+       }
+       if ($amm {$_} ) {
+          if (0 ==  $dmm ) {
+             %cc = &fidelity_mm;
+             $dmm = 1;
+             foreach $k (keys %cc) { $aa{$k} = $cc{$k}; }
+          }
+       }
+       if ($aasset {$_} ) {
+          if (0 ==  $dasset ) {
+             %cc = &fidelity_asset;
+             $dasset = 1;
              foreach $k (keys %cc) { $aa{$k} = $cc{$k}; }
           }
        }
@@ -207,6 +245,106 @@ sub fidelity_corpbond {
 
     # for Fidelity, we get them all. 
     $url = $FIDELITY_CORPBOND_URL;
+    $ua = LWP::UserAgent->new;
+    foreach (split('\n',$ua->request(GET $url)->content)) {
+	@q = grep { s/^"?(.*?)\s*"?\s*$/$1/; } split(',');
+
+        # extract the date which is usually on the second line fo the file.
+        if (! defined ($dayte)) {
+           if ( $days {$q[0]} ) {          
+              $dayte = $q[1];
+           }
+        }
+        $sym = $q[2];
+        if ($sym) {
+            $aa {$sym, "exchange"} = "Fidelity";  # Fidelity
+            $aa {$sym, "name"} = $q[0];
+            $aa {$sym, "number"} = $q[1];
+            $aa {$sym, "nav"} = $q[3];
+            $aa {$sym, "change"} = $q[4];
+            $aa {$sym, "yield"} = $q[7];
+            $aa {$sym, "date"} = $dayte;
+        }
+    }
+    return %aa;
+}
+
+# =======================================================================
+
+sub fidelity_glbnd {
+    my @symbols = @_;
+    my(@q,%aa,$ua,$url,$sym, $dayte);
+    my %days = ('Monday','Mon','Tuesday','Tue','Wednesday','Wed',
+                'Thursday','Thu','Friday','Fri','Saturday','Sat','Sunday','Sun');
+
+    # for Fidelity, we get them all. 
+    $url = $FIDELITY_GLBND_URL;
+    $ua = LWP::UserAgent->new;
+    foreach (split('\n',$ua->request(GET $url)->content)) {
+	@q = grep { s/^"?(.*?)\s*"?\s*$/$1/; } split(',');
+
+        # extract the date which is usually on the second line fo the file.
+        if (! defined ($dayte)) {
+           if ( $days {$q[0]} ) {          
+              $dayte = $q[1];
+           }
+        }
+        $sym = $q[2];
+        if ($sym) {
+            $aa {$sym, "exchange"} = "Fidelity";  # Fidelity
+            $aa {$sym, "name"} = $q[0];
+            $aa {$sym, "number"} = $q[1];
+            $aa {$sym, "nav"} = $q[3];
+            $aa {$sym, "change"} = $q[4];
+            $aa {$sym, "yield"} = $q[7];
+            $aa {$sym, "date"} = $dayte;
+        }
+    }
+    return %aa;
+}
+
+# =======================================================================
+
+sub fidelity_mm {
+    my @symbols = @_;
+    my(@q,%aa,$ua,$url,$sym, $dayte);
+    my %days = ('Monday','Mon','Tuesday','Tue','Wednesday','Wed',
+                'Thursday','Thu','Friday','Fri','Saturday','Sat','Sunday','Sun');
+
+    # for Fidelity, we get them all. 
+    $url = $FIDELITY_MM_URL;
+    $ua = LWP::UserAgent->new;
+    foreach (split('\n',$ua->request(GET $url)->content)) {
+	@q = grep { s/^"?(.*?)\s*"?\s*$/$1/; } split(',');
+
+        # extract the date which is usually on the second line fo the file.
+        if (! defined ($dayte)) {
+           if ( $days {$q[0]} ) {          
+              $dayte = $q[1];
+           }
+        }
+        $sym = $q[2];
+        if ($sym) {
+            $aa {$sym, "exchange"} = "Fidelity";  # Fidelity
+            $aa {$sym, "name"} = $q[0];
+            $aa {$sym, "number"} = $q[1];
+            $aa {$sym, "yield"} = $q[3];
+            $aa {$sym, "date"} = $dayte;
+        }
+    }
+    return %aa;
+}
+
+# =======================================================================
+
+sub fidelity_asset {
+    my @symbols = @_;
+    my(@q,%aa,$ua,$url,$sym, $dayte);
+    my %days = ('Monday','Mon','Tuesday','Tue','Wednesday','Wed',
+                'Thursday','Thu','Friday','Fri','Saturday','Sat','Sunday','Sun');
+
+    # for Fidelity, we get them all. 
+    $url = $FIDELITY_ASSET_URL;
     $ua = LWP::UserAgent->new;
     foreach (split('\n',$ua->request(GET $url)->content)) {
 	@q = grep { s/^"?(.*?)\s*"?\s*$/$1/; } split(',');
