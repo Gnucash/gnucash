e377e3b573ccf40173eeae1f063ddd7524ca8dfa
Linas Vepstas <linas@linas.org>
1999-08-04 03:22:00 +0000

    add QIF import code from  cbbrowne@godel.brownes.org
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/xacc-12-patch@1878 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/extensions.scm
+++ src/scm/extensions.scm
@@ -16,4 +16,10 @@
                                  "Scheme test2 hint"
                                  gnc:extensions-menu-test-func))
 
+  (gnc:extensions-menu-add-item "QIF Import"
+                                "Import QIF hint"
+                                (lambda ()
+                                (gnc:extensions-qif-import win)))
+
+
 (gnc:hook-add-dangler gnc:*main-window-opened-hook* gnc:extensions-menu-setup)
--- src/scm/main.scm
+++ src/scm/main.scm
@@ -8,8 +8,9 @@
   
   (gnc:load "hooks.scm")
   (gnc:load "doc.scm")
-  (gnc:load "extensions.scm")           ; Should this be here or somewhere else?
+  (gnc:load "extensions.scm")      ; Should this be here or somewhere else?
   (gnc:load "text-export.scm")
+  (gnc:load "importqif.scm")
   
   ;; Load the system and user configs
   (if (not (gnc:load-system-config-if-needed))
