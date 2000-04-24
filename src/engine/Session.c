69be0e4b8f34324f077fb8a4b48c8a313af79adc
Linas Vepstas <linas@linas.org>
2000-04-24 21:12:41 +0000

    fix address of FSF in copyright notice
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@2203 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/Session.c
+++ src/engine/Session.c
@@ -22,8 +22,11 @@
  * GNU General Public License for more details.                     *
  *                                                                  *
  * You should have received a copy of the GNU General Public License*
- * along with this program; if not, write to the Free Software      *
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.        *
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
 \********************************************************************/
 
 #include <errno.h>
