b9edafc5d416322a1e1abb5db0294f8a760ada99
Dave Peticolas <dave@krondo.com>
2000-07-12 09:01:52 +0000

    Merge from HEAD. split auto-complete bug fix.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/1.4@2569 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/SplitLedger.c
+++ src/SplitLedger.c
@@ -998,29 +998,11 @@ LedgerAutoCompletion(SplitRegister *reg, gncTableTraversalDir dir,
       xaccSetComboCellValue (reg->xfrmCell, fullname);
       xaccBasicCellSetChanged(&(reg->xfrmCell->cell), GNC_T);
 
-      /* auto-complete the amounts */
-      if ((STOCK_REGISTER    == typo) ||
-          (CURRENCY_REGISTER == typo) ||
-          (PORTFOLIO_LEDGER  == typo)) 
-        amount = xaccSplitGetShareAmount (auto_split);
-      else
-        amount = xaccSplitGetValue (auto_split);
-
-      xaccSetDebCredCellValue (reg->debitCell, reg->creditCell, amount);
-      xaccBasicCellSetChanged(&(reg->debitCell->cell), GNC_T);
-      xaccBasicCellSetChanged(&(reg->creditCell->cell), GNC_T);
+      amount = xaccSplitGetValue (auto_split);
 
       xaccSetDebCredCellValue (reg->ndebitCell, reg->ncreditCell, -amount);
-      xaccBasicCellSetChanged(&(reg->ndebitCell->cell), GNC_T);
-      xaccBasicCellSetChanged(&(reg->ncreditCell->cell), GNC_T);
-
-      amount = xaccSplitGetSharePrice (auto_split);
-      xaccSetPriceCellValue (reg->priceCell, amount);
-      xaccBasicCellSetChanged(&(reg->priceCell->cell), GNC_T);
-
-      amount = xaccSplitGetValue (auto_split);
-      xaccSetPriceCellValue (reg->valueCell, amount);
-      xaccBasicCellSetChanged(&(reg->valueCell->cell), GNC_T);
+      xaccBasicCellSetChanged (&(reg->ndebitCell->cell), GNC_T);
+      xaccBasicCellSetChanged (&(reg->ncreditCell->cell), GNC_T);
 
       /* copy cursor contents into the table */
       xaccCommitCursor (reg->table);
