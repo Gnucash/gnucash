17d6719688b53d3b0a56f9b6b2321a984304545e
Dave Peticolas <dave@krondo.com>
2000-07-20 23:12:55 +0000

    Add warning about unstable version to tips of the day.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@2605 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/tip-list.scm
+++ src/scm/tip-list.scm
@@ -1,4 +1,8 @@
 (
+ ("Warning!! This is a DEVELOPMENT version of GnuCash."
+  "It probably has lots of bugs and unstable features!"
+  "If you are looking for a stable personal finance application,"
+  "you should use the latest release of GnuCash 1.4.")
  ("The GnuCash online manual has lots of helpful information."
   "You can access the manual under the Help menu.")
  ("GnuCash does not use categories like Quicken does."
