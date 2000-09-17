b44cdcc256ad4a49b8acc7874d142f6bb82aa006
Dave Peticolas <dave@krondo.com>
2000-09-17 07:57:45 +0000

    Remove obsolete docs.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@2876 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/register/QuickFill.h
+++ src/register/QuickFill.h
@@ -31,25 +31,6 @@
 #include <glib.h>
 
 
-/** STRUCTS *********************************************************/
-
-/* The way quickfill works is this: the decscription field of a transaction
- * is used to insert a pointer to itself into the quickfill tree.  The
- * QuickFill struct is a node in the tree, and the qf array is an array
- * of pointers to children of the node.  (NULL, if no corresponding child.)
- * The index of the array is determined by the next letter in the string
- * that is the description field.  The trans field is a pointer to the last
- * inserted child that made it to this node.  At the root of the tree is
- * a QuickFill struct, with trans == NULL, and the array of pointers to
- * children.  When a transaction is inserted, the first letter of the
- * description field determines which child the transaction goes into.  
- * If the child is NULL, a new QuickFill node is created.  Otherwise,
- * the trans field of the child is changed to point to the new transaction.
- * Then, recursively, the process is repeated, with the next character in
- * the description field as the index to the array.  The recursion stops
- * when the end of the descriptions string is reached.
- */
-
 typedef enum
 {
   QUICKFILL_LIFO,
