155ce3612b1ac24ec51e831630a17c30ed245d99
Dave Peticolas <dave@krondo.com>
2000-11-02 23:21:18 +0000

    Merge current account maintenance patch from stable.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@3117 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/account-tree.c
+++ src/gnome/account-tree.c
@@ -868,13 +868,18 @@ gnc_account_tree_select_row(GtkCTree *ctree,
   tree->ignore_unselect = FALSE;
 
   account = gtk_ctree_node_get_row_data(ctree, GTK_CTREE_NODE(row));
-  node = g_list_find(tree->current_accounts, account);
-  if (node == NULL)
-    tree->current_accounts = g_list_prepend(tree->current_accounts, account);
 
-  gtk_signal_emit(GTK_OBJECT(ctree),
-		  account_tree_signals[SELECT_ACCOUNT],
-		  account);
+  if (account != NULL)
+  {
+    node = g_list_find(tree->current_accounts, account);
+
+    if (node == NULL)
+      tree->current_accounts = g_list_prepend(tree->current_accounts, account);
+
+    gtk_signal_emit(GTK_OBJECT(ctree),
+                    account_tree_signals[SELECT_ACCOUNT],
+                    account);
+  }
 
   GTK_CTREE_CLASS(parent_class)->tree_select_row(ctree, row, column);
 }
