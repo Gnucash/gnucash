e24a668bad748af2cc1cd856cf5efb18358ddf0f
Dave Peticolas <dave@krondo.com>
2000-12-08 01:11:14 +0000

    Carol Champagne's updated tips of the day.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@3262 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/tip-list.scm
+++ src/scm/tip-list.scm
@@ -5,18 +5,53 @@
   "you should use the latest release of GnuCash 1.4.")
  ("The GnuCash online manual has lots of helpful information."
   "You can access the manual under the Help menu.")
- ("GnuCash does not use categories like Quicken does."
-  "Instead, you should use Income and Expense accounts.")
+ ("You can easily import your existing financial data from Quicken,"
+  "MS Money or other programs that export QIF (Quicken Interchange"
+  "Format) files.  Click the Import button in the main window tool"
+  "bar and follow the instructions provided.")
+ ("Create new accounts by clicking the New button in the main window"
+  "tool bar.  This will bring up a dialog box where you can enter"
+  "account details.  For more information on choosing an account type"
+  "or setting up a chart of accounts, please see the GnuCash online"
+  "manual.")
+ ("Click the right mouse button in the main window to bring up the"
+  "account menu options. Within each register, clicking the right"
+  "mouse button brings up the transaction menu options.")
+ ("As you enter amounts in the register, you can use the GnuCash"
+  "calculator to add, subtract, multiply and divide .  Simply type"
+  "the first value, then select '+', '-','*', or '/'.  Type the"
+  "second value and press Enter to record the calculated amount.")
+ ("If you are familiar with other financial programs such as Quicken,"
+  "note that GnuCash uses accounts instead of categories to track"
+  "income and expenses.  For more information on income and expense"
+  "accounts, please see the GnuCash online manual.")
+ ("Quick-fill makes it easy to enter common transactions.  When you"
+  "type the first letter(s) of a common transaction description,"
+  "GnuCash will automatically complete the remainder of the"
+  "transaction as it was last entered.")
+ ("Type the first letter(s) of an existing account name in the"
+  "Transfer register column, and GnuCash will complete the name from"
+  "your list of accounts. For subaccounts, type the first letter(s)"
+  "of the parent account, followed by ':' and the first letter(s) of"
+  "the subaccount (e.g. A:C for Assets:Cash.)")
+ ("To enter multiple-split transactions such as a paycheck with"
+  "multiple deductions, click the Split button in the tool bar."
+  "Or choose either Auto-Split Ledger or Transaction Journal"
+  "from the Register|Style menu option.")
+ ("Want to see all your subaccount transactions in one register?"
+  "From the main menu, highlight the parent account and select"
+  "Accounts|Open Subaccounts from the menu.")
  ("In the reconcile window, you can press the spacebar"
   "to mark transactions as reconciled."
   "You can also press Tab and Shift-Tab to move between"
   "deposits and withdrawals.")
  ("When entering check numbers in the register, you can"
-  "press '+' to enter the next number. You can use '+'"
-  "and '-' to increment and decrement the number, respectively.")
+  "type '+' to enter the next number or '-' to enter the previous"
+  "number. You can use '+' and '-' to increment and decrement"
+  "dates as well.")
  ("You should back up your files regularly.  Just copy" 
   "the GnuCash .xac file to a floppy or other backup device.")
  ("To transfer funds between accounts with different currencies,"
-  "you need an intermediate currency trading account."
+  "you may need an intermediate currency trading account."
   "Please see the GnuCash online manual for details")
 )
