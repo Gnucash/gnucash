13094b5629536daf0f1c6e3ddb231d34fc0ca441
Dave Peticolas <dave@krondo.com>
2001-01-05 23:19:35 +0000

    Carol Champagne's update to the help topics index.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@3392 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/help-topics-index.scm
+++ src/scm/help-topics-index.scm
@@ -1,42 +1,62 @@
-(("Gnucash manual" "" 
-  (("Overview of Gnucash" ""
+(("GnuCash Manual" "" 
+  (("Overview" ""
     (("Introduction" "gnc-help:xacc-about.html" #f)
-     ("Getting help" "gnc-help:xacc-userdocs.html" #f)
-     ("Top 10 reasons to use Gnucash" "" #f)))
-   ("Getting started" ""
-    (("Using double entry" "gnc-help:xacc-doubleentry.html" #f)
-     ("The main window" "gnc-help:xacc-mainwin.html" #f)
-     ("Creating and saving files" "" #f)
-     ("Importing QIF files" "gnc-help:xacc-qif-import.html" #f)
-     ("Setting preferences" "gnc-help:xacc-preferences.html" #f)))
-   ("Organizing your accounts" ""
-    (("Accounts vs. categories" "gnc-help:xacc-quicken.html" #f)
-     ("Types of Gnucash accounts" "gnc-help:xacc-acctypes.html" #f)
-     ("Using income/expense accounts" "gnc-help:xacc-incomeexpense.html" #f)
-     ("Setting up a new account" "gnc-help:xacc-newacctwin.html" #f)
-     ("Entering commodity information" "gnc-help:xacc-commodity.html" #f)
-     ("Setting up a chart of accounts" "gnc-help:xacc-chartofaccts.html" #f)
-     ("Editing an account" "gnc-help:xacc-accountedit.html" #f)))
-   ("Entering transactions" ""
-    (("Overview of registers" "gnc-help:xacc-regwin.html" #f)
-     ("Choosing a mode" "gnc-help:xacc-regwin.html" #f)
-     ("Entering opening balances" "" #f)
-     ("Entering a basic transaction" "" #f)
-     ("Entering a multi-split transaction" "" #f)
-     ("Using Quickfill and shortcuts" ""
-      (("Date data input" "gnc-help:xacc-dateinput.html" #f)
-       ("Keyboard shortcuts" "gnc-help:xacc-regwinkbd.html" #f)))
-     ("Reconciling and reviewing transactions" ""
-      (("Reconcile window" "gnc-help:xacc-recnwin.html" #f)
-       ("Adjust Balance window" "gnc-help:xacc-adjbalwin.html" #f)
-       ("Finding transactions" "gnc-help:xacc-locatingtxns.html" #f)))))))
- ("Guppi demos" "" 
+     ("Help" "gnc-help:xacc-userdocs.html" #f)))
+   ("Basic Setup" ""
+    (("Main Window" "gnc-help:xacc-mainwin.html" #f)
+     ("Double Entry" "gnc-help:xacc-doubleentry.html" #f)
+     ("QIF Import" "gnc-help:xacc-qif-import.html"
+      (("Quicken" "gnc-help:xacc-quicken.html" #f)
+       ("MYM" "gnc-help:bofa-mym.html" #f)))
+     ("Preferences" "gnc-help:xacc-preferences.html" #f)))
+   ("Accounts" ""
+    (("Types" "gnc-help:xacc-acctypes.html" #f)
+     ("Vs. Categories" "gnc-help:xacc-quicken.html" #f)
+     ("Income/Expense" "gnc-help:xacc-incomeexpense.html" #f)
+     ("New Setup" "gnc-help:xacc-newacctwin.html" 
+      (("Commodity""gnc-help:xacc-commodity.html" #f)))
+     ("Chart of" "gnc-help:xacc-chartofaccts.html" #f)
+     ("Editing" "gnc-help:xacc-accountedit.html" #f)))
+   ("Transactions" ""
+    (("Registers" "gnc-help:xacc-regwin.html" #f)
+     ("Shortcuts" ""
+      (("Date Input" "gnc-help:xacc-dateinput.html" #f)
+       ("Keyboard" "gnc-help:xacc-regwin-kbd.html" #f)))
+     ("Reconciliation" "gnc-help:xacc-recnwin.html" #f)
+     ("Adjust Balance" "gnc-help:xacc-adjbalwin.html" #f)
+     ("Search" "gnc-help:xacc-locatingtxns.html" #f)))
+   ("Record Keeping" ""
+    (("Check Printing" "gnc-help:xacc-print-check.html" #f)
+     ("Capital Gains" "gnc-help:xacc-depreciation.html" #f)
+     ("Stock Ticker" "gnc-help:xacc-ticker.html" #f)
+     ("Depreciation" "gnc-help:xacc-depreciation.html" #f)
+     ("Currency" "gnc-help:xacc-currencyhandling.html"
+      ("Euro" "gnc-help:xacc-euro.html" #f))
+     ("Business" ""
+      (("Payables/Receivables" "gnc-help:xacc-apar.html" #f)
+       ("Depreciation" "gnc-help:xacc-depreciation.html" #f)))))
+   ("Reports" ""
+    (("Overview" "gnc-help:xacc-reports.html" #f)
+     ("Types" ""
+     (("Balance Sheet" "gnc-help:xacc-balancesheet.html" #f)
+      ("Profit & Loss" "gnc-help:xacc-pnl.html" #f)
+      ("Transaction" "gnc-help:xacc-trans-report.html" #f)
+      ("Stock Portfolio" "gnc-help:xacc-portfolio-report.html" #f)
+      ("Balance Tracking" "gnc-help:xacc-balancereport.html" #f)
+      ("Tax Report" "gnc-help:xacc-tax-report.html"
+       (("TXF Export" "gnc-help:xacc-txf-export-report.html"#f)
+        ("TXF Anomalies" "gnc-help:xacc-txf-export-anomalies.html" #f)))
+      ("Account Chart" "gnc-help:xacc-hierarchical-report.html" #f)))
+     ("Printing" "gnc-help:xacc-print.html" #f)))
+   ("More on GnuCash" ""
+    (("Hacking Documentation" "gnc-help:xacc-dochack.html" #f)
+     ("GNU GPL" "gnc-help:xacc-gpl.html" #f)))))
+ ("Guppi Demos" "" 
   (("Pie chart" "gnc-help:guppi-pie-demo.html" #f)
    ("Bar graph" "gnc-help:guppi-bar-demo.html" #f)
    ("Points" "gnc-help:guppi-points-demo.html" #f)))
- ("URL tests" ""
+ ("URL Tests" ""
   (("gnucash.org" "http://www.gnucash.org" #f)
    ("gnumatic.com" "http://www.gnumatic.com" #f)
+
    ("slashdot.org" "http://www.slashdot.org" #f))))
-
-
