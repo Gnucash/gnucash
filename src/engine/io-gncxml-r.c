867de1644bdcaa5e0c11d7522eea6a7008087661
Dave Peticolas <dave@krondo.com>
2001-03-04 11:09:23 +0000

    Add GPL headers.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@3745 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/io-gncxml-r.c
+++ src/engine/io-gncxml-r.c
@@ -1,11 +1,29 @@
-/*
- * io-gncxml-r.c -- read XML-format gnucash data file
- *
- * Initial code by Rob l. Browning 4Q 2000
- * Tuneups by James LewisMoss Dec 2000
- * Excessive hacking inas Vepstas January 2001
- *
- */
+/********************************************************************
+ * io-gncxml-r.c -- read XML-format gnucash data file               *
+ * Copyright (C) 2000 Gnumatic, Inc.                                *
+ *                                                                  *
+ * Initial code by Rob L. Browning 4Q 2000                          *
+ * Tuneups by James LewisMoss Dec 2000                              *
+ * Excessive hacking Linas Vepstas January 2001                     *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ *******************************************************************/
 
 #include "config.h"
 
--- src/engine/sixtp-kvp-parser.c
+++ src/engine/sixtp-kvp-parser.c
@@ -1,3 +1,26 @@
+/********************************************************************
+ * sixtp-kvp-parser.c                                               *
+ * Copyright 2001 Gnumatic, Inc.                                    *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+ ********************************************************************/
+
 #include "config.h"
 
 #include <string.h>
