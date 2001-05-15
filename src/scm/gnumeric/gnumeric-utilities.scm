8529c3558a1637d969e557063cccdc572517f5d6
Rob Browning <rlb@cs.utexas.edu>
2001-05-15 16:33:56 +0000

    * src/scm/report/transaction-report.scm (addto!): make a let-syntax since you apparently can't define-syntax before other defines in a nested let.
    * src/scm/report/register.scm (addto!): make a let-syntax since
    you apparently can't define-syntax before other defines in a
    nested let.
    
    * src/scm/gnumeric/gnumeric-utilities.scm: use srfis as modules.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4213 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/gnumeric/gnumeric-utilities.scm
+++ src/scm/gnumeric/gnumeric-utilities.scm
@@ -1,6 +1,8 @@
 ;;;; $Id$
 ;;;; gnumeric-utilities.scm - Gnumeric spreadsheet generation functions
 
+(use-modules (srfi srfi-19))
+
 (gnc:support "gnumeric/gnumeric-utilities.scm")
 (gnc:depend "xml-generator.scm")
 
@@ -39,7 +41,6 @@
 ;;; to suggest a better function.
 ;;; The point of this is that Gnumeric uses this as the "native" data
 ;;; representation. 
-(gnc:depend "srfi/srfi-19.scm")
 
 (define (exceldate y m d)
   (let 
