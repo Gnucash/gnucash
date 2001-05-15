a5a7fb93df8633f26babd855d5e607e24941d979
Rob Browning <rlb@cs.utexas.edu>
2001-05-15 16:28:59 +0000

    * src/scm/srfi/Makefile.am: moved to lib/srfi.
    * src/scm/srfi/README: moved to lib/srfi.
    
    * src/scm/srfi/srfi-1.r5rs.scm: merged to lib/srfi/srfi-1.scm.
    
    * src/scm/srfi/srfi-1.unclear.scm: merged to lib/srfi/srfi-1.scm.
    
    * src/scm/srfi/srfi-19.scm: moved to lib/srfi.
    
    * src/scm/srfi/srfi-8.guile.scm: merged to lib/srfi/srfi-8.scm.
    
    * src/scm/srfi/srfi-8.scm: merged to lib/srfi/srfi-8.scm.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4212 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/srfi/srfi-8.scm
+++
@@ -1,8 +0,0 @@
-;;; I'm maintaining the license that Olin put on the SRFI-1 reference
-;;; code.
-;;;
-;;; Copyright 1999, Rob Browning <rlb@cs.utexas.edu>. You may do as
-;;; you please with this code as long as you do not remove this
-;;; copyright notice or hold me liable for its use.
-
-(load "srfi-8.guile.scm")
