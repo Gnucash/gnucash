1e8f01215274d43f696257f378f6b37a5a6d6770
James LewisMoss <dres@debian.org>
2001-05-18 03:02:01 +0000

    remove cruft.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4235 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/io-gncxml.h
+++ src/engine/io-gncxml.h
@@ -43,36 +43,6 @@
 /* read in an account group from a file */
 gboolean gnc_book_load_from_xml_file(GNCBook *book);
 
-/* write all account info to a file */
-gboolean gnc_book_write_to_xml_file(GNCBook *book, const char *filename);
-
-#if 0
-
-/* read an account group from a buffer in memory 
- * the pointer bufp must point at the ascii xml, and bufsz
- * must be the size of the buffer.
- */
-AccountGroup *gncxml_read_from_buf (char *bufp, int bufsz);
-
-/* write all account info into memory.  This routine returns a
- * pointer to freshly malloced memory in bufp. You muse free
- * this memory when done.  The size that was written out is
- * returned in sz
- */
-void gncxml_write_to_buf (AccountGroup *group, char **bufp, int *sz);
- 
-/*
- * write only the account and currency info to the buf, do *not*
- * write any of the splits or transactions
- */
-void gncxml_write_group_to_buf (AccountGroup *group, char **bufp, int *sz);
-
-#endif
-
-/* write/read query terms to/from memory */
-void gncxml_write_query_to_buf (Query *q, char **bufp, int *sz);
-Query *gncxml_read_query (char *bufp, int bufsz);
-
 /* The is_gncxml_file() routine checks to see if the first few 
  * chars of the file look like gnc-xml data.
  */
