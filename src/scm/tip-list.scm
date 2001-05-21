fb258f4b78579bcbad303041e4db7c4d8096fc9b
Robert Graham Merkel <robert.merkel@benambra.org>
2001-05-21 02:27:11 +0000

    more documentation updates
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4248 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/tip-list.scm
+++ src/scm/tip-list.scm
@@ -12,6 +12,11 @@ MS Money or other programs that export QIF (Quicken Interchange
 Format) files.  Click the Import button in the main window tool
 bar and follow the instructions provided.")
 
+(N_ "If you are familiar with other financial programs such as Quicken,
+note that GnuCash uses accounts instead of categories to track
+income and expenses.  For more information on income and expense
+accounts, please see the GnuCash online manual.")
+
  (N_ "Create new accounts by clicking the New button in the main window
 tool bar.  This will bring up a dialog box where you can enter
 account details.  For more information on choosing an account type
@@ -27,11 +32,7 @@ calculator to add, subtract, multiply and divide .  Simply type
 the first value, then select '+', '-','*', or '/'.  Type the
 second value and press Enter to record the calculated amount.")
 
- (N_ "If you are familiar with other financial programs such as Quicken,
-note that GnuCash uses accounts instead of categories to track
-income and expenses.  For more information on income and expense
-accounts, please see the GnuCash online manual.")
-
+ 
  (N_ "Quick-fill makes it easy to enter common transactions.  When you
 type the first letter(s) of a common transaction description,
 GnuCash will automatically complete the remainder of the
@@ -68,4 +69,30 @@ your GnuCash data file to a floppy or other backup device.")
  (N_ "To transfer funds between accounts with different currencies,
 you may need an intermediate currency trading account.
 Please see the GnuCash online manual for details")
+
+ (N_ "GnuCash can display a variety of useful reports and
+graphs, which can be printed and exported as HTML.  Reports
+are created using \"New Report\" on the file menu.")
+
+ (N_ "You can pack multiple reports into a single window, 
+providing all the financial information you want at a glance.
+To do so, use the \"Multicolumn View\" report.")
+
+ (N_ "GnuCash can act as a simple web browser!  To display
+a web page as a report, use the \"Frame URL\" report.")
+
+ (N_ "Style Sheets affect how reports are displayed.  Choose
+a style sheet for your report as a report option, and use
+the Style Sheets menu to customize style sheets.")
+
+ (N_ "The GnuCash developers are easy to contact.  As well
+as several mailing lists, you can chat to them live on IRC!
+Join them on #gnucash at irc.gnome.org")
+
+ (N_ "There is a theory that if ever anyone discovers what
+the Universe is for and why it is here, it will instantly
+disappear and be replace with something even more bizarre and
+inexplicable.
+There is another theory that this has already happened.
+Douglas Adams, \"The Restaurant at the End of the Universe\"")
 )
