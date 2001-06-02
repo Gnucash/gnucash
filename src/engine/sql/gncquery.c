4414d37d18a6c29d008b8cec2e8709bef306ff69
Linas Vepstas <linas@linas.org>
2001-06-02 04:48:53 +0000

    add support for more query types
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4367 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/sql/gncquery.c
+++ src/engine/sql/gncquery.c
@@ -1,3 +1,25 @@
+/********************************************************************\
+ * gncquery.c : Convert gnucash engine Query into an SQL Query      *
+ * Copyright (C) 2001 Linas Vepstas <linas@linas.org>               *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
+
 /*
  * FILE:
  * gncquery.c
@@ -17,6 +39,7 @@
 #include <glib.h>
 #include <string.h>
 
+#include "escape.h"
 #include "gnc-engine-util.h"
 #include "gncquery.h"
 
@@ -27,6 +50,7 @@ struct _gnc_query {
    char * q_base;
    char * pq;
    size_t buflen;
+   sqlEscape *escape;
 };
 
 
@@ -43,6 +67,7 @@ sqlQuery_new(void)
    sq->buflen = INITIAL_BUFSZ;
 
    sq->pq = sq->q_base;
+   sq->escape = sqlEscape_new ();
    return sq;
 }
 
@@ -51,7 +76,32 @@ sql_Query_destroy (sqlQuery *sq)
 {
    if (!sq) return;
    g_free(sq->q_base);
+   sqlEscape_destroy (sq->escape);
    g_free(sq);
+}
+
+/* =========================================================== */
+
+#define STRING_TERM(fieldname)				\
+{							\
+   const char *tmp;					\
+							\
+   if (0 == pd->str.sense)				\
+   {							\
+      sq->pq = stpcpy (sq->pq, "NOT (");		\
+   }							\
+   sq->pq = stpcpy(sq->pq, fieldname " ~");		\
+   if (0 == pd->str.case_sens) {			\
+      sq->pq = stpcpy(sq->pq, "*");			\
+   }							\
+   sq->pq = stpcpy(sq->pq, " '");			\
+   tmp = sqlEscapeString (sq->escape, pd->str.matchstring); \
+   sq->pq = stpcpy(sq->pq, tmp);			\
+   sq->pq = stpcpy(sq->pq, "'");			\
+   if (0 == pd->str.sense)				\
+   {							\
+      sq->pq = stpcpy (sq->pq, ") ");			\
+   }							\
 }
 
 /* =========================================================== */
@@ -70,7 +120,9 @@ sqlQuery_build (sqlQuery*sq, Query *q)
 
    /* reset the buffer pointers */
    sq->pq = sq->q_base;
-   sq->pq = stpcpy(sq->pq, "SELECT transGuid FROM gncEntry WHERE ");
+   sq->pq = stpcpy(sq->pq, 
+               "SELECT gncEntry.transGuid FROM gncEntry, gncTransaction "
+               "  WHERE gncEntry.transGuid = gncTransaction.transGuid AND ( ");
 
    qterms = xaccQueryGetTerms (q);
 
@@ -111,52 +163,105 @@ sqlQuery_build (sqlQuery*sq, Query *q)
 
                PINFO("term is PR_ACCOUNT");
 
-               /* hack alert --  the following code is ignoring 
-                * both the 'how' and the 'sense' fields */
                for (acct = pd->acct.account_guids; acct; acct=acct->next)
                {
                   if (got_more) sq->pq = stpcpy(sq->pq, " AND ");
                   got_more = 1;
 
-                  sq->pq = stpcpy(sq->pq, "accountguid='");
+                  if ((0 == pd->acct.sense && ACCT_MATCH_NONE != pd->acct.how) ||
+                      (1 == pd->acct.sense && ACCT_MATCH_NONE == pd->acct.how))
+                  {
+                     sq->pq = stpcpy (sq->pq, "NOT ");
+                  }
+                  sq->pq = stpcpy(sq->pq, "gncEntry.accountGuid='");
                   sq->pq = guid_to_string_buff ((GUID*) acct->data, sq->pq);
                   sq->pq = stpcpy(sq->pq, "'");
                }
                break;
             }
+
             case PR_ACTION:
                PINFO("term is PR_ACTION");
+               STRING_TERM ("gncEntry.action");
                break;
+
             case PR_AMOUNT:
                PINFO("term is PR_AMOUNT");
+               PERR ("not implemented");
                break;
             case PR_BALANCE:
                PINFO("term is PR_BALANCE");
+               PERR ("not implemented");
                break;
             case PR_CLEARED:
                PINFO("term is PR_CLEARED");
+               PERR ("not implemented");
                break;
+
             case PR_DATE:
+            {
                PINFO("term is PR_DATE");
+               if (0 == pd->acct.sense)
+               {
+                  sq->pq = stpcpy (sq->pq, "NOT (");
+               }
+               if (pd->date.use_start)
+               {
+                  sq->pq = stpcpy(sq->pq, "gncTransaction.date_posted >= '");
+                  sq->pq = gnc_timespec_to_iso8601_buff (pd->date.start, sq->pq);
+                  sq->pq = stpcpy(sq->pq, "' ");
+               }
+               if (pd->date.use_end)
+               {
+                  if (pd->date.use_start) 
+                  {
+                     sq->pq = stpcpy(sq->pq, "AND ");
+                  }
+                  sq->pq = stpcpy(sq->pq, "gncTransaction.date_posted <= '");
+                  sq->pq = gnc_timespec_to_iso8601_buff (pd->date.end, sq->pq);
+                  sq->pq = stpcpy(sq->pq, "' ");
+               }
+               if (!pd->date.use_start && !pd->date.use_end) 
+               {
+                  sq->pq = stpcpy(sq->pq, "TRUE ");
+               }
+               if (0 == pd->acct.sense)
+               {
+                  sq->pq = stpcpy (sq->pq, ") ");
+               }
                break;
+            }
+
             case PR_DESC:
                PINFO("term is PR_DESC");
+               STRING_TERM ("gncTransaction.description");
                break;
+
             case PR_MEMO:
                PINFO("term is PR_MEMO");
+               STRING_TERM ("gncEntry.memo");
                break;
+
             case PR_MISC:
                PINFO("term is PR_MISC");
+               PERR ("not implemented");
                break;
+
             case PR_NUM:
                PINFO("term is PR_NUM");
+               STRING_TERM ("gncTransaction.num");
                break;
+
             case PR_PRICE:
                PINFO("term is PR_PRICE");
+               PERR ("not implemented");
                break;
+
             case PR_SHRS:
                PINFO("term is PR_SHRS");
+               PERR ("not implemented");
                break;
+
             default:
                PERR ("unkown query term type");
          }
@@ -166,6 +271,7 @@ sqlQuery_build (sqlQuery*sq, Query *q)
       if (il->data) sq->pq = stpcpy(sq->pq, ")");
    }
 
+   sq->pq = stpcpy(sq->pq, ")");
 
    /* limit the query result to a finite numbe of rows */
    max_rows = xaccQueryGetMaxSplits (q);
