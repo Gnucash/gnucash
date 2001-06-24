4c4f76674564574468d41afcaf2a4a626cbccc65
Dave Peticolas <dave@krondo.com>
2001-06-24 05:16:29 +0000

    2001-06-23  Kevin Finn  <kevinfinn@mediaone.net>
    	* src/gnome/window-reconcile.c: fix for display of auto end value
    	updates.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4770 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/window-reconcile.c
+++ src/gnome/window-reconcile.c
@@ -333,6 +333,10 @@ gnc_start_recn_date_changed (GtkWidget *widget, startRecnWindowData *data)
       xaccAccountGetShareBalanceAsOfDate (data->account, new_date) :
       xaccAccountGetBalanceAsOfDate (data->account, new_date);
 
+    /* use the correct sign */
+    if (gnc_reverse_balance (data->account))
+      new_balance = gnc_numeric_neg (new_balance);
+
     /* update the amount edit with the amount */
     gnc_amount_edit_set_amount (GNC_AMOUNT_EDIT (data->end_value), new_balance);
 }
