84c3a5d9386bb4497bece020eaf26d970d3ae6d7
Linas Vepstas <linas@linas.org>
2001-07-25 03:21:13 +0000

    modernize
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@4993 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/optional/swig/examples/scan-acct.pl
+++ src/optional/swig/examples/scan-acct.pl
@@ -6,15 +6,18 @@
 # the names and balances of the top-level accounts in the file.
 #
 # use lib '../swig/perl5/';
-use lib '../perl5/';
+use lib '..';
 use gnucash;                                           
 package gnucash;
 
 die "Usage: $0 <filename>" if $#ARGV < 0;
 print "its $ARGV[0]\n";
 
-$sess = gnucash::xaccMallocSession ();
-$grp = gnucash::xaccSessionBeginFile ($sess,$ARGV[0]);
+$session = gnucash::gnc_book_new ();
+gnucash::gnc_book_begin ($session, $ARGV[0], 0, 0);
+gnucash::gnc_book_load ($session);
+
+$grp = gnucash::gnc_book_get_group ($session);
 $numacc = gnucash::xaccGroupGetNumAccounts ($grp);
 print "Loaded $numacc accounts\n\n";
 
@@ -25,4 +28,4 @@ for ($i=0; $i<$numacc; $i++) {
    print "\tAccount: $acctname \tBalance: $baln\n";
 }
 
-gnucash::xaccSessionEnd ($sess);
+gnucash::gnc_book_end ($session);
