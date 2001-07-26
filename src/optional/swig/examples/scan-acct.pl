598441ad216216de3e3c5a59e61aa4f31adb41cc
Linas Vepstas <linas@linas.org>
2001-07-26 01:08:12 +0000

    add timing info
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@5016 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/optional/swig/examples/scan-acct.pl
+++ src/optional/swig/examples/scan-acct.pl
@@ -16,15 +16,21 @@ print "Will load $ARGV[0]\n";
 gnucash::gnc_engine_init(0, $ARGV);
 $session = gnucash::gnc_book_new ();
 
-$rc = gnucash::gnc_book_begin ($session, $ARGV[0], 0, 0);
+$rc = gnucash::gnc_book_begin ($session, $ARGV[0], 1, 0);
 if ($rc != 1) 
 {
    $err = gnucash::gnc_book_get_error ($session);
    print "Could not find $ARGV[0], errrocode=$err\n";
 }
 
+$secs = time;
+
 $rc = gnucash::gnc_book_load ($session);
 die "Could not load $ARGV[0]\n" if $rc != 1;
+
+($user,$sys,$cuser,$csys) = times;
+$elapsed = time() - $secs;
+print "time to load: user-cpu=$user sys-cpu=$sys elapsed(rounded to sec)=$elapsed\n";
 
 $grp = gnucash::gnc_book_get_group ($session);
 $numacc = gnucash::xaccGroupGetNumAccounts ($grp);
@@ -38,4 +44,16 @@ for ($i=0; $i<$numacc; $i++) {
    print "\tAccount: $acctname \tBalance: $baln\n";
 }
 
+$fsecs = time;
+($user,$sys,$cuser,$csys) = times;
+
 gnucash::gnc_book_end ($session);
+
+($fuser,$fsys,$cuser,$csys) = times;
+$elapsed = time() - $secs;
+print "time to finish: user-cpu=$fuser sys-cpu=$fsys elapsed(rounded to sec)=$elapsed\n";
+$fuser -= $user;
+$fsys -= $sys;
+$elapsed = time() - $fsecs;
+print "delta time to finish: user-cpu=$fuser sys-cpu=$fsys elapsed(rounded to sec)=$elapsed\n";
+
