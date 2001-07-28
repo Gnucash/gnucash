7a0527e19bb497f9776c3cdf7623c324d11aa57c
Linas Vepstas <linas@linas.org>
2001-07-28 03:26:45 +0000

    add version upgrade header
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@5047 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/engine/sql/version.h
@@ -0,0 +1,38 @@
+/********************************************************************\
+ * version.h -- handle back-ward compatible database table upgrades *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
+
+#ifndef PGEND_VERSION_H
+#define PGEND_VERSION_H
+
+/* The pgendVersionIsCurrent() routine ... Are we up to date ? 
+ *   Return 0 if we are at db version. Return +1 if we are newer.
+ *   Return -1 if we are older and so we can't run.
+ */
+
+int pgendVersionIsCurrent (PGBackend *be);
+
+/* The pgendUpgradeDB() routine installs upgrade features
+ *    into the sql database
+ */
+
+void pgendUpgradeDB (PGBackend *be);
+
+#endif PGEND_VERSION_H
