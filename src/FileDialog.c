9b51abbe21b7da440f6e1ca0f448544d7ba10f6d
Christian Stimming <stimming@tuhh.de>
2001-08-11 13:44:33 +0000

    2001-08-11  Christian Stimming  <stimming@tuhh.de>
    	* src/FileDialog.c (show_book_error): fix message.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@5113 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/FileDialog.c
+++ src/FileDialog.c
@@ -176,7 +176,7 @@ show_book_error (GNCBackendError io_error, const char *newfile)
       
     case ERR_SQL_DB_TOO_OLD:
       fmt = _("This database is from an older version of GnuCash.\n"
-              "Do you want to want to upgrade the database"
+              "Do you want to upgrade the database "
               "to the current version?");
       if (gnc_verify_dialog (fmt, TRUE)) { uh_oh = FALSE; }
       break;
