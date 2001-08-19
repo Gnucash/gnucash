08bbe150253eafd8fcc5edbb53c66f9ba3d6bd06
Christian Stimming <stimming@tuhh.de>
2001-08-19 21:51:29 +0000

    2001-08-19  Christian Stimming  <stimming@tuhh.de>
    	* po/gnc-glossary.txt, po/glossary/gnc-glossary.txt,
    	po/glossary/txt-to-pot.sh, po/glossary/de.po: Moved from the
    	concept of one big ascii table to the concept of one po file for
    	each translation of the glossary. Hence, created this
    	subdirectory.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@5194 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ po/glossary/txt-to-pot.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# This is a very, _very_, simple script to convert the .csv file into a .pot/.po.
+# Its not clever but it took me 2 minutes to write :)
+# Michael Twomey <michael.twomey@ireland.sun.com>
+# 23 March 2001
+# with slight GnuCash modifications by Christian Stimming <stimming@tuhh.de> 
+# 19 Aug 2001
+
+#check args
+if [ $# -eq 0 ]
+then
+	cat <<!
+Usage: `basename $0` gnc-glossary.txt > glossary.pot
+!
+	exit 1;
+fi
+
+GLOSSARY_CSV="$1";
+
+if [ ! -f "$GLOSSARY_CSV" ]
+then
+	echo "Can't find $GLOSSARY_CSV.";
+	exit 1;
+fi
+
+cat <<!
+# SOME DESCRIPTIVE TITLE.
+# Copyright (C) YEAR Free Software Foundation, Inc.
+# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.
+#
+#, fuzzy
+msgid ""
+msgstr ""
+"Project-Id-Version: PACKAGE VERSION\n"
+"POT-Creation-Date: `date +'%Y-%m-%d %H:%M%z'`\n"
+"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
+"Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
+"Language-Team: LANGUAGE <LL@li.org>\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=CHARSET\n"
+"Content-Transfer-Encoding: ENCODING\n"
+
+!
+
+#Yes this is the most simple awk script you've ever seen :)
+awk -F'\t' '{if ($2 != "") print "#. "$2; print "msgid "$1; print "msgstr \"\"\n"}' \
+$GLOSSARY_CSV
