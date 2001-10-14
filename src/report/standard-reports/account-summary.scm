728b711cd32d30ba945b97e4a2baf51d24a9f7da
Rob Browning <rlb@cs.utexas.edu>
2001-10-14 17:46:56 +0000

    * src/report/standard-reports/account-summary.scm: added use-modules.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@5613 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/standard-reports/account-summary.scm
+++ src/report/standard-reports/account-summary.scm
@@ -29,8 +29,9 @@
 (define-module (gnucash report account-summary))
 
 (use-modules (srfi srfi-1))
-
+(use-modules (gnucash bootstrap) (g-wrapped gw-gnc)) ;; FIXME: delete after we finish modularizing.
 (use-modules (gnucash gnc-module))
+
 (gnc:module-load "gnucash/report/report-system" 0)
 
 ;; account summary report
