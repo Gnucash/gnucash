53436822d2bc68d14120c5a08a7354b718556d40
Derek Atkins <derek@ihtfp.com>
2001-11-27 22:00:34 +0000

    fix a/r,a/p layout for split-view
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6081 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/register/ledger-core/split-register-layout.c
+++ src/register/ledger-core/split-register-layout.c
@@ -193,9 +193,10 @@ gnc_split_register_set_cells (SplitRegister *reg, TableLayout *layout)
 	gnc_table_layout_set_cell (layout, curs, DDUE_CELL,  0, 1);
         gnc_table_layout_set_cell (layout, curs, NUM_CELL,   0, 2);
         gnc_table_layout_set_cell (layout, curs, DESC_CELL,  0, 3);
-        gnc_table_layout_set_cell (layout, curs, TDEBT_CELL, 0, 4);
-        gnc_table_layout_set_cell (layout, curs, TCRED_CELL, 0, 5);
-        gnc_table_layout_set_cell (layout, curs, TBALN_CELL, 0, 6);
+
+        gnc_table_layout_set_cell (layout, curs, TDEBT_CELL, 0, 6);
+        gnc_table_layout_set_cell (layout, curs, TCRED_CELL, 0, 7);
+        gnc_table_layout_set_cell (layout, curs, TBALN_CELL, 0, 8);
 
         curs_last = curs;
         curs = gnc_table_layout_get_cursor (layout,
