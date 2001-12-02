2e0a6d197f3513bc84a0b6c76c5caf12cac0f32f
Linas Vepstas <linas@linas.org>
2001-12-02 18:26:51 +0000

    stub out test for accounting periods
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6167 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/engine/test/test-period.c
@@ -0,0 +1,63 @@
+
+/* test file created by Linas Vepstas <linas@linas.org>
+ * December 2001
+ * License: GPL
+ */
+
+#include <ctype.h>
+#include <glib.h>
+#include <guile/gh.h>
+#include <time.h>
+
+#include "Period.h"
+#include "gnc-book.h"
+#include "gnc-book-p.h"
+#include "gnc-module.h"
+#include "test-stuff.h"
+#include "test-engine-stuff.h"
+
+
+
+static void
+run_test (void)
+{
+  AccountGroup *grp;
+  GNCBook *book;
+  Timespec ts;
+
+int ok = 1;
+    if (!ok)
+    {
+      failure ("its borken");
+    }
+
+  book = gnc_book_new ();
+  if (!book)
+  {
+    failure("book not created");
+    exit(get_rv());
+  }
+
+  grp = get_random_group (book);
+  if(!grp)
+  {
+    failure("group not created");
+    exit(get_rv());
+  }
+
+  gnc_book_set_group (book, grp);
+
+
+  success ("periods work but wern't really tested yet");
+}
+
+int
+main (int argc, char **argv)
+{
+  run_test ();
+
+  success ("periods aren't realy tested yet");
+
+  print_test_results();
+  exit(get_rv());
+}
