d61b54006376078853c76fc216e08ed79e5fab17
Rob Browning <rlb@cs.utexas.edu>
2001-12-04 23:29:53 +0000

    * src/report/standard-reports/balance-sheet.scm: rename bootstrap to main.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6243 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/standard-reports/balance-sheet.scm
+++ src/report/standard-reports/balance-sheet.scm
@@ -26,7 +26,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-module (gnucash report balance-sheet))
-(use-modules (gnucash bootstrap) (g-wrapped gw-gnc)) ;; FIXME: delete after we finish modularizing.
+(use-modules (gnucash main) (g-wrapped gw-gnc)) ;; FIXME: delete after we finish modularizing.
 (use-modules (ice-9 slib))
 (use-modules (gnucash gnc-module))
 
