d26a9d3701951fdd7526b16409f0cde34fdeff4f
Rob Browning <rlb@cs.utexas.edu>
2001-12-04 23:23:02 +0000

    * src/report/utility-reports/view-column.scm: rename bootstrap to main.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6231 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/utility-reports/view-column.scm
+++ src/report/utility-reports/view-column.scm
@@ -27,7 +27,7 @@
 ;; don't have to worry about that here.
 
 (define-module (gnucash report view-column))
-(use-modules (gnucash bootstrap) (g-wrapped gw-gnc)) ;; FIXME: delete after we finish modularizing.
+(use-modules (gnucash main) (g-wrapped gw-gnc)) ;; FIXME: delete after we finish modularizing.
 (use-modules (ice-9 slib))
 (use-modules (gnucash gnc-module))
 
