fedac0fc5f21e3d4795d1e988db33a5a91affac7
Rob Browning <rlb@cs.utexas.edu>
2001-12-04 23:17:27 +0000

    * src/report/report-system/report.scm: rename bootstrap to main.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6219 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/report-system/report.scm
+++ src/report/report-system/report.scm
@@ -20,7 +20,7 @@
 ;; Boston, MA  02111-1307,  USA       gnu@gnu.org
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-modules (gnucash bootstrap))
+(use-modules (gnucash main))
 
 ;; This hash should contain all the reports available and will be used
 ;; to generate the reports menu whenever a new window opens and to
