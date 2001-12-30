562bf958ea38cbea0b401849a602f8979ece41a4
Linas Vepstas <linas@linas.org>
2001-12-30 15:29:41 +0000

    clean up the documentation
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6418 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/gnc-book.h
+++ src/engine/gnc-book.h
@@ -45,29 +45,43 @@
 GNCBook * gnc_book_new (void);
 void      gnc_book_destroy (GNCBook *book);
 
-const GUID * gnc_book_get_guid (GNCBook *book);
+/* 
+ * The gnc_book_get_guid() routine returns the GUID for this book.
+ * The gnc_book_get_slots() method will return the kvp data 
+ *    for the book 
+ *
+ * The gnc_book_get_group() returns the top-level group in the book.
+ * The gnc_book_get_pricedb() ditto 
+ * The gnc_book_get_commodity_table() ditto
+ * The gnc_book_get_schedxactions() returns the list of scheduled transactions.
+ * The gnc_book_get_template_group() ditto
+ */
+const GUID          * gnc_book_get_guid (GNCBook *book);
+kvp_frame           * gnc_book_get_slots (GNCBook *book);
+AccountGroup        * gnc_book_get_group (GNCBook *book);
+GNCPriceDB          * gnc_book_get_pricedb (GNCBook *book);
+gnc_commodity_table * gnc_book_get_commodity_table(GNCBook *book);
+GList               * gnc_book_get_schedxactions( GNCBook *book );
+AccountGroup        * gnc_book_get_template_group( GNCBook *book );
 
-/* The gnc_book_get_slots() method will return the kvp data 
- * for the book */
-kvp_frame * gnc_book_get_slots (GNCBook *book);
-
-/* Return the various top-level data structure pointers stored 
- * in the book.  */
-AccountGroup *gnc_book_get_group (GNCBook *book);
-GNCPriceDB   *gnc_book_get_pricedb (GNCBook *book);
-gnc_commodity_table* gnc_book_get_commodity_table(GNCBook *book);
-
-/**
- * Returns the list of scheduled transactions.
- **/
-GList * gnc_book_get_schedxactions( GNCBook *book );
-AccountGroup *gnc_book_get_template_group( GNCBook *book );
+/* The gnc_book_set_data() and gnc_book_get_data() routines allow
+ *    arbitrary pointers to structs to be stored in GNCBook.
+ *    This is the "prefered" method for extending GNCBook to hold
+ *    new data types.
+ */
+void gnc_book_set_data (GNCBook *book, const char *key, gpointer data);
+gpointer gnc_book_get_data (GNCBook *book, const char *key);
 
 /*
- * The gnc_book_not_saved() subroutine will return TRUE
- *    if any data in the book hasn't been saved to long-term storage.
+ * The gnc_book_not_saved() subroutine will return TRUE if any 
+ *    data in the book hasn't been saved to long-term storage.
+ *    (Actually, that's not quite true.  The book doesn't know 
+ *    anything about saving.  Its just that whenever data is modified,
+ *    the 'dirty' flag is set.  This routine returns the value of the 
+ *    'dirty' flag.  Its up to the backend to periodically reset this 
+ *    flag, when it acutally does save the data.)
  */
-gboolean gnc_book_not_saved(GNCBook *book);
+gboolean gnc_book_not_saved (GNCBook *book);
 
 /* The gnc_book_equal() method returns TRUE if the engine data
  * in the two given books is equal. */
@@ -76,9 +90,5 @@ gboolean gnc_book_equal (GNCBook *book_1, GNCBook *book_2);
 /* XXX FIXME count_transactions is a utility function, needs 
  * to be moved to some utility/support file.  */
 guint gnc_book_count_transactions(GNCBook *book);
-
-/* Store arbitrary pointers in the GNCBook for data storage extensibility */
-void gnc_book_set_data (GNCBook *book, const char *key, gpointer data);
-gpointer gnc_book_get_data (GNCBook *book, const char *key);
 
 #endif /* GNC_BOOK_H */
