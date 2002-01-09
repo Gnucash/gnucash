494aeb4dbce3d8f5875498ff36881cb81f807a3e
Rob Browning <rlb@cs.utexas.edu>
2002-01-09 21:48:36 +0000

    * src/gnc-module/test/mod-bar/bar-gwrap.scm: fix for new g-wrap.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6574 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnc-module/test/mod-bar/bar-gwrap.scm
+++ src/gnc-module/test/mod-bar/bar-gwrap.scm
@@ -1,17 +1,20 @@
 (use-modules (g-wrap))
-
 (debug-set! maxdepth 100000)
 (debug-set! stack 2000000)
 
-(let ((mod (gw:new-module "bar-gwrap")))
-  (gw:module-depends-on mod "gw-runtime")
-  (gw:module-set-declarations-ccodegen! 
-   mod 
-   (lambda (unused) 
-     (list "#include \"bar.h\"\n")))
+(use-modules (g-wrap gw-standard-spec))
+
+(let ((ws (gw:new-wrapset "bar-gwrap")))
+
+  (gw:wrapset-depends-on ws "gw-standard")
+
+  (gw:wrapset-add-cs-declarations! 
+   ws
+   (lambda (wrapset client-wrapset) 
+     "#include \"bar.h\"\n"))
 
   (gw:wrap-function
-   mod 'bar:hello
-   '<gw:bool> "bar_hello"
-   '()
+   ws
+   'bar:hello
+   '<gw:bool> "bar_hello" '()
    "Print a simple message from C"))
