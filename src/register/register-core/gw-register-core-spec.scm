ed5997aea50e70fdda349af0f468a81c28dc5762
Rob Browning <rlb@cs.utexas.edu>
2002-01-09 21:53:23 +0000

    * src/register/register-core/gw-register-core-spec.scm: fix for new g-wrap.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6586 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/register/register-core/gw-register-core-spec.scm
+++ src/register/register-core/gw-register-core-spec.scm
@@ -1,52 +1,33 @@
 (define-module (g-wrapped gw-register-core-spec))
 
-(use-modules (g-wrap))
-
 (debug-set! maxdepth 100000)
 (debug-set! stack    2000000)
 
-(let ((mod (gw:new-module "gw-register-core")))
-  (define (standard-c-call-gen result func-call-code)
-    (list (gw:result-get-c-name result) " = " func-call-code ";\n"))
+(use-modules (g-wrap))
+
+(use-modules (g-wrap gw-standard-spec))
+(use-modules (g-wrap gw-wct-spec))
+
+(let ((ws (gw:new-wrapset "gw-register-core")))
+
+  (gw:wrapset-depends-on ws "gw-standard")
+  (gw:wrapset-depends-on ws "gw-wct")
+
+  (gw:wrapset-set-guile-module! ws '(g-wrapped gw-register-core))
+
+  (gw:wrapset-add-cs-declarations!
+   ws
+   (lambda (wrapset client-wrapset) 
+     (if (eq? wrapset client-wrapset)
+         '()
+         "#include <basiccell.h>\n")))
   
-  (define (add-standard-result-handlers! type c->scm-converter)
-    (define (standard-pre-handler result)
-      (let* ((ret-type-name (gw:result-get-proper-c-type-name result))
-             (ret-var-name (gw:result-get-c-name result)))
-        (list "{\n"
-              "    " ret-type-name " " ret-var-name ";\n")))
-    
-    (gw:type-set-pre-call-result-ccodegen! type standard-pre-handler)
-    
-    (gw:type-set-post-call-result-ccodegen!
-     type
-     (lambda (result)
-       (let* ((scm-name (gw:result-get-scm-name result))
-              (c-name (gw:result-get-c-name result)))
-         (list
-          (c->scm-converter scm-name c-name)
-          "  }\n")))))
-  
-  (gw:module-depends-on mod "gw-runtime")
-
-  (gw:module-set-guile-module! mod '(g-wrapped gw-register-core))
-
-  (gw:module-set-declarations-ccodegen!
-   mod
-   (lambda (client-only?) 
-     (list 
-      "#include <basiccell.h>\n"
-      )))
-
-  (gw:wrap-non-native-type 
-   mod '<gnc:basic-cell>
-   "BasicCell*" "const BasicCell*")
+  (gw:wrap-as-wct ws '<gnc:basic-cell> "BasicCell*" "const BasicCell*")
 
   (gw:wrap-function
-   mod
+   ws
    'gnc:basic-cell-get-value
-   '(<gw:m-chars-callee-owned> gw:const)
+   '(<gw:mchars> callee-owned const)
    "gnc_basic_cell_get_value"
    '((<gnc:basic-cell> cell))
-   "Return the value of the cell.")
-  )
+   "Return the value of the cell."))
