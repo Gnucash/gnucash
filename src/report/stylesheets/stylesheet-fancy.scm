12cea663aa3bc4c3472c70c9d2fc2dc9f82b00d0
Dave Peticolas <dave@krondo.com>
2002-01-16 08:48:50 +0000

    right-align number cells (why was this changed at all)?
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6617 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/stylesheets/stylesheet-fancy.scm
+++ src/report/stylesheets/stylesheet-fancy.scm
@@ -197,7 +197,7 @@
     (gnc:html-document-set-style!
      ssdoc "number-cell"
      'tag "td"
-     'attribute (list "align" "left")
+     'attribute (list "align" "right")
      'attribute (list "nowrap"))
 
     (if (and bgpixmap
