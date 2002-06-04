8a185430e933fb4937a65550ba801d08d7192a34
Christian Stimming <stimming@tuhh.de>
2002-06-04 23:11:33 +0000

    2002-06-05  Christian Stimming  <stimming@tuhh.de>
    	* src/import-export/hbci/*: Add directory and initial files for
    	HBCI support module.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@6946 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/import-export/hbci/gncmod-hbci.c
@@ -0,0 +1,65 @@
+/*********************************************************************
+ * gncmod-tax-us.c
+ * module definition/initialization for us tax info 
+ * 
+ * Copyright (c) 2001 Linux Developers Group, Inc. 
+ *********************************************************************/
+
+#include <stdio.h>
+#include <guile/gh.h>
+#include <glib.h>
+
+#include "gnc-module.h"
+#include "gnc-module-api.h"
+
+/* version of the gnc module system interface we require */
+int libgncmod_hbci_LTX_gnc_module_system_interface = 0;
+
+/* module versioning uses libtool semantics. */
+int libgncmod_hbci_LTX_gnc_module_current  = 0;
+int libgncmod_hbci_LTX_gnc_module_revision = 0;
+int libgncmod_hbci_LTX_gnc_module_age      = 0;
+
+char *
+libgncmod_hbci_LTX_gnc_module_path(void) {
+  return g_strdup("gnucash/import-export/hbci");
+}
+
+char * 
+libgncmod_hbci_LTX_gnc_module_description(void) {
+  return g_strdup("Support for HBCI protocol");
+}
+
+static void
+lmod(char * mn) 
+{
+  char * form = g_strdup_printf("(use-modules %s)\n", mn);
+  gh_eval_str(form);
+  g_free(form);
+}
+
+int
+libgncmod_hbci_LTX_gnc_module_init(int refcount) {
+  /* load the engine (we depend on it) */
+  if(!gnc_module_load("gnucash/engine", 0)) {
+    return FALSE;
+  }
+
+  /* load the app-utils (we depend on it) */
+  if(!gnc_module_load("gnucash/app-utils", 0)) {
+    return FALSE;
+  }
+
+  /* load the HBCI Scheme code */
+  if(gh_eval_str("(use-modules (gnucash import-export hbci))") ==
+     SCM_BOOL_F) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+int
+libgncmod_hbci_LTX_gnc_module_end(int refcount) {
+  return TRUE;
+}
---
+++ src/import-export/hbci/hbci.scm
@@ -0,0 +1,6 @@
+(define-module (gnucash import-export hbci))
+
+;;(export gnc:txf-get-payer-name-source)
+;;(export txf-expense-categories)
+
+;;(load-from-path "txf.scm")
