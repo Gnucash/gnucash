873f0bf369120fd483c29fc9d027cddd3751cd67
Derek Atkins <derek@ihtfp.com>
2002-07-06 19:35:23 +0000

    * gw-kvp-spec.scm: wrap kvp_slot_set_slot_path_gslist(), 	  kvp_slot_get_slot_path_gslist(), and gnc_book_get_slots()
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@7089 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/gw-kvp-spec.scm
+++ src/engine/gw-kvp-spec.scm
@@ -28,7 +28,8 @@
      (list
       "#include <kvp_frame.h>\n"
       "#include <kvp-scm.h>\n"
-      "#include <Transaction.h>\n")))
+      "#include <Transaction.h>\n"
+      "#include <gnc-book.h>\n")))
 
   (gw:wrap-as-wct ws '<gnc:kvp-frame*> "kvp_frame*" "const kvp_frame*")
 
@@ -39,14 +40,6 @@
    '(c-var " = gnc_scm_to_kvp_value_ptr(" scm-var ");\n")
    '(scm-var " = gnc_kvp_value_ptr_to_scm(" c-var ");\n"))
   
-  (gw:wrap-function
-   ws
-   'gnc:transaction-get-slots
-   '<gnc:kvp-frame*>
-   "xaccTransGetSlots"
-   '((<gnc:Transaction*> s))
-   "Get the transaction's slots.")
-
   (gw:wrap-function
    ws
    'gnc:kvp-frame-set-slot
@@ -63,4 +56,44 @@
    '<gnc:kvp-value*>
    "kvp_frame_get_slot"
    '((<gnc:kvp-frame*> k) ((<gw:mchars> caller-owned const) c))
-   "Gets the slot c from frame k"))
+   "Gets the slot c from frame k")
+
+  (gw:wrap-function
+   ws
+   'gnc:kvp-frame-set-slot-path
+   '<gw:void>
+   "kvp_frame_set_slot_path_gslist"
+   '((<gnc:kvp-frame*> k) (<gnc:kvp-value*> v)
+     ((gw:glist-of (<gw:mchars> caller-owned const) caller-owned) key-path))
+   "Sets the path key-path in frame k to the value v")
+
+  (gw:wrap-function
+   ws
+   'gnc:kvp-frame-get-slot-path
+   '<gnc:kvp-value*>
+   "kvp_frame_get_slot_path_gslist"
+   '((<gnc:kvp-frame*> k)
+     ((gw:glist-of (<gw:mchars> caller-owned const) caller-owned) key-path))
+   "Gets the value at slots key-path in frame k")
+
+  ;;
+  ;; functions to get kvp-frames
+  ;;
+
+  (gw:wrap-function
+   ws
+   'gnc:transaction-get-slots
+   '<gnc:kvp-frame*>
+   "xaccTransGetSlots"
+   '((<gnc:Transaction*> s))
+   "Get the transaction's slots.")
+
+  (gw:wrap-function
+   ws
+   'gnc:book-get-slots
+   '<gnc:kvp-frame*>
+   "gnc_book_get_slots"
+   '((<gnc:Book*> b))
+   "Get the book's slots.")
+)
+
