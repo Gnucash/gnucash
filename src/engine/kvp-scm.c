1aa829972506044ff5501d49bdcfc450b10c66f2
Derek Atkins <derek@ihtfp.com>
2002-07-06 22:44:13 +0000

    * kvp-scm: implement KVP_TYPE_FRAME kvp-values
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@7091 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/kvp-scm.c
+++ src/engine/kvp-scm.c
@@ -52,9 +52,13 @@ gnc_scm_to_kvp_value_ptr(SCM val)
         free(newstr);
         return ret;
     }
+    else if(gw_wcp_p(val) &&
+	    gw_wcp_is_of_type_p(gh_eval_str("<gnc:kvp-frame*>"), val))
+    {
+        return gw_wcp_get_ptr(val);
+    }
     /* FIXME: add binary handler here when it's figured out */
     /* FIXME: add list handler here */
-    /* FIXME: add frame handler here when it's figured out */
     return NULL;
 }
 
@@ -85,12 +89,19 @@ gnc_kvp_value_ptr_to_scm(kvp_value* val)
         return gnc_timespec2timepair(kvp_value_get_timespec(val));
 	break;
 
+    case KVP_TYPE_FRAME:
+    {
+        kvp_frame *frame = kvp_value_get_frame(val);
+	if (frame)
+	  return gw_wcp_assimilate_ptr (frame,
+					gh_eval_str("<gnc:kvp-frame*>"));
+    }
+        break;
+
     /* FIXME: handle types below */
     case KVP_TYPE_BINARY:
         break;
     case KVP_TYPE_GLIST:
-        break;
-    case KVP_TYPE_FRAME:
         break;
     }
     return SCM_BOOL_F;
