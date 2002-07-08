69bbffed7a62c913aa7e76f2ad634f5ee7cb4eb2
Derek Atkins <derek@ihtfp.com>
2002-07-08 16:08:10 +0000

    Make the option-path available as a variable
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@7098 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnome-utils.scm
+++ src/gnome-utils/gnome-utils.scm
@@ -18,9 +18,12 @@
 (export gnc:new-menu-namer)
 (export gnc:*add-extension-hook*)
 
+(export gnc:*kvp-option-path*)
 (export gnc:kvp-option-dialog)
 
 (load-from-path "gnc-menu-extensions.scm")
+
+(define gnc:*kvp-option-path* '("options"))
 
 (define (gnc:kvp-option-dialog id-type slots title changed_cb)
   (let* ((options (gnc:make-kvp-options id-type))
@@ -28,13 +31,13 @@
 	 (optionwin (gnc:option-dialog-new #t title)))
 
     (define (apply-cb)
-      (gnc:options-scm->kvp options slots '("options") #t)
+      (gnc:options-scm->kvp options slots gnc:*kvp-option-path* #t)
       (if changed_cb (changed_cb)))
 
     (define (close-cb)
       (gnc:option-dialog-destroy optionwin)
       (gnc:option-db-destroy optiondb))
 
-    (gnc:options-kvp->scm options slots '("options"))
+    (gnc:options-kvp->scm options slots gnc:*kvp-option-path*)
     (gnc:option-dialog-set-callbacks optionwin apply-cb close-cb)
     (gnc:option-dialog-build-contents optionwin optiondb)))
