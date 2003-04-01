bdf876eadc9f96d98cabf2a8fc9cdbb9d4f467b0
Derek Atkins <derek@ihtfp.com>
2003-04-01 21:17:32 +0000

    * src/business/business-core/gncEntry.h:  Move the definitions 	  of PaymentType and DiscountHow higher earlier in the file 	  to prevent problems of circular dependencies. 	* src/business/business-core/gncInvoice.[ch]: 	  - Add the storage of the to_charge_amount. 	  - Add GetTotalOf() to find the total of Cash and Charge amounts 	  - Use the to_charge_amount when posting an invoice 	* src/business/business-core/dialog-invoice.c: 	  - add a to_charge_amount for Exp. Vouchers 	  - add a "Total Cash" and "Total Charge" summary 	* src/business/business-gnome/glade/invoice.glade: 	  - add the to_charge_amount frame and labels. 	* src/busines/business-core/file/gnc-invoice-xml-v2.c: Add support 	  for the to-charge amount.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/1.8@8154 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/business/business-core/gncInvoice.h
+++ src/business/business-core/gncInvoice.h
@@ -34,6 +34,7 @@ void gncInvoiceSetNotes (GncInvoice *invoice, const char *notes);
 void gncInvoiceSetCurrency (GncInvoice *invoice, gnc_commodity *currency);
 void gncInvoiceSetActive (GncInvoice *invoice, gboolean active);
 void gncInvoiceSetBillTo (GncInvoice *invoice, GncOwner *billto);
+void gncInvoiceSetToChargeAmount (GncInvoice *invoice, gnc_numeric amount);
 
 void gncInvoiceAddEntry (GncInvoice *invoice, GncEntry *entry);
 void gncInvoiceRemoveEntry (GncInvoice *invoice, GncEntry *entry);
@@ -57,6 +58,7 @@ const char * gncInvoiceGetNotes (GncInvoice *invoice);
 const char * gncInvoiceGetType (GncInvoice *invoice); 
 gnc_commodity * gncInvoiceGetCurrency (GncInvoice *invoice);
 GncOwner * gncInvoiceGetBillTo (GncInvoice *invoice);
+gnc_numeric gncInvoiceGetToChargeAmount (GncInvoice *invoice);
 gboolean gncInvoiceGetActive (GncInvoice *invoice);
 
 GNCLot * gncInvoiceGetPostedLot (GncInvoice *invoice);
@@ -65,6 +67,7 @@ Account * gncInvoiceGetPostedAcc (GncInvoice *invoice);
 
 /* return the "total" amount of the invoice */
 gnc_numeric gncInvoiceGetTotal (GncInvoice *invoice);
+gnc_numeric gncInvoiceGetTotalOf (GncInvoice *invoice, GncEntryPaymentType type);
 
 GList * gncInvoiceGetEntries (GncInvoice *invoice);
 
