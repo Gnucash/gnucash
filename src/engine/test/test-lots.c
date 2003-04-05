517270fd8c257abda3dca262a9fdbf35865c7d7d
Linas Vepstas <linas@linas.org>
2003-04-05 20:03:30 +0000

    add very simplistic test for lots
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@8197 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/engine/test/test-lots.c
@@ -0,0 +1,74 @@
+
+/* Test file created by Linas Vepstas <linas@linas.org>
+ * Minimal test to see if automatic lot scrubbing works.
+ * April 2003
+ * License: GPL
+ */
+
+#include <ctype.h>
+#include <glib.h>
+
+#include "Account.h"
+#include "Group.h"
+#include "Scrub2.h"
+#include "gnc-engine-util.h"
+#include "gnc-module.h"
+#include "test-stuff.h"
+#include "test-engine-stuff.h"
+#include "Transaction.h"
+
+
+
+static void
+run_test (void)
+{
+  GNCSession *sess;
+  GNCBook *book;
+  AccountGroup *grp;
+
+  if(!gnc_module_load("gnucash/engine", 0))
+  {
+    failure("couldn't load gnucash/engine");
+    exit(get_rv());
+  }
+
+  /* --------------------------------------------------------- */
+  /* In the first test, we will merely try to see if we can run
+   * without crashing.  We don't check to see if data is good. */
+  sess = get_random_session ();
+  book = gnc_session_get_book (sess);
+  if (!book)
+  {
+    failure("book not created");
+    exit(get_rv());
+  }
+
+  add_random_transactions_to_book (book, 720);
+
+  grp = gnc_book_get_group (book);
+  xaccGroupScrubLotsBalance (grp);
+
+  /* --------------------------------------------------------- */
+  /* In the second test, we create an account with unrealized gains,
+   * and see if that gets fixed correctly, with the correct balances,
+   * and etc.
+   * XXX not inplemented 
+   */
+  success ("automatic lot scrubbing lightly tested and seem to work");
+}
+
+static void
+main_helper (void *closure, int argc, char **argv)
+{
+  run_test ();
+
+  print_test_results();
+  exit(get_rv());
+}
+
+int
+main (int argc, char **argv)
+{
+  scm_boot_guile(argc, argv, main_helper, NULL);
+  return 0;
+}
