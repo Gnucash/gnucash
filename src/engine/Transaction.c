706477dc02189c4b8801c7d520b05ba4feb3b61a
Derek Atkins <derek@ihtfp.com>
2003-04-11 15:14:49 +0000

    * src/engine/Transaction.c: xaccSplitGetCorrAccountCode() should 	  actually return the account Code, not the account Name!  Fixes 	  #110561.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/1.8@8221 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/Transaction.c
+++ src/engine/Transaction.c
@@ -2404,7 +2404,7 @@ xaccSplitGetCorrAccountCode(const Split *sa)
   else
   {
     other_split_acc = xaccSplitGetAccount(other_split);
-    return xaccAccountGetName(other_split_acc);
+    return xaccAccountGetCode(other_split_acc);
   }
 }
 
