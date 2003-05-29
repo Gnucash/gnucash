698fce624f7bceb1f5bf895632f77dab15faedb7
unknown user <unknown@gnucash.org>
2003-05-29 16:42:13 +0000

    This commit was manufactured by cvs2svn to create branch '1.8'.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/1.8@8452 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/import-export/log-replay/gncmod-log-replay.c
@@ -0,0 +1,92 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
+/** @addtogroup Import_Export
+    @{ */
+ /**@internal
+ @file gncmod-log-replay.c
+ @brief module definition/initialization for the log replay module
+ @author Copyright (c) 2003 Benoit Grégoire bock@step.polymtl.ca
+ */
+#include "config.h"
+#include <glib.h>
+#include <libguile.h>
+#include "guile-mappings.h"
+
+#include "gnc-log-replay.h"
+#include "gnc-module.h"
+#include "gnc-module-api.h"
+
+/* version of the gnc module system interface we require */
+int libgncmod_log_replay_LTX_gnc_module_system_interface = 0;
+
+/* module versioning uses libtool semantics. */
+int libgncmod_log_replay_LTX_gnc_module_current  = 0;
+int libgncmod_log_replay_LTX_gnc_module_revision = 0;
+int libgncmod_log_replay_LTX_gnc_module_age      = 0;
+
+//static GNCModule bus_core;
+//static GNCModule file;
+
+/* forward references */
+char *libgncmod_log_replay_LTX_gnc_module_path(void);
+char *libgncmod_log_replay_LTX_gnc_module_description(void);
+int libgncmod_log_replay_LTX_gnc_module_init(int refcount);
+int libgncmod_log_replay_LTX_gnc_module_end(int refcount);
+
+
+char *
+libgncmod_log_replay_LTX_gnc_module_path(void)
+{
+  return g_strdup("gnucash/import-export/log-replay");
+}
+char *
+libgncmod_log_replay_LTX_gnc_module_description(void)
+{
+  return g_strdup("C code for log file replay");
+}
+int
+libgncmod_log_replay_LTX_gnc_module_init(int refcount)
+{
+  if(!gnc_module_load("gnucash/engine", 0))
+  {
+    return FALSE;
+  }
+  if(!gnc_module_load("gnucash/app-utils", 0))
+  {
+    return FALSE;
+  }
+  if(!gnc_module_load("gnucash/gnome-utils", 0))
+  {
+    return FALSE;
+  }
+    if(!gnc_module_load("gnucash/import-export", 0))
+  {
+    return FALSE;
+  }
+    scm_c_eval_string("(load-from-path \"log-replay/log-replay.scm\")");
+    scm_c_define_gsubr("gnc:log-replay", 0, 0, 0, scm_gnc_file_log_replay);
+  return TRUE;
+}
+
+int
+libgncmod_log_replay_LTX_gnc_module_end(int refcount)
+{
+  return TRUE;
+}
+/** @}*/
