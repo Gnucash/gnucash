e43973c77a7eedb68890c9c2f321909be1da3299
Linas Vepstas <linas@linas.org>
2003-06-09 21:23:44 +0000

    Add GPL license notice
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@8514 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/gncObject.h
+++ src/engine/gncObject.h
@@ -1,7 +1,29 @@
-/*
- * gncObject.h -- the Core Object Registration/Lookup Interface
- * Copyright (C) 2001,2002 Derek Atkins
- * Author: Derek Atkins <warlord@MIT.EDU>
+/********************************************************************\
+ * gncObject.h -- the Core Object Registration/Lookup Interface     *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+/** @addtogroup Engine
+    @{ */
+/** @file gncObject.h
+ * @breif the Core Object Registration/Lookup Interface
+ *
+ * @author Copyright (c) 2001,2002, Derek Atkins <warlord@MIT.EDU>
  */
 
 #ifndef GNC_OBJECT_H_
@@ -85,3 +107,4 @@ void gncObjectForeachBackend (const char *backend_name,
 			      gpointer user_data);
 
 #endif /* GNC_OBJECT_H_ */
+/** @} */
--- src/engine/gncObjectP.h
+++ src/engine/gncObjectP.h
@@ -1,7 +1,29 @@
-/*
- * gncObjectP.h -- the Core Object Registration Interface
- * Copyright (C) 2001, 2002 Derek Atkins
- * Author: Derek Atkins <warlord@MIT.EDU>
+/********************************************************************\
+ * gncObjectP.h -- the Core Object Registration/Lookup Interface    *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+/** @addtogroup Engine
+    @{ */
+/** @file gncObject.h
+ * @breif the Core Object Registration/Lookup Private Interface
+ *
+ * @author Copyright (c) 2001,2002, Derek Atkins <warlord@MIT.EDU>
  */
 
 #ifndef GNC_OBJECTP_H_
@@ -10,9 +32,11 @@
 #include "gncObject.h"
 
 /* Initialize the object registration subsystem */
+/* XXX A better name would be 'ObjectClassInitialize ... */
 void gncObjectInitialize (void);
 void gncObjectShutdown (void);
 
+/* Note that the following are per-class and not per-instance */
 /* To be called from within the book */
 void gncObjectBookBegin (GNCBook *book);
 void gncObjectBookEnd (GNCBook *book);
@@ -21,3 +45,4 @@ gboolean gncObjectIsDirty (GNCBook *book);
 void gncObjectMarkClean (GNCBook *book);
 
 #endif /* GNC_OBJECTP_H_ */
+/** @} */
