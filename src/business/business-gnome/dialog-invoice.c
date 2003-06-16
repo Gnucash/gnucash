a3b22eb924f7299aecf07a1c610eeb8947315c67
Derek Atkins <derek@ihtfp.com>
2003-06-16 02:24:54 +0000

    * src/business/business-gnome/dialog-invoice.c: change how the 	  billterms option menu is used so it will change properly if 	  you change the invoice's owner.  Fixes #115290.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@8617 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/business/business-gnome/dialog-invoice.c
+++ src/business/business-gnome/dialog-invoice.c
@@ -1321,7 +1321,7 @@ gnc_invoice_owner_changed_cb (GtkWidget *widget, gpointer data)
 
   /* XXX: I'm not sure -- should we change the terms if this happens? */
   iw->terms = term;
-  gnc_ui_billterms_optionmenu (iw->terms_menu, iw->book, TRUE, &iw->terms);
+  gnc_ui_optionmenu_set_value (iw->terms_menu, iw->terms);
 
   gnc_invoice_update_job_choice (iw);
 
@@ -1653,7 +1653,7 @@ gnc_invoice_update_window (InvoiceWindow *iw)
 
     /* fill in the terms menu */
     iw->terms = gncInvoiceGetTerms (invoice);
-    gnc_ui_billterms_optionmenu (iw->terms_menu, iw->book, TRUE, &iw->terms);
+    gnc_ui_optionmenu_set_value (iw->terms_menu, iw->terms);
 
     /*
      * Next, figure out if we've been posted, and if so set the
@@ -2092,6 +2092,7 @@ gnc_invoice_new_window (GNCBook *bookp, InvoiceDialogType type,
   gtk_widget_show_all (iw->dialog);
 
   /* Now fill in a lot of the pieces and display properly */
+  gnc_ui_billterms_optionmenu (iw->terms_menu, iw->book, TRUE, &iw->terms);
   gnc_invoice_update_window (iw);
 
   gnc_table_refresh_gui (gnc_entry_ledger_get_table (iw->ledger), TRUE);
@@ -2211,6 +2212,7 @@ gnc_invoice_window_new_invoice (GNCBook *bookp, GncOwner *owner,
 				       GNC_EVENT_MODIFY | GNC_EVENT_DESTROY);
 
   /* Now fill in a lot of the pieces and display properly */
+  gnc_ui_billterms_optionmenu (iw->terms_menu, iw->book, TRUE, &iw->terms);
   gnc_invoice_update_window (iw);
   gnc_table_refresh_gui (gnc_entry_ledger_get_table (iw->ledger), TRUE);
 
