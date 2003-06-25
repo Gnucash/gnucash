1f031da3a0045e8d5520ab4f147cee2d10f91ea4
Linas Vepstas <linas@linas.org>
2003-06-25 06:24:06 +0000

    start translating QueryNew
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/qof-port-5@8714 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/xlate.pl
+++ src/engine/xlate.pl
@@ -76,19 +76,76 @@ foreach (@files)
 		# s/CHAR_MATCH_ANY/QOF_CHAR_MATCH_ANY/g;
 		# s/CHAR_MATCH_NONE/QOF_CHAR_MATCH_NONE/g;
 
-		s/char_match_t/QofCharMatch/g;
-		s/guid_match_t/QofGuidMatch/g;
-		s/numeric_match_t/QofNumericMatch/g;
-		s/date_match_t/QofDateMatch/g;
-		s/string_match_t/QofStringMatch/g;
-		s/query_compare_t/QofQueryCompare/g;
+		# s/char_match_t/QofCharMatch/g;
+		# s/guid_match_t/QofGuidMatch/g;
+		# s/numeric_match_t/QofNumericMatch/g;
+		# s/date_match_t/QofDateMatch/g;
+		# s/string_match_t/QofStringMatch/g;
+		# s/query_compare_t/QofQueryCompare/g;
 
-		s/gncQueryCoreInit/qof_query_core_init/g;
-		s/gncQueryCoreShutdown/qof_query_core_shutdown/g;
-		s/gncQueryCoreGetPredicate/qof_query_core_get_predicate/g;
-		s/gncQueryCoreGetCompare/qof_query_core_get_compare/g;
+		# s/gncQueryCoreInit/qof_query_core_init/g;
+		# s/gncQueryCoreShutdown/qof_query_core_shutdown/g;
+		# s/gncQueryCoreGetPredicate/qof_query_core_get_predicate/g;
+		# s/gncQueryCoreGetCompare/qof_query_core_get_compare/g;
 		
-		s/gncQueryCorePredicateEqual/qof_query_core_predicate_equal/g;
+		# s/gncQueryCorePredicateEqual/qof_query_core_predicate_equal/g;
+
+		s/QUERY_AND/QOF_QUERY_AND/g;
+		s/QUERY_OR/QOF_QUERY_OR/g;
+		s/QUERY_NAND/QOF_QUERY_NAND/g;
+		s/QUERY_NOR/QOF_QUERY_NOR/g;
+		s/QUERY_XOR/QOF_QUERY_XOR/g;
+		s/QUERY_PARAM_BOOK/QOF_QUERY_PARAM_BOOK/g;
+		s/QUERY_PARAM_GUID/QOF_QUERY_PARAM_GUID/g;
+		s/QUERY_PARAM_ACTIVE/QOF_QUERY_PARAM_ACTIVE/g;
+
+		s/querynew_s/_QofQuery/g;
+		s/QueryNew/QofQuery/g;
+		s/QueryOp/QofQueryOp/g;
+		s/query_new_term/_QofQueryTerm/g;
+		s/query_new_sort/_QofQuerySort/g;
+
+		s/gncQueryBuildParamList/qof_query_build_param_list/g;
+		s/gncQueryCreate/qof_query_create/g;
+		s/gncQueryCreateFor/qof_query_create_for/g;
+		s/gncQueryDestroy/qof_query_destroy/g;
+		s/gncQuerySearchFor/qof_query_search_for/g;
+		s/gncQuerySetBook/qof_query_set_book/g;
+		s/gncQueryAddTerm/qof_query_add_term/g;
+		s/gncQueryAddGUIDMatch/qof_query_add_guid_match/g;
+		s/gncQueryAddGUIDListMatch/qof_query_AddGUIDListMatch/g;
+		s/gncQueryAddBooleanMatch/qof_query_AddBooleanMatch/g;
+		s/gncQueryRun/qof_query_Run/g;
+		s/gncQueryLastRun/qof_query_LastRun/g;
+		s/gncQueryClear/qof_query_Clear/g;
+		s/gncQueryPurgeTerms/qof_query_PurgeTerms/g;
+		s/gncQueryHasTerms/qof_query_HasTerms/g;
+		s/gncQueryNumTerms/qof_query_NumTerms/g;
+		s/gncQueryHasTermType/qof_query_HasTermType/g;
+		s/gncQueryCopy/qof_query_Copy/g;
+		s/gncQueryInvert/qof_query_Invert/g;
+		s/gncQueryMerge/qof_query_Merge/g;
+		s/gncQueryMergeInPlace/qof_query_MergeInPlace/g;
+		s/gncQuerySetSortOrder/qof_query_SetSortOrder/g;
+		s/gncQuerySetSortOptions/qof_query_SetSortOptions/g;
+		s/gncQuerySetSortIncreasing/qof_query_SetSortIncreasing/g;
+		s/gncQuerySetMaxResults/qof_query_SetMaxResults/g;
+		s/gncQueryEqual/qof_query_Equal/g;
+		s/gncQueryPrint/qof_query_Print/g;
+		s/gncQueryGetSearchFor/qof_query_GetSearchFor/g;
+		s/gncQueryGetBooks/qof_query_GetBooks/g;
+
+		s/gncQueryNewInit/qof_query_NewInit/g;
+		s/gncQueryNewShutdown/qof_query_NewShutdown/g;
+		s/gncQueryGetMaxResults/qof_query_GetMaxResults/g;
+		s/gncQueryGetTerms/qof_query_GetTerms/g;
+		s/gncQueryTermGetParamPath/qof_query_TermGetParamPath/g;
+		s/gncQueryTermGetPredData/qof_query_TermGetPredData/g;
+		s/gncQueryTermIsInverted/qof_query_TermIsInverted/g;
+		s/gncQueryGetSorts/qof_query_GetSorts/g;
+		s/gncQuerySortGetParamPath/qof_query_SortGetParamPath/g;
+		s/gncQuerySortGetSortOptions/qof_query_SortGetSortOptions/g;
+		s/gncQuerySortGetIncreasing/qof_query_SortGetIncreasing/g;
 		
 		print OF $_;
 	}
