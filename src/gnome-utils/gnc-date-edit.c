7382e6c5310b838bb63554bcf37030bf92e71f68
Linas Vepstas <linas@linas.org>
2003-06-30 04:33:59 +0000

    fixup date printing
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@8804 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnc-date-edit.c
+++ src/gnome-utils/gnc-date-edit.c
@@ -144,7 +144,7 @@ day_selected (GtkCalendar *calendar, GNCDateEdit *gde)
 
 	gtk_calendar_get_date (calendar, &year, &month, &day);
 
-        printDate (buffer, day, month + 1, year);
+	qof_print_date_buff (buffer, 40, day, month + 1, year);
 	gtk_entry_set_text (GTK_ENTRY (gde->date_entry), buffer);
 	gtk_signal_emit (GTK_OBJECT (gde), date_edit_signals [DATE_CHANGED]);
 }
@@ -481,7 +481,7 @@ gnc_date_edit_set_time (GNCDateEdit *gde, time_t the_time)
 	mytm = localtime (&the_time);
 
 	/* Set the date */
-        printDate (buffer,
+	qof_print_date_buff (buffer, 40,
                    mytm->tm_mday,
                    mytm->tm_mon + 1,
                    1900 + mytm->tm_year);
