077b3efb43f99bf6a4c752d8e35228510f814ade
unknown user <unknown@gnucash.org>
2003-08-11 05:42:31 +0000

    This commit was manufactured by cvs2svn to create branch 'gnucash-gnome2-dev'.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/gnucash-gnome2-dev@9053 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/backend/file/gnc-backend-file.h
@@ -0,0 +1,60 @@
+/********************************************************************
+ * gnc-backend-file.h: load and save data to files                  *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
+/** @file gnc-backend-file.h
+ *  @breif load and save data to files 
+ *  @author Copyright (c) 2000 Gnumatic Inc.
+ *  @author Copyright (c) 2002 Derek Atkins <warlord@MIT.EDU>
+ *  @author Copyright (c) 2003 Linas Vepstas <linas@linas.org>
+ *
+ * This file implements the top-level QofBackend API for saving/
+ * restoring data to/from an ordinary Unix filesystem file.
+ */
+
+#ifndef GNC_BACKEND_FILE_H_
+#define GNC_BACKEND_FILE_H_
+
+#include "qofbackend.h"
+#include "qofbackend-p.h"
+
+struct FileBackend_struct
+{
+    QofBackend be;
+
+    char *dirname;
+    char *fullpath;  /* Fully qualified path to book */
+    char *lockfile;
+    char *linkfile;
+    int lockfd;
+
+    QofBook *primary_book;  /* The primary, main open book */
+};
+
+typedef struct FileBackend_struct FileBackend;
+
+typedef enum 
+{
+    GNC_BOOK_NOT_OURS,
+    GNC_BOOK_BIN_FILE,
+    GNC_BOOK_XML1_FILE,
+    GNC_BOOK_XML2_FILE,
+} QofBookFileType;
+
+#endif /* GNC_BACKEND_FILE_H_ */
