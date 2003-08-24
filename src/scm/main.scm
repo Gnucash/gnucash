5ea9106d33bbb7130ef5c005c01d360206f9e74a
David Hampton <hampton@employees.org>
2003-08-24 01:41:44 +0000

    Tweak next version number.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/gnucash-gnome2-dev@9151 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/main.scm
+++ src/scm/main.scm
@@ -356,7 +356,7 @@ string and 'directories' must be a list of strings."
      (_ "Report bugs and other problems to gnucash-devel@gnucash.org.\n")
      (_ "You can also lookup and file bug reports at http://bugzilla.gnome.org\n")
      (_ "The last stable version was ") "GnuCash 1.8.4" "\n"
-     (_ "The next stable version will be ") "GnuCash 1.10 or 2.0"
+     (_ "The next stable version will be ") "GnuCash 2.0"
      "\n\n"))))
 
 (define (gnc:startup-pass-1)
