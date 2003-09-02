8de8dee8aefa1acd0c6983f23303922044d9abab
Linas Vepstas <linas@linas.org>
2003-09-02 03:26:04 +0000

    micro tweak
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@9211 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/druid-stock-split.c
+++ src/gnome/druid-stock-split.c
@@ -389,7 +389,7 @@ stock_split_finish (GnomeDruidPage *druidpage,
   xaccTransSetCurrency (trans, gnc_default_currency ());
 
   date = gnc_date_edit_get_date (GNC_DATE_EDIT (info->date_edit));
-  xaccTransSetDateSecs (trans, date);
+  xaccTransSetDatePostedSecs (trans, date);
 
   {
     const char *description;
