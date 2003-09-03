84a1db4fa8090809c16582cf287805650a177376
David Hampton <hampton@employees.org>
2003-09-03 00:17:06 +0000

    Add GPL boilerplate with Jan's approval.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/gnucash-gnome2-dev@9221 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnc-tree-model-example-account.h
+++ src/gnome-utils/gnc-tree-model-example-account.h
@@ -1,7 +1,26 @@
 /* 
- * gnc-tree-model-example-account.h -- GtkTreeModel implementation to display examnple accounts in a GtkTreeView.
+ * gnc-tree-model-example-account.h -- GtkTreeModel implementation to
+ *	display examnple accounts in a GtkTreeView.
+ *
  * Copyright (C) 2003 Jan Arne Petersen
  * Author: Jan Arne Petersen <jpetersen@uni-bonn.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact:
+ *
+ * Free Software Foundation           Voice:  +1-617-542-5942
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org
  */
 
 #ifndef __GNC_TREE_MODEL_EXAMPLE_ACCOUNT_H
