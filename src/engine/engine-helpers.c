3331bbe954b4e6c0e793de86924c36b004f7a015
Derek Atkins <derek@ihtfp.com>
2003-09-10 00:01:54 +0000

    * src/engine/engine-helpers.c:  turn off scheme garbage collection 	  during query->scm and scm->query conversion.  It shaves about 50% 	  off the test-scm-query-string tests. 	* src/engine/test-core/test-engine-stuff.c:  add some code to help 	  track down what the get_random_query() is doing.  Turned off by 	  default. 	* src/app-utils/test/test-scm-query-string.c: make the test 	  deterministic by seeding the RNG with a known value.  There is a 	  known "delay" at #245 with a seed of 1 on Linux systems.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@9273 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/engine-helpers.c
+++ src/engine/engine-helpers.c
@@ -1706,6 +1706,8 @@ gnc_query2scm (QofQuery *q)
 
   if (!q) return SCM_BOOL_F;
 
+  ++scm_block_gc;
+
   /* terms */
   pair = scm_cons (gnc_query_terms2scm (qof_query_get_terms (q)), SCM_EOL);
   pair = scm_cons (scm_str2symbol ("terms"), pair);
@@ -1741,6 +1743,7 @@ gnc_query2scm (QofQuery *q)
 
   /* Reverse this list; tag it as 'query-v2' */
   pair = scm_reverse (query_scm);
+  --scm_block_gc;
   return scm_cons (scm_str2symbol ("query-v2"), pair);
 }
 
@@ -1964,6 +1967,8 @@ gnc_scm2query_v2 (SCM query_scm)
   gboolean si1 = TRUE, si2 = TRUE, si3 = TRUE;
   int max_results = -1;
 
+  ++scm_block_gc;
+
   while (!SCM_NULLP (query_scm))
   {
     char *symbol;
@@ -2050,6 +2055,8 @@ gnc_scm2query_v2 (SCM query_scm)
 
     free (symbol);
   }
+
+  --scm_block_gc;
 
   if (ok && search_for) {
     qof_query_search_for (q, search_for);
