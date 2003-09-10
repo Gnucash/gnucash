300a5c1b044c09c260046991717acb0b3f68eb29
Linas Vepstas <linas@linas.org>
2003-09-10 13:54:24 +0000

    flesh things out
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/auto-gains4@9276 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/Scrub3.c
+++ src/engine/Scrub3.c
@@ -1,14 +1,50 @@
+/********************************************************************\
+ * Scrub3.c -- Constrain Cap Gains to Track Sources of Gains        *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+\********************************************************************/
 
-/* If a split has been pulled apart to make it fit into two (or more)
- * lots, then it becomes theoretically possible for each subsplit to
- * have a distinct price.  But this would be wrong: each subsplit should
- * have the same price, within rounding errors.  This routine will
- * examine the indicated split for sub-splits, and adjust the value
- * of each so that they all have the same price.
+/*  @file Scrub3.c
+ *  @breif Constrain Cap Gains to Track Sources of Gains
+ *  @author Created by Linas Vepstas Sept 2003
+ *  @author Copyright (c) 2003 Linas Vepstas <linas@linas.org>
+ *
+ * Provides a set of functions and utilities for checking and
+ * repairing ('scrubbing clean') the usage of Cap Gains
+ * transactions in stock and commodity accounts.  
  */
 
+#include "config.h"
+
+#include <glib.h>
+
+#include "gnc-commodity.h"
+#include "gnc-engine.h"
+#include "gnc-numeric.h"
+#include "gnc-trace.h"
 #include "kvp_frame.h"
+#include "Scrub3.h"
+#include "Transaction.h"
 #include "TransactionP.h"
+
+// static short module = MOD_LOT;
+
+/* ========================================================== */
 
 void
 xaccScrubSubSplitPrice (Split *split)
@@ -21,7 +57,7 @@ xaccScrubSubSplitPrice (Split *split)
    if (!split) return;
    g_return_if_fail (split->parent);
 
-   /* If there are no sub-splits, ten there's nothing to do. */
+   /* If there are no sub-splits, then there's nothing to do. */
    kval = kvp_frame_get_value (split->kvp_data, "lot-split");
    if (!kval) return;  
 
@@ -32,16 +68,46 @@ xaccScrubSubSplitPrice (Split *split)
    src_amt = xaccSplitGetAmount (split);
    src_val = xaccSplitGetValue (split);
 
+   /* Loop over splits, adjust each so that it has the same
+    * ratio (i.e. price).  Change the value to get things 
+    * right; do not change the amount */
    for (node=split->parent->splits; node; node=node->next)
    {
       Split *s = node->data;
+      Transaction *txn = s->parent;
       gnc_numeric dst_amt, dst_value, target_val;
+      gnc_numeric delta;
+      int scu;
+
+      /* Skip the reference split */
+      if (s == split) continue;
+
+      scu = gnc_commodity_get_fraction (txn->common_currency);
 
       dst_amt = xaccSplitGetAmount (s);
       dst_val = xaccSplitGetValue (s);
       target_val = gnc_numeric_mul (dst_amt, src_val,
                         GNC_DENOM_AUTO, GNC_DENOM_REDUCE);
       target_val = gnc_numeric_div (target_val, src_amt,
-                        gnc_numeric_denom(dst_val), GNC_DENOM_EXACT);
+                        scu, GNC_DENOM_EXACT);
+
+      /* If the required price changes are 'small', do nothing.
+       * That is a case that the user will have to deal with
+       * manually.  This routine is really intended only for
+       * a gross level of synchronization.
+       */
+      delta = gnc_numeric_sub_fixed (target_val, dst_val);
+      delta = gnc_numeric_abs (delta);
+      if (3 * delta.num  < delta.denom) continue;
+
+      /* If the amount is small, pass on that too */
+      if ((-2 < dst_amt.num) && (dst_amt.num < 2)) continue;
+
+      /* Make the actual adjustment */
+      xaccTransBeginEdit (txn);
+      xaccSplitSetValue (s, target_val);
+      xaccTransCommitEdit (txn);
    }
 }
+
+/* ========================== END OF FILE  ========================= */
