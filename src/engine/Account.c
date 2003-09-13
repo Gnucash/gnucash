0cbaa7ba47f55440fe6fd0f8496701f3bb0388e9
Linas Vepstas <linas@linas.org>
2003-09-13 19:29:45 +0000

    remove obsolete kvp subroutine usage
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/auto-gains5@9295 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/Account.c
+++ src/engine/Account.c
@@ -2190,17 +2190,10 @@ xaccAccountGetTaxUSCode (Account *account)
 void
 xaccAccountSetTaxUSCode (Account *account, const char *code)
 {
-  KvpFrame *frame;
-
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-
-  frame = kvp_frame_get_frame (account->kvp_data, "tax-US", NULL);
-
-  kvp_frame_set_slot_nc (frame, "code",
-                         code ? kvp_value_new_string (code) : NULL);
+  kvp_frame_set_str (account->kvp_data, "/tax-US/code", code);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2210,33 +2203,17 @@ xaccAccountSetTaxUSCode (Account *account, const char *code)
 const char *
 xaccAccountGetTaxUSPayerNameSource (Account *account)
 {
-  KvpValue *value;
-
-  if (!account)
-    return FALSE;
-
-  value = kvp_frame_get_slot_path (account->kvp_data,
-                                   "tax-US", "payer-name-source", NULL);
-  if (!value)
-    return NULL;
-
-  return kvp_value_get_string (value);
+  if (!account) return NULL;
+  return kvp_frame_get_string (account->kvp_data, "/tax-US/payer-name-source");
 }
 
 void
 xaccAccountSetTaxUSPayerNameSource (Account *account, const char *source)
 {
-  KvpFrame *frame;
-
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-
-  frame = kvp_frame_get_frame (account->kvp_data, "tax-US", NULL);
-
-  kvp_frame_set_slot_nc (frame, "payer-name-source",
-                         source ? kvp_value_new_string (source) : NULL);
+  kvp_frame_set_str (account->kvp_data, "/tax-US/payer-name-source", source);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2539,14 +2516,11 @@ xaccAccountGetReconcileLastDate (Account *account, time_t *last_date)
 void
 xaccAccountSetReconcileLastDate (Account *account, time_t last_date)
 {
-  KvpFrame *frame;
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-  frame = kvp_frame_get_frame (account->kvp_data, "reconcile-info", NULL);
-  kvp_frame_set_slot_nc (frame, "last-date", 
-                               kvp_value_new_gint64 (last_date));
+  kvp_frame_set_gint64 (account->kvp_data, 
+                 "/reconcile-info/last-date", last_date);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2586,16 +2560,15 @@ void
 xaccAccountSetReconcileLastInterval (Account *account, int months, int days)
 {
   KvpFrame *frame;
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-  frame = kvp_frame_get_frame (account->kvp_data, "reconcile-info",
-			       "last-interval", NULL);
-  kvp_frame_set_slot_nc (frame, "months", 
-                               kvp_value_new_gint64 (months));
-  kvp_frame_set_slot_nc (frame, "days", 
-                               kvp_value_new_gint64 (days));
+
+  frame = kvp_frame_get_frame (account->kvp_data, 
+         "/reconcile-info/last-interval");
+
+  kvp_frame_set_gint64 (frame, "months", months);
+  kvp_frame_set_gint64 (frame, "days", days);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2637,16 +2610,13 @@ void
 xaccAccountSetReconcilePostponeDate (Account *account,
                                      time_t postpone_date)
 {
-  KvpFrame *frame;
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-  frame = kvp_frame_get_frame (account->kvp_data, 
-                         "reconcile-info", "postpone", NULL);
 
-  kvp_frame_set_slot_nc (frame, "date", 
-                         kvp_value_new_gint64 (postpone_date));
+  /* XXX this should be using timespecs, not gints !! */
+  kvp_frame_set_gint64 (account->kvp_data,
+            "/reconcile-info/postpone/date", postpone_date);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2689,16 +2659,11 @@ void
 xaccAccountSetReconcilePostponeBalance (Account *account,
                                         gnc_numeric balance)
 {
-  KvpFrame *frame;
-  if (!account)
-    return;
+  if (!account) return;
 
   xaccAccountBeginEdit (account);
-  frame = kvp_frame_get_frame (account->kvp_data, 
-                         "reconcile-info", "postpone", NULL);
-
-  kvp_frame_set_slot_nc (frame, "balance", 
-                         kvp_value_new_gnc_numeric (balance));
+  kvp_frame_set_gnc_numeric (account->kvp_data,
+           "/reconcile-info/postpone/balance", balance);
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2763,18 +2728,15 @@ xaccAccountGetAutoInterestXfer (Account *account, gboolean default_value)
 void
 xaccAccountSetAutoInterestXfer (Account *account, gboolean option)
 {
-  KvpFrame *frame;
   if (!account)
     return;
 
   xaccAccountBeginEdit (account);
-  frame = kvp_frame_get_frame (account->kvp_data, 
-                         "reconcile-info", NULL);
 
   /* FIXME: need KVP_TYPE_BOOLEAN for this someday */
-
-  kvp_frame_set_slot_nc (frame, "auto-interest-transfer", 
-                         kvp_value_new_string (option ? "true" : "false"));
+  kvp_frame_set_str (account->kvp_data,
+       "/reconcile-info/auto-interest-transfer",
+       (option ? "true" : "false"));
 
   mark_account (account);
   account->core_dirty = TRUE;
@@ -2904,19 +2866,16 @@ dxaccAccountGetQuoteTZ(Account *acc)
 void
 xaccAccountSetReconcileChildrenStatus(Account *account, gboolean status)
 { 
-  KvpFrame *frame;
-  if (!account)
-    return;
+  if (!account) return;
   
   xaccAccountBeginEdit (account);
   
-  frame = kvp_frame_get_frame (account->kvp_data, "reconcile-info", NULL);
-  kvp_frame_set_slot_nc (frame,
-                         "include-children",
-                         status ? kvp_value_new_gint64 (status) : NULL);
+  /* XXX FIXME: someday this should use KVP_TYPE_BOOLEAN */
+  kvp_frame_set_gint64 (account->kvp_data, 
+        "/reconcile-info/include-children", status);
+
   account->core_dirty = TRUE;
   xaccAccountCommitEdit (account);
-  return;
 }
 
 /********************************************************************\
