3cdf102f93f9878aae6a99447486959283e943bd
Linas Vepstas <linas@linas.org>
2003-10-17 04:40:59 +0000

    missed converting employee in prior cycle
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@9534 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/business/business-core/gncEmployee.c
+++ src/business/business-core/gncEmployee.c
@@ -1,5 +1,26 @@
+/********************************************************************\
+ * gncEmployee.c -- the Core Employee Interface                     *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
 /*
- * gncEmployee.c -- the Core Employee Interface
  * Copyright (C) 2001,2002 Derek Atkins
  * Author: Derek Atkins <warlord@MIT.EDU>
  */
@@ -150,14 +171,6 @@ void gncEmployeeSetLanguage (GncEmployee *employee, const char *language)
   SET_STR(employee, employee->language, language);
   mark_employee (employee);
   gncEmployeeCommitEdit (employee);
-}
-
-void gncEmployeeSetGUID (GncEmployee *employee, const GUID *guid)
-{
-  if (!employee || !guid) return;
-  if (guid_equal (guid, &employee->inst.entity.guid)) return;
-  
-  qof_entity_set_guid (&employee->inst.entity, guid);
 }
 
 void gncEmployeeSetAcl (GncEmployee *employee, const char *acl)
--- src/business/business-core/gncEmployeeP.h
+++ src/business/business-core/gncEmployeeP.h
@@ -1,5 +1,26 @@
-/*
+/********************************************************************\
  * gncEmployeeP.h -- the Core Employee Interface
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/*
  * Copyright (C) 2001 Derek Atkins
  * Author: Derek Atkins <warlord@MIT.EDU>
  */
@@ -8,9 +29,11 @@
 #define GNC_EMPLOYEEP_H_
 
 #include "gncEmployee.h"
+#inlcude "qofid-p.h"
 
 gboolean gncEmployeeRegister (void);
 gint64 gncEmployeeNextID (QofBook *book);
-void gncEmployeeSetGUID (GncEmployee *employee, const GUID *guid);
+
+#define gncEmployeeSetGUID(E,G) qof_entity_set_guid(QOF_ENTITY(E),(G))
 
 #endif /* GNC_EMPLOYEEP_H_ */
