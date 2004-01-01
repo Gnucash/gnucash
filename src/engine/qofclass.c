b2107d0e637f6d9eab5e82c79c3f47525428e808
unknown user <unknown@gnucash.org>
2004-01-01 18:24:31 +0000

    This commit was manufactured by cvs2svn to create branch 'gnucash-gnome2-dev'.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/gnucash-gnome2-dev@9743 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/engine/qofclass.c
@@ -0,0 +1,167 @@
+/********************************************************************\
+ * qofclass.c -- provide QOF paramterized data objects              *
+ * Copyright (C) 2002 Derek Atkins <warlord@MIT.EDU>                *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+#include "config.h"
+
+#include <glib.h>
+
+#include "gnc-engine-util.h"
+#include "gnc-trace.h"
+#include "qofclass.h"
+#include "qofclass-p.h"
+#include "qofquery.h"
+
+static short module = MOD_QUERY;
+
+static GHashTable *paramTable = NULL;
+static GHashTable *sortTable = NULL;
+static gboolean initialized = FALSE;
+
+static gboolean clear_table (gpointer key, gpointer value, gpointer user_data)
+{
+  g_hash_table_destroy (value);
+  return TRUE;
+}
+
+/********************************************************************/
+/* PUBLISHED API FUNCTIONS */
+
+void qof_class_register (QofIdTypeConst obj_name,
+                 QofSortFunc default_sort_function,
+                 const QofParam *params)
+{
+  int i;
+
+  if (!obj_name) return;
+
+  if (default_sort_function)
+    g_hash_table_insert (sortTable, (char *)obj_name, default_sort_function);
+
+  if (params) {
+    GHashTable *ht = g_hash_table_lookup (paramTable, obj_name);
+
+    /* If it doesn't already exist, create a new table for this object */
+    if (!ht) {
+      ht = g_hash_table_new (g_str_hash, g_str_equal);
+      g_hash_table_insert (paramTable, (char *)obj_name, ht);
+    }
+
+    /* Now insert all the parameters */
+    for (i = 0; params[i].param_name; i++)
+      g_hash_table_insert (ht,
+               (char *)params[i].param_name,
+               (gpointer)&(params[i]));
+  }
+}
+
+void qof_class_init(void)
+{
+  if (initialized) return;
+  initialized = TRUE;
+
+  paramTable = g_hash_table_new (g_str_hash, g_str_equal);
+  sortTable = g_hash_table_new (g_str_hash, g_str_equal);
+}
+
+void qof_class_shutdown (void)
+{
+  if (!initialized) return;
+  initialized = FALSE;
+
+  g_hash_table_foreach_remove (paramTable, clear_table, NULL);
+  g_hash_table_destroy (paramTable);
+  g_hash_table_destroy (sortTable);
+}
+
+
+const QofParam * 
+qof_class_get_parameter (QofIdTypeConst obj_name,
+                          const char *parameter)
+{
+  GHashTable *ht;
+
+  g_return_val_if_fail (obj_name, NULL);
+  g_return_val_if_fail (parameter, NULL);
+
+  ht = g_hash_table_lookup (paramTable, obj_name);
+  if (!ht)
+    PERR ("no object type %s", obj_name);
+  g_return_val_if_fail (ht, NULL);
+
+  return (g_hash_table_lookup (ht, parameter));
+}
+
+QofAccessFunc 
+qof_class_get_parameter_getter (QofIdTypeConst obj_name,
+                                 const char *parameter)
+{
+  const QofParam *prm;
+
+  g_return_val_if_fail (obj_name, NULL);
+  g_return_val_if_fail (parameter, NULL);
+
+  prm = qof_class_get_parameter (obj_name, parameter);
+  if (prm)
+    return prm->param_getfcn;
+
+  return NULL;
+}
+
+QofSetterFunc 
+qof_class_get_parameter_setter (QofIdTypeConst obj_name,
+                                 const char *parameter)
+{
+  const QofParam *prm;
+
+  g_return_val_if_fail (obj_name, NULL);
+  g_return_val_if_fail (parameter, NULL);
+
+  prm = qof_class_get_parameter (obj_name, parameter);
+  if (prm)
+    return prm->param_setfcn;
+
+  return NULL;
+}
+
+QofType 
+qof_class_get_parameter_type (QofIdTypeConst obj_name,
+                               const char *param_name)
+{
+  const QofParam *prm;
+
+  if (!obj_name || !param_name) return NULL;
+
+  prm = qof_class_get_parameter (obj_name, param_name);
+  if (!prm) return NULL;
+
+  return (prm->param_type);
+}
+
+QofSortFunc 
+qof_class_get_default_sort (QofIdTypeConst obj_name)
+{
+  if (!obj_name) return NULL;
+  return g_hash_table_lookup (sortTable, obj_name);
+}
+
+/* ============================= END OF FILE ======================== */
---
+++ src/engine/qofinstance.c
@@ -0,0 +1,168 @@
+/********************************************************************\
+ * qofinstance.c -- handler for fields common to all objects        *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652       *
+ * Boston, MA  02111-1307,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+/*
+ * Object instance holds many common fields that most
+ * gnucash objects use.
+ * 
+ * Copyright (C) 2003 Linas Vepstas <linas@linas.org>
+ */
+
+#include "gnc-trace.h"
+
+#include "kvp-util-p.h"
+#include "qofbook.h"
+#include "qofbook-p.h"
+#include "qofid.h"
+#include "qofid-p.h"
+#include "qofinstance.h"
+#include "qofinstance-p.h"
+
+static short module = MOD_ENGINE;
+
+/* ========================================================== */
+
+void 
+qof_instance_init (QofInstance *inst, QofIdType type, QofBook *book)
+{
+   QofCollection *col;
+
+   inst->book = book;
+	inst->kvp_data = kvp_frame_new();
+	inst->editlevel = 0;
+	inst->do_free = FALSE;
+	inst->dirty = FALSE;
+
+   col = qof_book_get_collection (book, type);
+	qof_entity_init (&inst->entity, type, col);
+}
+
+void 
+qof_instance_release (QofInstance *inst)
+{
+   kvp_frame_delete (inst->kvp_data);
+	inst->editlevel = 0;
+	inst->do_free = FALSE;
+   inst->dirty = FALSE;
+	qof_entity_release (&inst->entity);
+}
+
+const GUID *
+qof_instance_get_guid (QofInstance *inst)
+{
+   if (!inst) return NULL;
+   return &inst->entity.guid;
+}
+
+QofBook * 
+qof_instance_get_book (QofInstance *inst)
+{
+   if (!inst) return NULL;
+   return inst->book;
+}
+
+KvpFrame* 
+qof_instance_get_slots (QofInstance *inst)
+{
+  if (!inst) return NULL;
+  return inst->kvp_data;
+}
+
+gboolean
+qof_instance_is_dirty (QofInstance *inst)
+{
+  if (!inst) return FALSE;
+  return inst->dirty;
+}
+
+/* ========================================================== */
+/* setters */
+
+void
+qof_instance_mark_clean (QofInstance *inst)
+{
+  if(!inst) return;
+  inst->dirty = FALSE;
+}
+
+void 
+qof_instance_set_slots (QofInstance *inst, KvpFrame *frm)
+{
+  if (!inst) return;
+  if (inst->kvp_data && (inst->kvp_data != frm))
+  {
+    kvp_frame_delete(inst->kvp_data);
+  }
+                                                                                
+  inst->dirty = TRUE;
+  inst->kvp_data = frm;
+}
+
+/* ========================================================== */
+
+void
+qof_instance_gemini (QofInstance *to, QofInstance *from)
+{
+  time_t now;
+
+  /* Books must differ for a gemini to be meaningful */
+  if (!from || !to || (from->book == to->book)) return;
+
+  now = time(0);
+
+  /* Make a note of where the copy came from */
+  gnc_kvp_bag_add (to->kvp_data, "gemini", now,
+                                  "inst_guid", &from->entity.guid,
+                                  "book_guid", &from->book->entity.guid,
+                                  NULL);
+  gnc_kvp_bag_add (from->kvp_data, "gemini", now,
+                                  "inst_guid", &to->entity.guid,
+                                  "book_guid", &to->book->entity.guid,
+                                  NULL);
+
+  to->dirty = TRUE;
+}
+
+QofInstance *
+qof_instance_lookup_twin (QofInstance *src, QofBook *target_book)
+{
+	QofCollection *col;
+   KvpFrame *fr;
+   GUID * twin_guid;
+   QofInstance * twin;
+                                                                                
+   if (!src || !target_book) return NULL;
+   ENTER (" ");
+
+   fr = gnc_kvp_bag_find_by_guid (src->kvp_data, "gemini",
+                    "book_guid", &target_book->entity.guid);
+                                                                                
+   twin_guid = kvp_frame_get_guid (fr, "inst_guid");
+
+	col = qof_book_get_collection (target_book, src->entity.e_type);
+   twin = (QofInstance *) qof_collection_lookup_entity (col, twin_guid);
+
+   LEAVE (" found twin=%p", twin);
+   return twin;
+}
+
+/* ========================== END OF FILE ======================= */
