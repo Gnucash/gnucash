e1f8b3682d44da3b49bdddc05762580c70df70cd
Derek Atkins <derek@ihtfp.com>
2004-01-23 02:25:13 +0000

    * src/app-utils/Makefile.am: 	  add new-file druid provider (and callback) 	* src/app-utils/gnc-druid-provider-desc-file.[ch]: 	  provider descriptor for enter-file provider 	* src/app-utils/gnc-druid-provider-file-cb.[ch]: 	  provider callback for enter-file provider
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/gnucash-gnome2-dev@9803 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/app-utils/gnc-druid-provider-desc-file.c
@@ -0,0 +1,99 @@
+
+
+#include "config.h"
+#include "gnc-druid-provider-desc-file.h"
+#include "gnc-basic-gobject.h"
+
+static void gnc_druid_provider_desc_file_class_init	(GNCDruidProviderDescFileClass *class);
+static void gnc_druid_provider_desc_file_init		(GNCDruidProviderDescFile *gspaper);
+static void gnc_druid_provider_desc_file_finalize	(GObject *obj);
+
+static GNCDruidProviderDescClass *parent_class;
+
+GNC_BASIC_GOBJECT(GNCDruidProviderDescFile, GNCDruidProviderDescFileClass,
+		  G_TYPE_GNC_DRUID_PROVIDER_DESC,
+		  gnc_druid_provider_desc_file_class_init,
+		  gnc_druid_provider_desc_file_init,
+		  gnc_druid_provider_desc_file_get_type,
+		  gnc_druid_provider_desc_file_new)
+
+static void
+gnc_druid_provider_desc_file_class_init (GNCDruidProviderDescFileClass *klass)
+{
+  GObjectClass *object_class;
+	
+  object_class = G_OBJECT_CLASS (klass);
+  parent_class = g_type_class_peek_parent (klass);
+
+  object_class->finalize = gnc_druid_provider_desc_file_finalize;
+}
+
+static void
+gnc_druid_provider_desc_file_finalize (GObject *obj)
+{
+  GNCDruidProviderDescFile *desc = (GNCDruidProviderDescFile *)obj;
+
+  if (desc->text)
+    g_free(desc->text);
+  if (desc->last_directory)
+    g_free(desc->last_directory);
+
+  G_OBJECT_CLASS (parent_class)->finalize(obj);
+}
+
+static void
+gnc_druid_provider_desc_file_init (GNCDruidProviderDescFile *o)
+{
+  o->parent.name = GNC_DRUID_PROVIDER_TYPE_FILE;
+}
+
+GNCDruidProviderDescFile*
+gnc_druid_provider_desc_file_new_with_data(const gchar* title,
+					   const gchar* text,
+					   const gchar* last_dir,
+					   GNCDruidProviderCB next_cb)
+{
+  GNCDruidProviderDescFile* desc;
+  GNCDruidProviderDesc* gdp_desc;
+
+  desc = gnc_druid_provider_desc_file_new();
+  g_assert(desc);
+  gdp_desc = GNC_DRUID_PROVIDER_DESC(desc);
+
+  gdp_desc->next_cb = next_cb;
+
+  if (text)
+    gnc_druid_provider_desc_file_set_text(desc, text);
+  if (last_dir)
+    gnc_druid_provider_desc_file_set_last_dir(desc, last_dir);
+  if (title)
+    gnc_druid_provider_desc_set_title(gdp_desc, title);
+
+  return desc;
+}
+
+void
+gnc_druid_provider_desc_file_set_text(GNCDruidProviderDescFile* desc,
+				      const gchar* text)
+{
+  g_return_if_fail(desc);
+  g_return_if_fail(IS_GNC_DRUID_PROVIDER_DESC_FILE(desc));
+  g_return_if_fail(text);
+
+  if (desc->text)
+    g_free(desc->text);
+  desc->text = g_strdup(text);
+}
+
+void
+gnc_druid_provider_desc_file_set_last_dir(GNCDruidProviderDescFile* desc,
+					  const gchar* last_dir)
+{
+  g_return_if_fail(desc);
+  g_return_if_fail(IS_GNC_DRUID_PROVIDER_DESC_FILE(desc));
+  g_return_if_fail(last_dir);
+
+  if (desc->last_directory)
+    g_free(desc->last_directory);
+  desc->last_directory = g_strdup(last_dir);
+}
---
+++ src/app-utils/gnc-druid-provider-desc-file.h
@@ -0,0 +1,57 @@
+
+
+#ifndef GNC_DRUID_PROVIDER_DESC_FILE_H
+#define GNC_DRUID_PROVIDER_DESC_FILE_H
+
+#ifdef __cplusplus
+//extern "C" {
+#endif
+
+#include <glib.h>
+#include <glib-object.h>
+
+#define G_TYPE_GNC_DRUID_PROVIDER_DESC_FILE	gnc_druid_provider_desc_file_get_type()
+#define GNC_DRUID_PROVIDER_DESC_FILE(obj)	G_TYPE_CHECK_INSTANCE_CAST((obj), G_TYPE_GNC_DRUID_PROVIDER_DESC_FILE, GNCDruidProviderDescFile)
+#define IS_GNC_DRUID_PROVIDER_DESC_FILE(obj)	G_TYPE_CHECK_INSTANCE_TYPE((obj), G_TYPE_GNC_DRUID_PROVIDER_DESC_FILE)
+
+typedef struct _GNCDruidProviderDescFile GNCDruidProviderDescFile;
+typedef struct _GNCDruidProviderDescFileClass GNCDruidProviderDescFileClass;
+
+#include "gnc-druid.h"
+#include "gnc-druid-provider-file-cb.h"
+
+#define GNC_DRUID_PROVIDER_TYPE_FILE	"file"
+
+struct _GNCDruidProviderDescFile
+{
+  GNCDruidProviderDesc parent;
+
+  gchar* text;
+  gchar* last_directory;
+};
+
+struct _GNCDruidProviderDescFileClass
+{
+  GNCDruidProviderDescClass parent;
+};
+
+GType	gnc_druid_provider_desc_file_get_type(void);
+GNCDruidProviderDescFile* gnc_druid_provider_desc_file_new(void);
+GNCDruidProviderDescFile*
+gnc_druid_provider_desc_file_new_with_data(const gchar* title,
+					   const gchar* text,
+					   const gchar* last_dir,
+					   GNCDruidProviderCB next_cb);
+
+void	gnc_druid_provider_desc_file_set_text(GNCDruidProviderDescFile*,
+					       const gchar* text);
+void	gnc_druid_provider_desc_file_set_last_dir(GNCDruidProviderDescFile*,
+						  const gchar* last_dir);
+
+/* methods */
+
+#ifdef __cplusplus
+//}
+#endif
+
+#endif /* GNC_DRUID_PROVIDER_DESC_FILE_H */
