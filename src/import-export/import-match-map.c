524f98c9d39b925cc71db52b37da32fdca47325e
Christian Stimming <stimming@tuhh.de>
2004-05-08 09:32:36 +0000

    (gnc_imap_add_account_bayes): Skip the case when a token is the empty string.
    2004-05-08  Christian Stimming  <stimming@tuhh.de>
    
    	* src/import-export/import-match-map.c
    	(gnc_imap_add_account_bayes): Skip the case when a token is the
    	empty string. This caused many warnings about a NULL kvp_frame
    	lookup.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@9956 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/import-export/import-match-map.c
+++ src/import-export/import-match-map.c
@@ -482,6 +482,13 @@ void gnc_imap_add_account_bayes(GncImportMatchMap *imap, GList *tokens, Account 
   for(current_token = g_list_first(tokens); current_token;
       current_token = current_token->next)
     {
+      /* Jump to next iteration if the pointer is not valid or if the
+	 string is empty. In HBCI import we almost always get an empty
+	 string, which doesn't work in the kvp loopkup later. So we
+	 skip this case here. */
+      if (!current_token->data || (*((char*)current_token->data) == '\0'))
+	continue;
+
       /* start off with no tokens for this account */
       token_count = 0;
 
