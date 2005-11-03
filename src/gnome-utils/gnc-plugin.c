b8138fcba91a1921f84839dc35d24d807578e36a
David Hampton <hampton@employees.org>
2005-11-03 00:47:13 +0000

    Null terminate code properly.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@11797 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnc-plugin.c
+++ src/gnome-utils/gnc-plugin.c
@@ -313,11 +313,7 @@ gnc_plugin_set_important_actions (GtkActionGroup *action_group,
 
   for (i = 0; name[i]; i++) {
     action = gtk_action_group_get_action (action_group, name[i]);
-#ifdef HAVE_GOFFICE
-    g_object_set (G_OBJECT(action), "is_important", TRUE, FALSE, NULL);
-#else
-    g_object_set (G_OBJECT(action), "is_important", TRUE, FALSE);
-#endif
+    g_object_set (G_OBJECT(action), "is_important", TRUE, NULL);
   }
 
   /* If this trips, you've got too many "important" actions.  That
