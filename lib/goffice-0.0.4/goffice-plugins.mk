f7d4c747f45841edbb1dbee110b3bec7eb55f2f5
Neil Williams <linux@codehelp.co.uk>
2005-11-20 20:33:25 +0000

    Fix unnecessary subdirectory handling of config headers
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/goffice-update@11993 57a11ea4-9604-0410-9ed3-97b8803252fd

--- lib/goffice-0.0.4/goffice-plugins.mk
+++ lib/goffice-0.0.4/goffice-plugins.mk
@@ -1,5 +1,6 @@
 AM_CPPFLAGS = \
-    -I$(top_srcdir)/lib/goffice-0.0.4		\
+    -I$(top_srcdir)/lib/libgsf-1.12.3	    \
+    -I$(top_srcdir)/lib/goffice-0.0.4 		\
     -I$(top_builddir)/lib/goffice-0.0.4		\
     $(GOFFICE_DEPS_CFLAGS)
 
