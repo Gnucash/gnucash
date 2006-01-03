f301cae67983513dc66224184b8a0cc743c1a8b6
Chris Shoemaker <c.shoemaker@cox.net>
2006-01-03 23:49:39 +0000

    Invert the program entry point.
    'gnucash' is now a shell script that calls the executable
    'gnucash-bin' which uses libguile to load guile modules.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@12252 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/bin/gnucash-bin.c
@@ -0,0 +1,43 @@
+/*
+ * gnucash-bin.c -- The program entry point for GnuCash
+ *
+ * Copyright (C) 2006 Chris Shoemaker <c.shoemaker@cox.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact:
+ *
+ * Free Software Foundation           Voice:  +1-617-542-5942
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <libguile.h>
+#include "glib.h"
+
+static void
+inner_main (void *closure, int argc, char **argv)
+{
+    /* module initializations would go here */
+    //scm_c_eval_string("(display %load-path)");
+    scm_c_eval_string("(use-modules (gnucash main))");
+    scm_c_eval_string("(gnc:main)");
+    return;
+}
+
+int main(int argc, char ** argv)
+{
+    scm_boot_guile(argc, argv, inner_main, 0);
+    exit(0); /* never reached */
+}
