2d984ba480aef106455fb5001c767cfcf515ac63
David Hampton <hampton@employees.org>
2006-01-29 22:50:39 +0000

    Andreas Köhler's patch to fix commodity editor warnings/crash.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@13037 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnc-tree-model-commodity.c
+++ src/gnome-utils/gnc-tree-model-commodity.c
@@ -1052,7 +1052,7 @@ gnc_tree_model_commodity_path_changed (GncTreeModelCommodity *model,
   GtkTreeIter iter;
 
   debug_path(ENTER, path);
-  if (gtk_tree_path_up (path)) {
+  if (gtk_tree_path_up (path) && gtk_tree_path_get_depth (path) > 0) {
     debug_path(DEBUG, path);
     gtk_tree_model_get_iter (GTK_TREE_MODEL(model), &iter, path);
     DEBUG("iter %s", iter_to_string(&iter));
