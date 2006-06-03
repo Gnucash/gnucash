dce59455695c1dc00908fef0adce8e2852651a24
David Hampton <hampton@employees.org>
2006-06-03 19:58:18 +0000

    The register code shouldn't process any keystrokes that have a "modifier" (e.g. Alt, Meta, etc.) key pressed.  This allows Alt-Ctl-Pgup/Down in a register to change the notebook page, just like from the accounts page. Also remove some dead code.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@14308 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/register/register-gnome/gnucash-sheet.c
+++ src/register/register-gnome/gnucash-sheet.c
@@ -1609,8 +1609,16 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
         gnucash_cursor_get_virt (GNUCASH_CURSOR(sheet->cursor), &cur_virt_loc);
         new_virt_loc = cur_virt_loc;
 
+	/* Don't process any keystrokes where a modifier key (Alt,
+	 * Meta, etc.) is being held down.  This should't include
+	 * MOD2, aka NUM LOCK. */
+	if (event->state & (GDK_MOD1_MASK | GDK_MOD3_MASK |
+			    GDK_MOD4_MASK | GDK_MOD5_MASK))
+		pass_on = TRUE;
+
 	/* Calculate tentative physical values */
-        switch (event->keyval) {
+	if (!pass_on) {
+            switch (event->keyval) {
                 case GDK_Return:
                 case GDK_KP_Enter:
                         g_signal_emit_by_name(sheet->reg, "activate_cursor");
@@ -1666,7 +1674,7 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
                 case GDK_Down:
                 case GDK_Menu:
                         if (event->keyval == GDK_Menu ||
-                            event->state & (GDK_CONTROL_MASK | GDK_MOD1_MASK))
+                            event->state & GDK_CONTROL_MASK)
                         {
                                 GncItemEdit *item_edit;
 
@@ -1696,22 +1704,9 @@ gnucash_sheet_key_press_event (GtkWidget *widget, GdkEventKey *event)
                                 return TRUE;
 
 			pass_on = TRUE;
-
-                        /* This is a piece of logic from gtkentry.c. We
-                           are trying to figure out whether to change the
-                           selection. If this is a regular character, we
-                           don't want to change the selection, as it will
-                           get changed in the insert callback. */
-                        if ((event->keyval >= 0x20) && (event->keyval <= 0xFF))
-                        {
-                                if (event->state & GDK_CONTROL_MASK)
-                                        break;
-                                if (event->state & GDK_MOD1_MASK)
-                                        break;
-                        }
-
 			break;
-        }
+            }
+	}
 
 	/* Forward the keystroke to the input line */
 	if (pass_on)
