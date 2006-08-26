aa52b5aaea382e34a17ba5c0cc278ba61773bdef
Derek Atkins <derek@ihtfp.com>
2006-08-26 23:06:35 +0000

    revert back to checking for libgoffice-0.3.pc
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@14733 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/install.sh
+++ packaging/win32/install.sh
@@ -531,7 +531,7 @@ function inst_goffice() {
     _GOFFICE_UDIR=`unix_path $GOFFICE_DIR`
     add_to_env $_GOFFICE_UDIR/lib/pkgconfig PKG_CONFIG_PATH
     set +e
-    quiet pkg-config --exists libgoffice-1
+    quiet pkg-config --exists libgoffice-0.3
     if [ $? = 0 ] ; then
 	set -e
 	echo "goffice already installed.  skipping."
@@ -564,8 +564,7 @@ function inst_goffice() {
 	    make install
 	qpopd
     fi
-    pkg-config --exists libgoffice-0.3 && die "Sorry, for goffice gnucash expects a pkgconfig file with the name libgoffice-1.pc, but you have an installed file libgoffice-0.3.pc. You should probably rename that file."
-    pkg-config --exists libgoffice-1 || die "goffice not installed correctly"
+    pkg-config --exists libgoffice-0.3 || die "goffice not installed correctly"
 }
 
 function inst_svn() {
