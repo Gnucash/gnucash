e2b78c603494f87738417fb0c1e958920dd65d8d
Christian Stimming <stimming@tuhh.de>
2006-10-02 11:55:51 +0000

    Update openssl version number as reported by Thomas Arildsen
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@14918 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/custom.sh
+++ packaging/win32/custom.sh
@@ -70,7 +70,7 @@ GUILE_URL="http://ftp.gnu.org/pub/gnu/guile/guile-1.6.8.tar.gz"
 SLIB_URL="http://swiss.csail.mit.edu/ftpdir/scm/slib3a3.zip"
 GUILE_DIR=$GLOBAL_DIR\\guile
 
-OPENSSL_URL="http://www.slproweb.com/download/Win32OpenSSL-0_9_8c.exe"
+OPENSSL_URL="http://www.slproweb.com/download/Win32OpenSSL-0_9_8d.exe"
 OPENSSL_DIR=$GLOBAL_DIR\\openssl
 
 PEXPORTS_URL="http://www.emmestech.com/software/cygwin/pexports-0.43/pexports-0.43.zip"
