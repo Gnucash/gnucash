d6d784617a8f63728daa4e391420866af1db3534
David Hampton <hampton@employees.org>
2007-04-21 19:18:16 +0000

    Disable the close callback while updating reports.  Fixes #416986.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@15959 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/dialog-options.c
+++ src/gnome-utils/dialog-options.c
@@ -1300,6 +1300,8 @@ gnc_options_dialog_notebook(GNCOptionWin * win)
 void
 gnc_options_dialog_response_cb(GtkDialog *dialog, gint response, GNCOptionWin *window)
 {
+  GNCOptionWinCallback close_cb;
+
   switch (response) {
    case GTK_RESPONSE_HELP:
     if(window->help_cb)
@@ -1309,8 +1311,11 @@ gnc_options_dialog_response_cb(GtkDialog *dialog, gint response, GNCOptionWin *w
    case GTK_RESPONSE_OK:
    case GTK_RESPONSE_APPLY:
     gnc_options_dialog_changed_internal (window->dialog, FALSE);
+    close_cb = window->close_cb;
+    window->close_cb = NULL;
     if (window->apply_cb)
       window->apply_cb (window, window->apply_cb_data);
+    window->close_cb = close_cb;
     if (response == GTK_RESPONSE_APPLY)
       break;
     /* fall through */
