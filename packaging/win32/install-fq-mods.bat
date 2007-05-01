06c231104733bd2362dc5be2d18f995cbd998d2a
Andreas Köhler <andi5.py@gmx.net>
2007-05-01 22:05:42 +0000

    Add support for ActivePerl 5.6 to install-fq-mods.bat.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@16036 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/install-fq-mods.bat
+++ packaging/win32/install-fq-mods.bat
@@ -3,9 +3,28 @@
 if not exist ssleay32.dll (
   echo.
   echo Did not find ssleay32.dll in current directory.
-  echo Please start this batch file in the bin directory created by the installer.
+  echo Please start this batch file in the bin directory created by the setup.exe.
   goto error
 )
+
+echo.
+echo * Check Perl
+echo.
+perl -e "$ver=1000*sprintf(\"%%.3f\", $]); exit(int($ver/100)+$ver%%1000);"
+if %errorlevel% equ 58 (
+  set _haveperl58=1
+  goto ccp
+)
+if %errorlevel% equ 56 (
+  set _haveperl58=
+  goto ccp
+)
+echo.
+echo Did not find a usable perl.
+echo Please install ActivePerl (http://www.activestate.com/store/activeperl)
+echo and add the bin directory to your Path environment variable.
+goto error
+:ccp
 
 echo * Install DateManip
 echo.
@@ -17,7 +36,11 @@ echo * Install Crypt-SSLeay
 echo.
 set OLDPATH=%PATH%
 set PATH=%CD%;%PATH%
-echo anything | perl -x -S ppm install http://theoryx5.uwinnipeg.ca/ppms/Crypt-SSLeay.ppd
+if defined _haveperl58 (
+  echo anything | perl -x -S ppm install http://theoryx5.uwinnipeg.ca/ppms/Crypt-SSLeay.ppd
+) else (
+  perl -x -S ppm install http://theoryx5.uwinnipeg.ca/ppmpackages/Crypt-SSLeay.ppd
+)
 set errlvlbak=%errorlevel%
 set PATH=%OLDPATH%
 if %errlvlbak% neq 0 goto error
