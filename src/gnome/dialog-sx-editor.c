07e94bda8e30eed3e0ca4f48805883ae1fe12dc1
Joshua Sled <jsled@asynchronous.org>
2007-07-06 21:13:48 +0000

    Update LICENSE to explain the state of the world.  Add OpenSSL exemption to my sources.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@16254 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/dialog-sx-editor.c
+++ src/gnome/dialog-sx-editor.c
@@ -5,6 +5,17 @@
  * This program is free software; you can redistribute it and/or    *
  * modify it under the terms of version 2 of the GNU General Public *
  * License as published by the Free Software Foundation.            *
+ *
+ * As a special exception, permission is granted to link the binary
+ * module resultant from this code with the OpenSSL project's
+ * "OpenSSL" library (or modified versions of it that use the same
+ * license as the "OpenSSL" library), and distribute the linked
+ * executable.  You must obey the GNU General Public License in all
+ * respects for all of the code used other than "OpenSSL". If you
+ * modify this file, you may extend this exception to your version
+ * of the file, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version
+ * of this file.
  *                                                                  *
  * This program is distributed in the hope that it will be useful,  *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
