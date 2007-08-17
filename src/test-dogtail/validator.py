6f5aba7f5a4b57e383de9789b1eb349cb2c6ff5e
Ahmed Sayed <ahmadsayed83@yahoo.com>
2007-08-17 21:17:19 +0000

    add copyright comments
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/dogtail@16442 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/test-dogtail/validator.py
+++ src/test-dogtail/validator.py
@@ -1,5 +1,26 @@
-""" untility functions Work as a generic validator """
-
+ ####################################################################
+ # GnuCash.py -- Wrapper of Gnucash widgets and dialogs             #
+ # Copyright (C) 2007 Ahmed Sayed Hassan, <ahmadsayed83@yahoo.com>  #
+ #                                                                  #
+ # This program is free software; you can redistribute it and/or    #
+ # modify it under the terms of the GNU General Public License as   #
+ # published by the Free Software Foundation; either version 2 of   #
+ # the License, or (at your option) any later version.              #
+ #                                                                  #
+ # This program is distributed in the hope that it will be useful,  #
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of   #
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    #
+ # GNU General Public License for more details.                     #
+ #                                                                  #
+ # You should have received a copy of the GNU General Public License#
+ # along with this program; if not, contact:                        #
+ #                                                                  #
+ # Free Software Foundation           Voice:  +1-617-542-5942       #
+ # 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       #
+ # Boston, MA  02110-1301,  USA       gnu@gnu.org                   #
+ #                                                                  #
+ ####################################################################
+ 
 __author__ = "Ahmed Sayed <ahmadsayed83@yahoo.com>"
 import os
 spacer = ' '
