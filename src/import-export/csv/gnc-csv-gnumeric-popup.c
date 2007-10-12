a6839478a4e7486034b0ee7dc63c42641b213758
Andreas Köhler <andi5.py@gmx.net>
2007-10-12 22:51:34 +0000

    Merge csv-import branch back into trunk.
     configure.in                                       |    2
     lib/Makefile.am                                    |    4
     lib/stf/Makefile.am                                |   13
     lib/stf/README                                     |    2
     lib/stf/stf-parse.c                                | 1414 +++++++++++++++++++++
     lib/stf/stf-parse.h                                |  112 +
     src/bin/gnucash-bin.c                              |    1
     src/import-export/Makefile.am                      |    4
     src/import-export/csv/Makefile.am                  |   59
     src/import-export/csv/example-file.csv             |    4
     src/import-export/csv/gnc-csv-gnumeric-popup.c     |  194 ++
     src/import-export/csv/gnc-csv-gnumeric-popup.h     |   78 +
     src/import-export/csv/gnc-csv-import.c             | 1173 +++++++++++++++++
     src/import-export/csv/gnc-csv-import.h             |   33
     src/import-export/csv/gnc-csv-model.c              | 1199 +++++++++++++++++
     src/import-export/csv/gnc-csv-model.h              |  122 +
     src/import-export/csv/gnc-csv-preview-dialog.glade |  496 +++++++
     src/import-export/csv/gnc-csv2glist.c              |  187 --
     src/import-export/csv/gnc-csv2glist.h              |   39
     src/import-export/csv/gnc-plugin-csv-ui.xml        |   11
     src/import-export/csv/gnc-plugin-csv.c             |  160 ++
     src/import-export/csv/gnc-plugin-csv.h             |   60
     src/import-export/csv/gncmod-csv-import.c          |   91 +
     23 files changed, 5228 insertions(+), 230 deletions(-)
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@16561 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ src/import-export/csv/gnc-csv-gnumeric-popup.c
@@ -0,0 +1,194 @@
+/* The following is code copied from Gnumeric 1.7.8 licensed under the
+ * GNU General Public License version 2. It is from the file
+ * gnumeric/src/gui-util.c, and it has been modified slightly to work
+ * within GnuCash. */
+
+/* Miguel de Icaza is not sure specifically who from the Gnumeric
+ * community is the copyright owner of the code below, so, on his
+ * recommendation, here is the full list of Gnumeric authors.
+ *
+ * Miguel de Icaza, creator.
+ * Jody Goldberg, maintainer.
+ * Harald Ashburner, Options pricers
+ * Sean Atkinson, functions and X-Base importing.
+ * Michel Berkelaar, Simplex algorithm for Solver (LP Solve).
+ * Jean Brefort, Core charting engine.
+ * Grandma Chema Celorio, Tester and sheet copy.
+ * Frank Chiulli, OLE support.
+ * Kenneth Christiansen, i18n, misc stuff.
+ * Zbigniew Chyla, plugin system, i18n.
+ * J.H.M. Dassen (Ray), debian packaging.
+ * Jeroen Dirks, Simplex algorithm for Solver (LP Solve).
+ * Tom Dyas, plugin support.
+ * Gergo Erdi, Gnumeric hacker.
+ * John Gotts, rpm packaging.
+ * Andreas J. Guelzow, Gnumeric hacker.
+ * Jon K. Hellan, Gnumeric hacker.
+ * Ross Ihaka, special functions.
+ * Jukka-Pekka Iivonen, numerous functions and tools.
+ * Jakub Jelinek, Gnumeric hacker.
+ * Chris Lahey, number format engine.
+ * Adrian Likins, documentation, debugging.
+ * Takashi Matsuda, original text plugin.
+ * Michael Meeks, Excel and OLE2 importing.
+ * Lutz Muller, SheetObject improvements.
+ * Emmanuel Pacaud, Many plot types for charting engine.
+ * Federico M. Quintero, canvas support.
+ * Mark Probst, Guile support.
+ * Rasca, HTML, troff, LaTeX exporters.
+ * Vincent Renardias, original CSV support, French localization.
+ * Ariel Rios, Guile support.
+ * Uwe Steinmann, Paradox Importer.
+ * Arturo Tena, OLE support.
+ * Almer S. Tigelaar, Gnumeric hacker.
+ * Bruno Unna, Excel bits.
+ * Daniel Veillard, XML support.
+ * Vladimir Vuksan, financial functions.
+ * Morten Welinder, Gnumeric hacker and leak plugging demi-god.
+ */
+
+#include "gnc-csv-gnumeric-popup.h"
+
+#include <glib/gi18n.h>
+
+static void
+popup_item_activate (GtkWidget *item, gpointer *user_data)
+{
+	GnumericPopupMenuElement const *elem =
+		g_object_get_data (G_OBJECT (item), "descriptor");
+	GnumericPopupMenuHandler handler =
+		g_object_get_data (G_OBJECT (item), "handler");
+
+	g_return_if_fail (elem != NULL);
+	g_return_if_fail (handler != NULL);
+
+	if (handler (elem, user_data))
+		gtk_widget_destroy (gtk_widget_get_toplevel (item));
+}
+
+static void
+gnumeric_create_popup_menu_list (GSList *elements,
+				 GnumericPopupMenuHandler handler,
+				 gpointer user_data,
+				 int display_filter,
+				 int sensitive_filter,
+				 GdkEventButton *event)
+{
+	GtkWidget *menu, *item;
+	char const *trans;
+
+	menu = gtk_menu_new ();
+
+	for (; elements != NULL ; elements = elements->next) {
+		GnumericPopupMenuElement const *element = elements->data;
+		char const * const name = element->name;
+		char const * const pix_name = element->pixmap;
+
+		item = NULL;
+
+		if (element->display_filter != 0 &&
+		    !(element->display_filter & display_filter))
+			continue;
+
+		if (name != NULL && *name != '\0') {
+			trans = _(name);
+			item = gtk_image_menu_item_new_with_mnemonic (trans);
+			if (element->sensitive_filter != 0 &&
+			    (element->sensitive_filter & sensitive_filter))
+				gtk_widget_set_sensitive (GTK_WIDGET (item), FALSE);
+			if (pix_name != NULL) {
+				GtkWidget *image = gtk_image_new_from_stock (pix_name,
+                                        GTK_ICON_SIZE_MENU);
+				gtk_widget_show (image);
+				gtk_image_menu_item_set_image (
+					GTK_IMAGE_MENU_ITEM (item),
+					image);
+			}
+		} else {
+			/* separator */
+			item = gtk_menu_item_new ();
+			gtk_widget_set_sensitive (item, FALSE);
+		}
+
+		if (element->index != 0) {
+			g_signal_connect (G_OBJECT (item),
+				"activate",
+				G_CALLBACK (&popup_item_activate), user_data);
+			g_object_set_data (
+				G_OBJECT (item), "descriptor", (gpointer)(element));
+			g_object_set_data (
+				G_OBJECT (item), "handler", (gpointer)handler);
+		}
+
+		gtk_widget_show (item);
+		gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
+	}
+
+	gnumeric_popup_menu (GTK_MENU (menu), event);
+}
+
+void
+gnumeric_create_popup_menu (GnumericPopupMenuElement const *elements,
+			    GnumericPopupMenuHandler handler,
+			    gpointer user_data,
+			    int display_filter, int sensitive_filter,
+			    GdkEventButton *event)
+{
+	int i;
+	GSList *tmp = NULL;
+
+	for (i = 0; elements [i].name != NULL; i++)
+		tmp = g_slist_prepend (tmp, (gpointer)(elements + i));
+
+	tmp = g_slist_reverse (tmp);
+	gnumeric_create_popup_menu_list (tmp, handler, user_data,
+		display_filter, sensitive_filter, event);
+	g_slist_free (tmp);
+}
+
+static void
+kill_popup_menu (GtkWidget *widget, GtkMenu *menu)
+{
+	g_return_if_fail (menu != NULL);
+	g_return_if_fail (GTK_IS_MENU (menu));
+
+	g_object_unref (G_OBJECT (menu));
+}
+
+/**
+ * gnumeric_popup_menu :
+ * @menu : #GtkMenu
+ * @event : #GdkEventButton optionally NULL
+ *
+ * Bring up a popup and if @event is non-NULL ensure that the popup is on the
+ * right screen.
+ **/
+void
+gnumeric_popup_menu (GtkMenu *menu, GdkEventButton *event)
+{
+	g_return_if_fail (menu != NULL);
+	g_return_if_fail (GTK_IS_MENU (menu));
+
+#if GLIB_CHECK_VERSION(2,10,0) && GTK_CHECK_VERSION(2,8,14)
+	g_object_ref_sink (menu);
+#else
+	g_object_ref (menu);
+	gtk_object_sink (GTK_OBJECT (menu));
+#endif
+
+	if (event)
+		gtk_menu_set_screen (menu,
+				     gdk_drawable_get_screen (event->window));
+
+	g_signal_connect (G_OBJECT (menu),
+		"hide",
+		G_CALLBACK (kill_popup_menu), menu);
+
+	/* Do NOT pass the button used to create the menu.
+	 * instead pass 0.  Otherwise bringing up a menu with
+	 * the right button will disable clicking on the menu with the left.
+	 */
+	gtk_menu_popup (menu, NULL, NULL, NULL, NULL, 0, 
+			(event != NULL) ? event->time 
+			: gtk_get_current_event_time());
+}
---
+++ src/import-export/csv/gnc-csv-model.h
@@ -0,0 +1,122 @@
+/********************************************************************\
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
+\********************************************************************/
+
+/** @file
+     @brief CSV import GUI
+     *
+     gnc-csv-import.h
+     @author Copyright (c) 2007 Benny Sperisen <lasindi@gmail.com>
+ */
+
+#ifndef GNC_CSV_MODEL_H
+#define GNC_CSV_MODEL_H
+
+#include "config.h"
+
+#include "Account.h"
+#include "Transaction.h"
+
+#include "stf/stf-parse.h"
+
+/** Enumeration for column types. These are the different types of
+ * columns that can exist in a CSV/Fixed-Width file. There should be
+ * no two columns with the same type except for the GNC_CSV_NONE
+ * type. */
+enum GncCsvColumnType {GNC_CSV_NONE,
+                       GNC_CSV_DATE,
+                       GNC_CSV_DESCRIPTION,
+                       GNC_CSV_BALANCE,
+                       GNC_CSV_DEPOSIT,
+                       GNC_CSV_WITHDRAWAL,
+                       GNC_CSV_NUM,
+                       GNC_CSV_NUM_COL_TYPES};
+
+/** Enumeration for error types. These are the different types of
+ * errors that various functions used for the CSV/Fixed-Width importer
+ * can have. */
+enum GncCsvErrorType {GNC_CSV_FILE_OPEN_ERR,
+                      GNC_CSV_ENCODING_ERR};
+
+/** Struct for containing a string. This struct simply contains
+ * pointers to the beginning and end of a string. We need this because
+ * the STF code that gnc_csv_parse calls requires these pointers. */
+typedef struct
+{
+  char* begin;
+  char* end;
+} GncCsvStr;
+
+/* TODO We now sort transactions by date, not line number, so we
+ * should probably get rid of this struct and uses of it. */
+
+/** Struct pairing a transaction with a line number. This struct is
+ * used to keep the transactions in order. When rows are separated
+ * into "valid" and "error" lists (in case some of the rows have cells
+ * that are unparseable), we want the user to still be able to
+ * "correct" the error list. If we keep the line numbers of valid
+ * transactions, we can then put transactions created from the newly
+ * corrected rows into the right places. */
+typedef struct
+{
+  int line_no;
+  Transaction* trans;
+  gnc_numeric balance; /**< The (supposed) balance after this transaction takes place */
+  gboolean balance_set; /**< TRUE if balance has been set from user data, FALSE otherwise */
+} GncCsvTransLine;
+
+extern const int num_date_formats;
+/* A set of date formats that the user sees. */
+extern const gchar* date_format_user[];
+
+/* This array contains all of the different strings for different column types. */
+extern gchar* gnc_csv_column_type_strs[];
+
+/** Struct containing data for parsing a CSV/Fixed-Width file. */
+typedef struct
+{
+  gchar* encoding;
+  GMappedFile* raw_mapping; /**< The mapping containing raw_str */
+  GncCsvStr raw_str; /**< Untouched data from the file as a string */
+  GncCsvStr file_str; /**< raw_str translated into UTF-8 */
+  GPtrArray* orig_lines; /**< file_str parsed into a two-dimensional array of strings */
+  GArray* orig_row_lengths; /**< The lengths of rows in orig_lines
+                             * before error messages are appended */
+  int orig_max_row; /**< Holds the maximum value in orig_row_lengths */
+  GStringChunk* chunk; /**< A chunk of memory in which the contents of orig_lines is stored */
+  StfParseOptions_t* options; /**< Options controlling how file_str should be parsed */
+  GArray* column_types; /**< Array of values from the GncCsvColumnType enumeration */
+  GList* error_lines; /**< List of row numbers in orig_lines that have errors */
+  GList* transactions; /**< List of GncCsvTransLine*s created using orig_lines and column_types */
+  int date_format; /**< The format of the text in the date columns from date_format_internal. */
+} GncCsvParseData;
+
+GncCsvParseData* gnc_csv_new_parse_data(void);
+
+void gnc_csv_parse_data_free(GncCsvParseData* parse_data);
+
+int gnc_csv_load_file(GncCsvParseData* parse_data, const char* filename,
+                      GError** error);
+
+int gnc_csv_convert_encoding(GncCsvParseData* parse_data, const char* encoding, GError** error);
+
+int gnc_csv_parse(GncCsvParseData* parse_data, gboolean guessColTypes, GError** error);
+
+int gnc_csv_parse_to_trans(GncCsvParseData* parse_data, Account* account, gboolean redo_errors);
+
+#endif
