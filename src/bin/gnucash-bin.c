8da4c360ab2fd9af2c0f41ad949a04deafe693ed
Joshua Sled <jsled@asynchronous.org>
2007-12-15 20:06:56 +0000

    Don't make unknown command-line arguments abort the program, just warn and exit non-zero. BP
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@16650 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/bin/gnucash-bin.c
+++ src/bin/gnucash-bin.c
@@ -285,7 +285,8 @@ gnucash_command_line(int *argc, char **argv)
     g_option_context_add_group (context, gtk_get_option_group (FALSE));
     if (!g_option_context_parse (context, argc, &argv, &error))
     {
-         g_error("Error parsing command line arguments: [%s]; try `gnucash --help` for available options.", error->message);
+         g_warning("Error parsing command line arguments: [%s]; try `gnucash --help` for available options.", error->message);
+         exit(1);
     }
     g_option_context_free (context);
     if (error)
