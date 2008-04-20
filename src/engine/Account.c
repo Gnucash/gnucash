91242ea1ad25da9cd189398fb2743034be443e8b
Andreas Köhler <andi5.py@gmx.net>
2008-04-20 19:24:49 +0000

    [r17087] Add a risk-reduction measure to xaccAccountStagedTransactionTraversal() in case of a naughty thunk.
    Add warnings to doxygen documentation for all Account.h functions with
    TransactionCallback parameters.
    
    Committed by cedayiv.
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/2.2@17111 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/engine/Account.c
+++ src/engine/Account.c
@@ -4375,6 +4375,7 @@ xaccAccountStagedTransactionTraversal (const Account *acc,
 {
   AccountPrivate *priv;
   GList *split_p;
+  GList *next;
   Transaction *trans;
   Split *s;
   int retval;
@@ -4382,7 +4383,13 @@ xaccAccountStagedTransactionTraversal (const Account *acc,
   if (!acc) return 0;
 
   priv = GET_PRIVATE(acc);
-  for(split_p = priv->splits; split_p; split_p = g_list_next(split_p)) {
+  for(split_p = priv->splits; split_p; split_p = next) {
+    /* Get the next element in the split list now, just in case some
+     * naughty thunk destroys the one we're using. This reduces, but
+     * does not eliminate, the possibility of undefined results if
+     * a thunk removes splits from this account. */
+    next = g_list_next(split_p);
+
     s = split_p->data;
     trans = s->parent;   
     if (trans && (trans->marker < stage)) {
