4201814cea2342ae8cb231abc7c380fc75384c4c
Andreas Köhler <andi5.py@gmx.net>
2008-05-11 00:02:14 +0000

    Correct quickfill direction in transfer dialogs.
    gnc_xfer_dialog_quickfill() messed up debit and credit.  When the "from
    account" (left in normal mode, right/credit when using formal accounting
    terms) was chosen as basis for the quickfill and a match was found, the
    remote account should be selected on the "to account" side.
    
    BP
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@17149 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/dialog-transfer.c
+++ src/gnome-utils/dialog-transfer.c
@@ -623,14 +623,17 @@ gnc_xfer_dialog_quickfill( XferDialog *xferData )
   {
     GNCAccountType other_type;
     GtkWidget *other_button;
-    
+    XferDirection other_direction;
+
     DEBUG("updating other split");
     if (xferData->quickfill == XFER_DIALOG_FROM) {
-      other_button = xferData->from_show_button;
+      other_button = xferData->to_show_button;
+      other_direction = XFER_DIALOG_TO;
     }
     else
     {
-      other_button = xferData->to_show_button;
+      other_button = xferData->from_show_button;
+      other_direction = XFER_DIALOG_FROM;
     }
 
     other_type = xaccAccountGetType(other_acct);
@@ -641,7 +644,7 @@ gnc_xfer_dialog_quickfill( XferDialog *xferData )
     if( (other_type == ACCT_TYPE_EXPENSE) || (other_type == ACCT_TYPE_INCOME) )
       gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(other_button), TRUE);
 
-    gnc_transfer_dialog_set_selected_account (xferData, other_acct, xferData->quickfill);
+    gnc_transfer_dialog_set_selected_account (xferData, other_acct, other_direction);
 
     changed = TRUE;
   }
