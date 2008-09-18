Gnucash/gnucash

commit 9173035d57600d464042ec6c13ee7b9c8f1720e8

Merge gda-dev2 branch into trunk.
This introduces the dbi backend and the --enable-dbi configure option.



git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@17444 57a11ea4-9604-0410-9ed3-97b8803252fd

src/gnome/dialog-price-editor.c
@@ -121,9 +121,9 @@ price_to_gui (PriceEditDialog *pedit_dialog)
   gnc_numeric value;
   Timespec date;
 
-  if (pedit_dialog->price)
+  commodity = gnc_price_get_commodity (pedit_dialog->price);
+  if (commodity)
   {
-    commodity = gnc_price_get_commodity (pedit_dialog->price);
     currency = gnc_price_get_currency (pedit_dialog->price);
     date = gnc_price_get_time (pedit_dialog->price);
     source = gnc_price_get_source (pedit_dialog->price);
@@ -132,11 +132,10 @@ price_to_gui (PriceEditDialog *pedit_dialog)
   }
   else
   {
-    commodity = NULL;
     currency = gnc_default_currency ();
     date.tv_sec = time (NULL);
     date.tv_nsec = 0;
-    source = "";
+    source = "user:price-editor";
     type = "";
     value = gnc_numeric_zero ();
   }
@@ -384,22 +383,6 @@ gnc_price_pedit_dialog_create (GtkWidget *parent,
 }
 
 static void
-gnc_price_new_price_init (GNCPrice *price)
-{
-  Timespec date;
-
-  gnc_price_begin_edit (price);
-
-  gnc_price_set_source (price, "user:price-editor");
-  date.tv_sec = time (NULL);
-  date.tv_nsec = 0;
-  gnc_price_set_time (price, date);
-
-  gnc_price_commit_edit (price);
-  
-}
-
-static void
 close_handler (gpointer user_data)
 {
   PriceEditDialog *pedit_dialog = user_data;
@@ -436,7 +419,7 @@ show_handler (const char *class, gint component_id,
  * Args:   parent  - the parent of the window to be created         *
  * Return: nothing                                                  *
 \********************************************************************/
-GNCPrice *
+void
 gnc_price_edit_dialog (GtkWidget * parent,
 		       QofSession *session,
 		       GNCPrice * price,
@@ -448,7 +431,7 @@ gnc_price_edit_dialog (GtkWidget * parent,
   if ((type == GNC_PRICE_EDIT) &&
       (gnc_forall_gui_components (DIALOG_PRICE_EDIT_CM_CLASS,
 				  show_handler, price)))
-      return(price);
+      return;
 
   pedit_dialog = g_new0 (PriceEditDialog, 1);
   gnc_price_pedit_dialog_create (parent, pedit_dialog, session);
@@ -461,7 +444,7 @@ gnc_price_edit_dialog (GtkWidget * parent,
     } else {
       price = gnc_price_create (pedit_dialog->book);
     }
-    gnc_price_new_price_init(price);
+
     pedit_dialog->new = TRUE;
     /* New price will only have one ref, this dialog. */
     break;
@@ -473,16 +456,12 @@ gnc_price_edit_dialog (GtkWidget * parent,
 
   pedit_dialog->price = price;
   price_to_gui(pedit_dialog);
-
   component_id = gnc_register_gui_component (DIALOG_PRICE_EDIT_CM_CLASS,
                                              refresh_handler, close_handler,
                                              pedit_dialog);
   gnc_gui_component_set_session (component_id, pedit_dialog->session);
-
   gtk_widget_grab_focus (pedit_dialog->commodity_edit);
-
   gtk_widget_show (pedit_dialog->dialog);
-  return(price);
 }
 
 /********************************************************************\
@@ -503,5 +482,6 @@ gnc_price_edit_by_guid (GtkWidget * parent, const GUID * guid)
   if (price == NULL)
     return(NULL);
 
-  return(gnc_price_edit_dialog(parent, session, price, GNC_PRICE_EDIT));
+  gnc_price_edit_dialog(parent, session, price, GNC_PRICE_EDIT);
+  return price;
 }

commit 018473e85503a7c449f9e0d23dc9d31c8e9bab4d

Price Editor: Set the correct source for new, user-entered prices. This builds on r17525 to fix this problem, which was introduced in r17444.
git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@17556 57a11ea4-9604-0410-9ed3-97b8803252fd

src/gnome/dialog-price-editor.c
@@ -46,6 +46,7 @@
 
 #define DIALOG_PRICE_EDIT_CM_CLASS "dialog-price-edit"
 #define GCONF_SECTION "dialogs/price_editor"
+#define DIALOG_PRICE_EDIT_SOURCE "user:price-editor"
 
 /* This static indicates the debugging module that this .o belongs to.  */
 /* static short module = MOD_GUI; */
@@ -135,7 +136,7 @@ price_to_gui (PriceEditDialog *pedit_dialog)
     currency = gnc_default_currency ();
     date.tv_sec = time (NULL);
     date.tv_nsec = 0;
-    source = "user:price-editor";
+    source = DIALOG_PRICE_EDIT_SOURCE;
     type = "";
     value = gnc_numeric_zero ();
   }
@@ -448,6 +449,7 @@ gnc_price_edit_dialog (GtkWidget * parent,
     } else {
       price = gnc_price_create (pedit_dialog->book);
     }
+    gnc_price_set_source (price, DIALOG_PRICE_EDIT_SOURCE);
 
     pedit_dialog->new = TRUE;
     /* New price will only have one ref, this dialog. */