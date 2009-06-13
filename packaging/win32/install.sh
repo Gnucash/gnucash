6ff4b733527c78ce80f9f592cb63b095ecbb0937
Derek Atkins <derek@ihtfp.com>
2009-06-13 21:50:40 +0000

    Properly reconfigure AqBanking 3.8.3 with the updated configure.ac
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@18129 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/install.sh
+++ packaging/win32/install.sh
@@ -987,6 +987,8 @@ function inst_aqbanking() {
                 inst_qt4
                 if [ -n "$AQBANKING_PATCH" -a -f "$AQBANKING_PATCH" ] ; then
                     patch -p1 < $AQBANKING_PATCH
+                    automake
+                    aclocal -I m4 ${ACLOCAL_FLAGS}
                     autoconf
                 fi
                 ./configure ${HOST_XCOMPILE} \
