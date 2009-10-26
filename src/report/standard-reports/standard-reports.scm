9f31693c7e0b3f68ff67ae2c671aa8b64431daf2
Christian Stimming <stimming@tuhh.de>
2009-10-26 20:15:47 +0000

    Bug #593906: Fix crash at account report
    Patch by Mike Alexander.
    
    This is the fixed version of r18352 which was reverted in r18360.
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@18396 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/standard-reports/standard-reports.scm
+++ src/report/standard-reports/standard-reports.scm
@@ -156,6 +156,9 @@
 (for-each
     (lambda (x) (resolve-module (append '(gnucash report standard-reports) (list x))))
 	(get-report-list))
+	
+; Need this to be able to call gnc:register-report-create-internal
+(use-modules (gnucash report standard-reports register))
 
 (use-modules (gnucash gnc-module))
 (gnc:module-load "gnucash/engine" 0)
