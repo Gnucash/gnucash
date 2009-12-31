ee8a1499e73079a114f696a293c303f36d28628a
Mike Alexander <mta@umich.edu>
2009-12-31 23:15:31 +0000

    Load gnc-build-url from the correct file so the accounts list option works again.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@18542 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/utility-reports/hello-world.scm
+++ src/report/utility-reports/hello-world.scm
@@ -12,7 +12,7 @@
 (debug-enable 'backtrace)
 
 (gnc:module-load "gnucash/report/report-system" 0)
-(gnc:module-load "gnucash/gnome-utils" 0) ;for gnc-build-url
+(gnc:module-load "gnucash/html" 0) ;for gnc-build-url
 
 ;; This function will generate a set of options that GnuCash
 ;; will use to display a dialog where the user can select
