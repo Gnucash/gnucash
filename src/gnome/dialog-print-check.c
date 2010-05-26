24787b7618bbe80fe818e2cd4224a21deb781c0c
Geert Janssens <janssens-geert@telenet.be>
2010-05-26 20:27:08 +0000

    Fix 64-bit compiler error introduced in r19206
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@19208 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/dialog-print-check.c
+++ src/gnome/dialog-print-check.c
@@ -951,7 +951,7 @@ format_read_item_placement(const gchar * file,
         if (error)
             goto failed;
         value = doubles_to_string(dd, dd_len);
-        g_debug("Check file %s, group %s, key %s, length %d; values: %s",
+        g_debug("Check file %s, group %s, key %s, length %"G_GSIZE_FORMAT"; values: %s",
                 file, KF_GROUP_ITEMS, key, dd_len, value);
         g_free(value);
 
@@ -1367,7 +1367,7 @@ format_read_general_info(const gchar * file,
     if (!error)
     {
         value = doubles_to_string(dd, dd_len);
-        g_debug("Check file %s, group %s, key %s, length %d; values: %s",
+        g_debug("Check file %s, group %s, key %s, length %"G_GSIZE_FORMAT"; values: %s",
                 file, KF_GROUP_TOP, KF_KEY_TRANSLATION, dd_len, value);
         g_free(value);
 
