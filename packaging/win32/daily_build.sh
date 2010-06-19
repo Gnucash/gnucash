ad768a3449d374b58f54f2370693d3bb6deacab3
Geert Janssens <janssens-geert@telenet.be>
2010-06-19 13:28:23 +0000

    Document what to do if svn isn't found. This only applies to the scripts used to setup an automated build system
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@19277 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/daily_build.sh
+++ packaging/win32/daily_build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# Note: for this script to work, you need to make sure bash can
+#       find svn on your system. If it's not in the default
+#       Windows path, you will have to add it yourself, for
+#       example like this:
+#       - create a file /etc/profile.d/svn.sh
+#       - add this line: export PATH=/c/soft/svn/bin:$PATH
+#       (Use the real path to your svn installation, obviously)
 
 set -e
 
