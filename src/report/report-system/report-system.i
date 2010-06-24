Gnucash/gnucash

commit 63f8b4b25757857b0464d0785982f4a867b24c8b

Bug #616606: Add gnc-get-default-report-font-family function callable from scm.
Currently returns "Arial" but can be modified to get default GtkWindow font.


git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@19285 57a11ea4-9604-0410-9ed3-97b8803252fd

src/report/report-system/report-system.i
@@ -11,3 +11,6 @@ SCM scm_init_sw_report_system_module (void);
 
 SCM gnc_report_find(gint id);
 gint gnc_report_add(SCM report);
+
+%newobject gnc_get_default_report_font_family;
+gchar* gnc_get_default_report_font_family();

src/report/report-system/gnc-report.c
@@ -195,3 +195,8 @@ gnc_report_name( SCM report )
     return g_strdup(scm_to_locale_string(value));
 }
 
+gchar*
+gnc_get_default_report_font_family(void)
+{
+    return g_strdup("Arial");
+}

src/report/report-system/gnc-report.h
@@ -46,4 +46,6 @@ gint gnc_report_add(SCM report);
 void gnc_reports_flush_global(void);
 GHashTable *gnc_reports_get_global(void);
 
+gchar* gnc_get_default_report_font_family(void);
+
 #endif

src/report/report-system/html-fonts.scm
@@ -60,7 +60,7 @@
         (
             (opt-register 
                 (lambda (opt) (gnc:register-option options opt)))
-            (font-family "Arial")
+            (font-family (gnc-get-default-report-font-family))
         )
         (opt-register
             (gnc:make-font-option

commit 9b29398cf9f88dd1045be995d5294af1a8462002

Fix build failure introduced by r19285 on systems with guile 1.6.
git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@19289 57a11ea4-9604-0410-9ed3-97b8803252fd

src/report/report-system/report-system.i
@@ -3,6 +3,7 @@
 /* Includes the header in the wrapper code */
 #include <config.h>
 #include <gnc-report.h>
+#include <guile-mappings.h>
 
 SCM scm_init_sw_report_system_module (void);
 %}