f8432c0cb60bbc38b996edb38d181cd72cbcac86
Christian Stimming <stimming@tuhh.de>
2010-08-29 19:43:05 +0000

    I18n: Fix last remaining user-visible mention of the abbreviation "SX".
    This should have never appeared in a user message. However, in order
    not to make this single string untranslated, we add the correct form
    but don't use it yet so that it can be translated, and we will be
    using it once 2.4.0 is out and the string freeze lifted.
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@19506 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome/dialog-sx-editor.c
+++ src/gnome/dialog-sx-editor.c
@@ -214,8 +214,15 @@ sxed_confirmed_cancel(GncSxEditorDialog *sxed)
     if ( gnc_sxed_check_changed( sxed ) )
     {
         const char *sx_changed_msg =
+#if 1
             _( "This SX has changed; are you "
                "sure you want to cancel?" );
+#else
+        /* Translators: This message is currently not yet used; it
+         * will be used once the 2.4.0 string freeze is lifted. */
+            _( "This Scheduled Transaction has changed; are you "
+               "sure you want to cancel?" );
+#endif
         if (!gnc_verify_dialog(sxed->dialog, FALSE, "%s", sx_changed_msg))
         {
             return FALSE;
