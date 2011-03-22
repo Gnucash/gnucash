117a73e700397f3c6d4cbd53811aaa750e28b784
Geert Janssens <janssens-geert@telenet.be>
2011-03-22 20:24:13 +0000

    Rework the Windows weekly 2.4 build to avoid parallel builds (which would fail BP (note that r20452 should be backported first, though I forgot to add the BP marker)
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@20454 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/daily_build.bat
+++ packaging/win32/daily_build.bat
@@ -3,5 +3,10 @@ rem It simply calls the actual MSYS Shell script to perform
 rem the daily build and then the tag builds.
 
 cd c:\soft\packaging
+
+rem Development build (daily)
 c:\msys\1.0\bin\sh.exe --login c:\soft\packaging\daily_build.sh
+rem Tags build (daily -- only tags that weren't built yet)
 c:\msys\1.0\bin\sh.exe --login c:\soft\packaging\build_tags.sh
+rem 2.4 branch build (weekly)
+c:\msys\1.0\bin\sh.exe --login c:\soft\packaging\daily_build.sh c:\soft-2.4\packaging
--- packaging/win32/weekly_build.bat
+++
@@ -1,6 +0,0 @@
-rem This is the Windows Batch Script for the daily builds.
-rem It simply calls the actual MSYS Shell script to perform
-rem the daily build and then the tag builds.
-
-cd c:\soft-2.4\packaging
-c:\msys\1.0\bin\sh.exe --login c:\soft-2.4\packaging\daily_build.sh
