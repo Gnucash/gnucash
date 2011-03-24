a3443e4f90f770d9f00ac8c558833d856707506d
Christian Stimming <stimming@tuhh.de>
2011-03-24 20:10:45 +0000

    Add code to start up a Python console during Py module init, but disabled by default.
    If a python console is wanted, change the last section
    of src/python/init.py to "if True:".
    
    From: Andy Clayton <q3aiml@gmail.com>
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@20471 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/python/init.py
+++ src/python/init.py
@@ -2,6 +2,12 @@ import sys
 import _sw_app_utils
 from gnucash import *
 
+import gtk
+import os
+sys.path.append(os.path.dirname(__file__))
+print "woop", os.path.dirname(__file__)
+import pycons.console as cons
+
 print "Hello from python!"
 
 print "test", sys.modules.keys()
@@ -19,5 +25,66 @@ print "test3", dir(acct)
 #print acct.GetBalance()
 #print acct.GetSplitList()
 
+
 #print "test2", dir(gnucash.gnucash_core_c)
 
+class Console (cons.Console):
+    """ GTK python console """
+
+    def __init__(self, argv=[], shelltype='python', banner=[],
+                 filename=None, size=100):
+        cons.Console.__init__(self, argv, shelltype, banner, filename, size)
+        self.buffer.create_tag('center',
+                               justification=gtk.JUSTIFY_CENTER,
+                               font='Mono 4')
+        self.figures = []
+        self.callbacks = []
+        self.last_figure = None
+        self.active_canvas = None
+        self.view.connect ('key-press-event', self.key_press_event)
+        self.view.connect ('button-press-event', self.button_press_event)
+        self.view.connect ('scroll-event', self.scroll_event)
+
+
+    def key_press_event (self, widget, event):
+        """ Handle key press event """
+        
+        if self.active_canvas:
+            self.active_canvas.emit ('key-press-event', event)
+            return True
+        return cons.Console.key_press_event (self, widget, event)
+
+    def scroll_event (self, widget, event):
+        """ Scroll event """
+        if self.active_canvas:
+            return True
+        return False
+ 
+    def button_press_event (self, widget, event):
+        """ Button press event """
+        return self.refresh()
+
+    def refresh (self):
+        """ Refresh drawing """
+        for fig in self.figures:
+            figure, canvas, anchor = fig
+            canvas.draw()
+        return False
+
+
+# Change this to "if True:" to switch on a python console at gnucash
+# startup:
+if False:
+    console = Console(argv = [], shelltype = 'python', banner = [['woop', 'title']], size = 100)
+
+    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    window.set_position(gtk.WIN_POS_CENTER)
+    window.set_default_size(800,600)
+    window.set_border_width(0)
+    # Hm. gtk.main_quit will kill gnucash without closing the file
+    # properly. That's kinda bad.
+    window.connect('destroy-event', gtk.main_quit)
+    window.connect('delete-event', gtk.main_quit)
+    window.add (console)
+    window.show_all()
+    console.grab_focus()
