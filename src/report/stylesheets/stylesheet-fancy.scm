eeb3250927fc89010217b87b12e717b686d97927
Christian Stimming <stimming@tuhh.de>
2011-05-15 19:28:04 +0000

    Bug #650163: Fix Technicolor style that outputs wrong tags <btd> instead of <b><td>
    Patch by Bert:
    
    The technicolor stylesheet outputs </btd> tags where what they actually meant
    to do was </b></td>. This may render report outputs wrongly in different
    browsers and prevents HTML validation.
    
    The proposed patch removes the bold tags as well (for example for the total
    rows) since the font and its weight are set in the options (and thus reflected
    in the CSS style). There is no need for the report to set the bold face with
    the <b> tag.
    
    In addition, the attached patch also closed the <center> tag at the beginning
    of the body (also preventing validation). Note that that this tag is deprecated
    for newer version of html (but will pass HTML 4.01 Transitional).
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@20647 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/stylesheets/stylesheet-fancy.scm
+++ src/report/stylesheets/stylesheet-fancy.scm
@@ -270,22 +270,22 @@
 
           (gnc:html-document-set-style!
              ssdoc "total-number-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "class" "total-number-cell"))
 
           (gnc:html-document-set-style!
              ssdoc "total-number-cell-neg"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "class" "total-number-cell neg"))
 
           (gnc:html-document-set-style!
              ssdoc "total-label-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "class" "total-label-cell"))
 
           (gnc:html-document-set-style!
              ssdoc "centered-label-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "class" "centered-label-cell"))
         )
         (begin ;; this is for gtkhtml
@@ -339,22 +339,22 @@
 
           (gnc:html-document-set-style!
              ssdoc "total-number-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "align" "right"))
 
           (gnc:html-document-set-style!
              ssdoc "total-number-cell-neg"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "align" "right"))
 
           (gnc:html-document-set-style!
              ssdoc "total-label-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "align" "left"))
 
           (gnc:html-document-set-style!
              ssdoc "centered-label-cell"
-             'tag '("td" "b")
+             'tag '("td")
              'attribute (list "align" "center"))
         )
     )
@@ -473,6 +473,8 @@
        (gnc:html-document-objects doc))
       
       (gnc:html-document-add-object! ssdoc t))
+    (gnc:html-document-add-object! ssdoc
+       (gnc:make-html-text "</center>")) ;;TODO: make this a div instead of <center> (deprecated)
     ssdoc))
 
 (gnc:define-html-style-sheet 
