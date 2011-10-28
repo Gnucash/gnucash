7fa08b00ca558ef43e0763a8f6dc203b679acc71
Geert Janssens <janssens-geert@telenet.be>
2011-10-28 14:14:03 +0000

    Remove the deprecated optionmenu widget based convenience functions. They are no longer used in the GnuCash code base.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@21503 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/business/business-gnome/business-gnome-utils.h
+++ src/business/business-gnome/business-gnome-utils.h
@@ -63,38 +63,6 @@ void gnc_fill_account_select_combo (GtkWidget *combo, QofBook *book,
                                     GList *acct_types,
                                     GList *acct_commodities);
 
-
-/* Create an optionmenu of available billing terms and attach it to
- * the menu passed in.  If none_ok is true, then add "none" as a
- * choice (with data set to NULL).  Any time the menu changes,
- * 'choice' will be set to the chosen option.  If *choice is non-NULL,
- * then that will be the default option setting when the menu is
- * created.
- */
-void gnc_ui_billterms_optionmenu (GtkWidget *omenu, QofBook *book,
-                                  gboolean none_ok, GncBillTerm **choice);
-
-/* Same thing except for the tax tables */
-void
-gnc_ui_taxtables_optionmenu (GtkWidget *omenu, QofBook *book,
-                             gboolean none_ok, GncTaxTable **choice);
-
-/* Build an option menu for choosing a GncTaxIncluded */
-void gnc_ui_taxincluded_optionmenu (GtkWidget *omenu, GncTaxIncluded *choice);
-
-
-/* Here are some "generic option menu" utilities that can be used with
- * ANY of the above option-menu types.  In particular the following
- * functions are useful for hooking the above option menus into the
- * GNC Option infrastructure.
- */
-
-void gnc_ui_optionmenu_set_changed_callback (GtkWidget *omenu,
-        void (*changed_cb)(GtkWidget*, gpointer),
-        gpointer cb_arg);
-gpointer gnc_ui_optionmenu_get_value (GtkWidget *omenu);
-void gnc_ui_optionmenu_set_value (GtkWidget *omenu, gpointer data);
-
 /* Create a combo box of available billing terms based on
  * the combo box If none_ok is true, then add "none" as a
  * choice (with data set to NULL).  If inital_choice is non-NULL,
