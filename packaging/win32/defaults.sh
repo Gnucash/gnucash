f338e3fe41a1a1e99e2990d75886c38c4e2076b7
Christian Stimming <stimming@tuhh.de>
2011-12-04 20:44:24 +0000

    Win32 build: Minor version upgrade of aqbanking and ktoblzcheck.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@21666 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/defaults.sh
+++ packaging/win32/defaults.sh
@@ -379,13 +379,13 @@ else
 fi
 set_default GWENHYWFAR_DIR $GLOBAL_DIR\\gwenhywfar
 
-KTOBLZCHECK_VERSION="1.36"
+KTOBLZCHECK_VERSION="1.37"
 set_default KTOBLZCHECK_URL "$SF_MIRROR/ktoblzcheck/ktoblzcheck-${KTOBLZCHECK_VERSION}.tar.gz"
 # ktoblzcheck is being installed into GWENHYWFAR_DIR
 
 if [ "$AQBANKING5" = "yes" ]; then
-    AQBANKING_VERSION="5.0.16"
-    set_default AQBANKING_URL "http://www2.aquamaniac.de/sites/download/download.php?package=03&release=87&file=01&dummy=aqbanking-${AQBANKING_VERSION}.tar.gz"
+    AQBANKING_VERSION="5.0.17"
+    set_default AQBANKING_URL "http://www2.aquamaniac.de/sites/download/download.php?package=03&release=88&file=01&dummy=aqbanking-${AQBANKING_VERSION}.tar.gz"
     set_default AQBANKING_WITH_QT no
 else
     AQBANKING_VERSION="4.2.4"
