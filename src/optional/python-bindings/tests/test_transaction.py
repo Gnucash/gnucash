1619cb7f0aa3a6bd12330708efb10f0941a42273
John Ralls <jralls@ceridwen.us>
2011-12-12 22:41:51 +0000

    [Testing] Suppress expected messages in optional/python-bindings tests
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@21725 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/optional/python-bindings/tests/test_transaction.py
+++ src/optional/python-bindings/tests/test_transaction.py
@@ -1,11 +1,31 @@
 from unittest import main
 
 from gnucash import Transaction, Book, Account, Split
+from test_stuff import *
 
 from test_book import BookSession
 
 class TransactionSession( BookSession ):
     def setUp(self):
+        self.domain1 = "gnc.engine"
+        self.domain2 = "gnc.engine.scrub"
+        level =  G_LOG_LEVEL_CRITICAL
+        check1 = TestErrorStruct()
+        check1.log_domain = self.domain1
+        check1.log_level = level
+        check1.msg = "[xaccScrubUtilityGetOrMakeAccount()] No currency specified!"
+        test_add_error(check1)
+        check2 = TestErrorStruct()
+        check2.log_domain = self.domain1
+        check2.log_level = level
+        check2.msg = "[xaccTransScrubSplits()] Transaction doesn't have a currency!"
+        test_add_error(check2)
+        self.hdlr1 = test_set_list_handler(self.domain1, level, None)
+        check3 = TestErrorStruct()
+        check3.log_domain = "gnc.engine.scrub"
+        check3.log_level = level
+        check3.msg = "[xaccScrubUtilityGetOrMakeAccount()] No currency specified!"
+        self.hdlr2 = test_set_checked_handler(self.domain2, level, check3)
         BookSession.setUp(self)
         self.trans = Transaction(self.book)
         #Evil bug means we must set a split for the transaction before making
@@ -15,17 +35,32 @@ class TransactionSession( BookSession ):
         ############
         self.trans.SetCurrency(self.currency)
 
+    def tearDown(self):
+        g_log_remove_handler(self.domain1, self.hdlr1)
+        g_log_remove_handler(self.domain2, self.hdlr2)
+        test_clear_error_list ()
+
 class TestTransaction( TransactionSession ):
     def test_equal(self):
         TRANS = self.trans
         self.assertTrue( TRANS.Equal(self.trans, True, False, False, False) )
 
     def test_clone(self):
+        domain = "gnc.engine"
+        level =  G_LOG_LEVEL_WARNING
+        check = TestErrorStruct()
+        check.log_domain = domain
+        check.log_level = level
+        check.msg = "[xaccTransEqual()] GUIDs differ"
+        hdlr = test_set_checked_handler(domain, level, check)
+
         TRANS = self.trans.Clone()
-        #Clone and original should have different GUIDs
+       #Clone and original should have different GUIDs
         self.assertFalse( TRANS.Equal(self.trans, True, False, False, False) )
         #Clone and original should have the same balance
         self.assertTrue( TRANS.Equal(self.trans, False, False, True, False) )
+
+        g_log_remove_handler(domain, hdlr)
 
     def test_setcurrency(self):
         self.assertTrue( self.currency.equal( self.trans.GetCurrency() ) )
