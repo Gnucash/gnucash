5945b199135d1332bb4c181a9d0dd5b69c4a58cb
Geert Janssens <janssens-geert@telenet.be>
2011-12-19 15:46:51 +0000

    [21740]Replace no-op function force_account_dirty with qof_book_mark_dirty See bug #665998 for more details.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/2.4@21754 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/import-export/aqbanking/gnc-ab-kvp.c
+++ src/import-export/aqbanking/gnc-ab-kvp.c
@@ -41,7 +41,6 @@
 /* This static indicates the debugging module that this .o belongs to.  */
 static QofLogModule log_module = G_LOG_DOMAIN;
 
-static void force_account_dirty(Account *acct);
 static kvp_frame *gnc_ab_get_account_kvp(const Account *a, gboolean create);
 static kvp_frame *gnc_ab_get_book_kvp(QofBook *b, gboolean create);
 
@@ -60,7 +59,7 @@ gnc_ab_set_account_accountid(Account *a, const gchar *id)
     kvp_value *value = kvp_value_new_string(id);
     xaccAccountBeginEdit(a);
     kvp_frame_set_slot_nc(frame, AB_ACCOUNT_ID, value);
-    force_account_dirty(a);
+    qof_book_mark_dirty(gnc_get_current_book());
     xaccAccountCommitEdit(a);
 }
 
@@ -79,7 +78,7 @@ gnc_ab_set_account_bankcode(Account *a, const gchar *code)
     kvp_value *value = kvp_value_new_string(code);
     xaccAccountBeginEdit(a);
     kvp_frame_set_slot_nc(frame, AB_BANK_CODE, value);
-    force_account_dirty(a);
+    qof_book_mark_dirty(gnc_get_current_book());
     xaccAccountCommitEdit(a);
 }
 
@@ -98,7 +97,7 @@ gnc_ab_set_account_uid(Account *a, guint32 uid)
     kvp_value *value = kvp_value_new_gint64(uid);
     xaccAccountBeginEdit(a);
     kvp_frame_set_slot_nc(frame, AB_ACCOUNT_UID, value);
-    force_account_dirty(a);
+    qof_book_mark_dirty(gnc_get_current_book());
     xaccAccountCommitEdit(a);
 }
 
@@ -117,7 +116,7 @@ gnc_ab_set_account_trans_retrieval(Account *a, Timespec time)
     kvp_value *value = kvp_value_new_timespec(time);
     xaccAccountBeginEdit(a);
     kvp_frame_set_slot_nc(frame, AB_TRANS_RETRIEVAL, value);
-    force_account_dirty(a);
+    qof_book_mark_dirty(gnc_get_current_book());
     xaccAccountCommitEdit(a);
 }
 
@@ -138,19 +137,6 @@ gnc_ab_set_book_template_list(QofBook *b, GList *template_list)
     kvp_frame_set_slot_nc(frame, AB_TEMPLATES, value);
     qof_book_mark_dirty(b);
     qof_book_commit_edit(b);
-}
-
-static void
-force_account_dirty(Account *acct)
-{
-    gchar *name = g_strdup(xaccAccountGetName(acct));
-
-    /* This is necessary because modifying the KvpFrames doesn't mark
-     * accounts dirty, which means the changes wont be propagated to the
-     * backend.
-     */
-    xaccAccountSetName(acct, name);
-    g_free(name);
 }
 
 static kvp_frame *
