7053ea17f007845ffda9c9c528e30bddc2dc961d
Christian Stimming <stimming@tuhh.de>
2012-02-24 20:16:21 +0000

    [22063] Bug #670727: Clarify license of a file to be GPLv2 and GPLv3
    gnc-dense-cal.h: License remark was forgotten in r19507, but the author
    Josh Sled agreed on GPLv2 and GPLv3 licensing in his gnucash-devel
    message on 2010-08-28.
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/branches/2.4@22064 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/gnc-dense-cal.h
+++ src/gnome-utils/gnc-dense-cal.h
@@ -4,7 +4,7 @@
  *                                                                  *
  * This program is free software; you can redistribute it and/or    *
  * modify it under the terms of the GNU General Public License as   *
- * published by the Free Software Foundation, under version 2 of    *
+ * published by the Free Software Foundation, under version 2 and/or version 3 of    *
  * the License.                                                     *
  *                                                                  *
  * This program is distributed in the hope that it will be useful,  *
