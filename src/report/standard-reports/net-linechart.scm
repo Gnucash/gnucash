331bcf384bd5e8ec38e1c36222a5ea88ff3a6aae
Mike Evans <mikee@saxicola.co.uk>
2012-06-16 11:52:51 +0000

    Add formatting to table.
    Add cell padding to columns in table.
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22222 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/report/standard-reports/net-linechart.scm
+++ src/report/standard-reports/net-linechart.scm
@@ -416,6 +416,11 @@
            (gnc:html-document-add-object! document chart)
              (if show-table?
              (let ((table (gnc:make-html-table)))
+                (gnc:html-table-set-style!
+                  table "table"
+                  'attribute (list "border" 0)
+                  'attribute (list "cellspacing" 0)
+                  'attribute (list "cellpadding" 4))
                 (gnc:html-table-set-col-headers!
                  table
                  (append
