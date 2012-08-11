50f62a5516a69d873c6dbb30e83a2c0aa8e9d098
Geert Janssens <janssens-geert@telenet.be>
2012-08-11 14:24:45 +0000

    First attempt at rewriting the win32 automated build scripts for git
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22304 57a11ea4-9604-0410-9ed3-97b8803252fd

---
+++ packaging/win32/daily_build_git.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Note: for this script to work, git must have been setup before
+#       in a way that doesn't conflict with the GnuCash build.
+#       The easiest way to do so is to run the build once manually
+#       with a properly set up custom.sh.
+
+set -e
+
+function qpushd() { pushd "$@" >/dev/null; }
+function qpopd() { popd >/dev/null; }
+function unix_path() { echo "$*" | sed 's,^\([A-Za-z]\):,/\1,;s,\\,/,g'; }
+
+qpushd "$(dirname $(unix_path "$0"))"
+. functions.sh
+. defaults.sh
+
+# Variables
+_GIT_UDIR=`unix_path $GIT_DIR`
+set_env "$_GIT_UDIR/bin/git" GIT_CMD
+export $GIT_CMD
+
+$GIT_CMD pull
+./build_package_git.sh
+qpopd
---
+++ packaging/win32/weekly_build_git.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Note: for this script to work, git must have been setup before
+#       in a way that doesn't conflict with the GnuCash build.
+#       The easiest way to do so is to run the build once manually
+#       with a properly set up custom.sh.
+
+set -e
+
+## Only run this script on Monday night (first day of the week)
+if [ `date +%u` != 1 ] ; then exit ; fi
+
+function qpushd() { pushd "$@" >/dev/null; }
+function qpopd() { popd >/dev/null; }
+function unix_path() { echo "$*" | sed 's,^\([A-Za-z]\):,/\1,;s,\\,/,g'; }
+
+qpushd "$(dirname $(unix_path "$0"))"
+. functions.sh
+. defaults.sh
+
+# Variables
+_GIT_UDIR=`unix_path $GIT_DIR`
+set_env "$_GIT_UDIR/bin/git" GIT_CMD
+export $GIT_CMD
+
+$GIT_CMD pull
+./build_package.sh
+qpopd
