dc50587f55b998bbf3447e38c75ea6bcef1a7172
Geert Janssens <janssens-geert@telenet.be>
2012-08-17 20:30:41 +0000

    Win32 nightly: match built package name to the one expected for upload
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22329 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/dist-impl.sh
+++ packaging/win32/dist-impl.sh
@@ -310,8 +310,11 @@ function dist_finish() {
     if [ "$BUILD_FROM_TARBALL" = "no" ]; then
         # And changing output filename
         PKG_VERSION=`grep PACKAGE_VERSION ${_BUILD_UDIR}/config.h | cut -d" " -f3 | cut -d\" -f2 `
-        SVN_REV=`grep GNUCASH_SVN_REV ${_BUILD_UDIR}/src/core-utils/gnc-svninfo.h | cut -d" " -f3 | cut -d\" -f2 `
-        SETUP_FILENAME="gnucash-${PKG_VERSION}-${REPOS_TYPE}-r${SVN_REV}-setup.exe"
+        REVISION=`grep GNUCASH_SVN_REV ${_BUILD_UDIR}/src/core-utils/gnc-svninfo.h | cut -d" " -f3 | cut -d\" -f2 `
+        if [ "$REPOS_TYPE" = "svn" ]; then
+          SETUP_FILENAME="gnucash-${PKG_VERSION}-${REPOS_TYPE}-r${REVISION}-setup.exe"
+        else
+          SETUP_FILENAME="gnucash-${PKG_VERSION}-$(date +'%Y-%m-%d')-${REPOS_TYPE}-${REVISION}-setup.exe"
         qpushd ${_GNUCASH_UDIR}
             mv gnucash-${PKG_VERSION}-setup.exe ${SETUP_FILENAME}
         qpopd
