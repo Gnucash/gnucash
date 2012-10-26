8573f4576e715d9d00dae41f8cd10299c52a4ef5
Geert Janssens <janssens-geert@telenet.be>
2012-10-26 08:49:41 +0000

    Add unittests for mysql and postgresql backends
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22449 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/backend/dbi/test/utest-backend-dbi-basic.c
+++ src/backend/dbi/test/utest-backend-dbi-basic.c
@@ -17,6 +17,8 @@ static const gchar* suitename = "/backend/dbi";
 void test_suite_gnc_backend_dbi_basic(void);
 
 void do_test_sqlite(void);
+void do_test_mysql(void);
+void do_test_pgsql(void);
 
 #define DBI_TEST_XML_FILENAME "test-dbi.xml"
 #define FILE_NAME "sqlite3:///tmp/test-sqlite3-file"
@@ -46,7 +48,45 @@ do_test_sqlite(void)
 }
 
 void
+do_test_mysql(void)
+{
+    gchar* filename;
+    QofSession* session_1;
+
+    g_test_log_set_fatal_handler(handler, 0);
+
+    // Create a session with data
+    session_1 = qof_session_new();
+    qof_session_begin( session_1, DBI_TEST_XML_FILENAME, FALSE, FALSE, FALSE );
+    qof_session_load( session_1, NULL );
+
+    g_test_message ( "Using database: %s\n", TEST_MYSQL_URL );
+    test_dbi_store_and_reload( "mysql", session_1, TEST_MYSQL_URL );
+}
+
+void
+do_test_pgsql(void)
+{
+    gchar* filename;
+    QofSession* session_1;
+
+    g_test_log_set_fatal_handler(handler, 0);
+
+    // Create a session with data
+    session_1 = qof_session_new();
+    qof_session_begin( session_1, DBI_TEST_XML_FILENAME, FALSE, FALSE, FALSE );
+    qof_session_load( session_1, NULL );
+
+    g_test_message ( "Using database: %s\n", TEST_PGSQL_URL );
+    test_dbi_store_and_reload( "postgres", session_1, TEST_PGSQL_URL );
+}
+
+void
 test_suite_gnc_backend_dbi_basic(void)
 {
     GNC_TEST_ADD_FUNC(suitename, "gnc dbi test sqlite", do_test_sqlite);
+    if ( strlen( TEST_MYSQL_URL ) > 0 )
+        GNC_TEST_ADD_FUNC(suitename, "gnc dbi test mysql", do_test_mysql);
+    if ( strlen( TEST_PGSQL_URL ) > 0 )
+        GNC_TEST_ADD_FUNC(suitename, "gnc dbi test pgsql", do_test_pgsql);
 }
