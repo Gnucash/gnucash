96f660eeb1ee4eb6e0db4c912f6acb668aedb97d
Christian Stimming <stimming@tuhh.de>
2013-04-24 09:08:31 +0000

    Finally fix the maddening long waiting times after clicking "Ok" in the import matcher window.
    Turns out we were simply missing a suspend_gui_refresh call before actually
    processing the results. Sigh.
    
    (Can go into 2.4 because the bugfix is so surprisingly simple.)
    BP
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22929 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/import-export/import-main-matcher.c
+++ src/import-export/import-main-matcher.c
@@ -48,6 +48,7 @@
 #include "import-match-picker.h"
 #include "import-backend.h"
 #include "import-account-matcher.h"
+#include "app-utils/gnc-component-manager.h"
 
 #define GCONF_SECTION "dialogs/import/generic_matcher/transaction_list"
 
@@ -164,6 +165,10 @@ on_matcher_ok_clicked (GtkButton *button,
     if (!gtk_tree_model_get_iter_first(model, &iter))
         return;
 
+    /* Don't run any queries and/or split sorts while processing the matcher
+    results. */
+    gnc_suspend_gui_refresh();
+
     do
     {
         gtk_tree_model_get(model, &iter,
@@ -186,6 +191,9 @@ on_matcher_ok_clicked (GtkButton *button,
         }
     }
     while (gtk_tree_model_iter_next (model, &iter));
+
+    /* Allow GUI refresh again. */
+    gnc_resume_gui_refresh();
 
     gnc_gen_trans_list_delete (info);
     /* DEBUG ("End") */
