1b80da7f94f211aecee85f890de7ca675f1e9980
Geert Janssens <janssens-geert@telenet.be>
2013-05-03 12:04:03 +0000

    Bug #698781 can't build with glib 2.36: error: 'g_type_init' is deprecated
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22944 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/libqof/qof/qofutil.c
+++ src/libqof/qof/qofutil.c
@@ -258,7 +258,9 @@ static gboolean qof_util_str_equal(gconstpointer v, gconstpointer v2)
 void
 qof_init (void)
 {
-    g_type_init();
+#ifndef HAVE_GLIB_2_36
+    g_type_init(); /* Automatic as of GLib 2.36 */
+#endif
     qof_log_init();
     qof_string_cache_init();
     guid_init ();
