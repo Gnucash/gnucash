2d15a1bc73b659b22fa275ef4f72cdc842b69b74
John Ralls <jralls@ceridwen.us>
2013-05-04 22:28:35 +0000

    Allow svnlog2ul.sh to run on the current working copy
    And from the root directory instead of having to cd to util/svnlog2ul
    
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@22948 57a11ea4-9604-0410-9ed3-97b8803252fd

--- util/svnlog2ul/svnlog2ul.sh
+++ util/svnlog2ul/svnlog2ul.sh
@@ -2,8 +2,9 @@
 #
 # svnlog2ul.sh <prevrelease> <newrelease>
 #
-# This script will extract all the svn commit messages
-# from the repository between two releases.
+# This script will extract all the svn commit messages from the
+# repository between two releases or from a previous release and the
+# current checkout's HEAD.
 # The result will be printed on standard out as a
 # html unordered list ("bulleted list").
 #
@@ -17,14 +18,25 @@
 # The search will return all commit messages between
 # <prevrelease> and <newrelease>
 #
-# Example:
-# 
+# Examples:
+# This will compile the changes between two tagged releases:
 #    svnlog2ul.sh 2.3.7 2.3.8
+# This will compile the changes between the tagged release and the
+# current HEAD in checked out working copy:
+#    svnlog2ul.sh 2.4.7
 
 oldrelease=$1
 newrelease=$2
 
-oldrev=$(svn info --xml http://svn.gnucash.org/repo/gnucash/tags/$oldrelease | xsltproc getlastcommit.xslt -)
-newrev=$(svn info --xml http://svn.gnucash.org/repo/gnucash/tags/$newrelease | xsltproc getlastcommit.xslt -)
+dir=`dirname "$0"`
 
-svn log -r$newrev:$oldrev --xml http://svn.gnucash.org/repo/gnucash/tags/$newrelease | xsltproc log2ul.xslt -
+oldrev=$(svn info --xml http://svn.gnucash.org/repo/gnucash/tags/$oldrelease | xsltproc "$dir/getlastcommit.xslt" -)
+if [ "x$2" == "x" ]
+then
+  newrev=$(svn info -r HEAD --xml | xsltproc "$dir/getlastcommit.xslt" -)
+  svn log -r$newrev:$oldrev --xml | xsltproc "$dir/log2ul.xslt" -
+else
+  newrev=$(svn info --xml http://svn.gnucash.org/repo/gnucash/tags/$newrelease | xsltproc "$dir/getlastcommit.xslt" -)
+  svn log -r$newrev:$oldrev --xml http://svn.gnucash.org/repo/gnucash/tags/$newrelease | xsltproc "$dir/log2ul.xslt" -
+fi
+
