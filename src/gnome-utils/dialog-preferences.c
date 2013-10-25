d429a83792d5ff969902f99375e9c6cb38ac24ff
Geert Janssens <janssens-geert@telenet.be>
2013-10-25 19:03:00 +0000

    Avoid the need for a duplicate dialog-preferences.glade
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@23331 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/gnome-utils/dialog-preferences.c
+++ src/gnome-utils/dialog-preferences.c
@@ -1071,32 +1071,35 @@ gnc_preferences_dialog_create(void)
     DEBUG("Opening dialog-preferences.glade:");
     builder = gtk_builder_new();
 
-#ifdef REGISTER2_ENABLED
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "auto_decimal_places_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "autosave_interval_minutes_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "save_on_close_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "date_backmonth_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "max_transactions_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "key_length_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "new_search_limit_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "retain_days_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "tab_width_adj");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "date_formats");
-    gnc_builder_add_from_file (builder, "dialog-preferences2.glade", "GnuCash Preferences");
-#else
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "auto_decimal_places_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "autosave_interval_minutes_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "save_on_close_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "date_backmonth_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "max_transactions_adj");
+    gnc_builder_add_from_file (builder, "dialog-preferences.glade", "key_length_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "new_search_limit_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "retain_days_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "tab_width_adj");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "date_formats");
     gnc_builder_add_from_file (builder, "dialog-preferences.glade", "GnuCash Preferences");
-#endif
 
     dialog = GTK_WIDGET(gtk_builder_get_object (builder, "GnuCash Preferences"));
+
+#ifndef REGISTER2_ENABLED
+    /* Hide preferences that are related to register2 */
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "label14"));
+    gtk_widget_hide (box);
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "pref/general.register/key-length"));
+    gtk_widget_hide (box);
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "pref/general.register/show-extra-dates"));
+    gtk_widget_hide (box);
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "pref/general.register/show-calendar-buttons"));
+    gtk_widget_hide (box);
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "pref/general.register/selection-to-blank-on-expand"));
+    gtk_widget_hide (box);
+    box = GTK_WIDGET (gtk_builder_get_object (builder, "pref/general.register/show-extra-dates-on-selection"));
+    gtk_widget_hide (box);
+#endif
 
     label = GTK_WIDGET(gtk_builder_get_object (builder, "sample_account"));
     g_object_set_data(G_OBJECT(dialog), "sample_account", label);
