679e78e45c6c57787fb87a6e57f95c70cb5ac281
Mike Alexander <mta@umich.edu>
2013-11-16 22:59:00 +0000

    Turn off the scheme compiler's "possibly unbound variable" warnings. In guile 2.0 we get nearly 7500 of them loading the scheme files.
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@23402 57a11ea4-9604-0410-9ed3-97b8803252fd

--- src/scm/main.scm
+++ src/scm/main.scm
@@ -23,6 +23,14 @@
          (>= (string->number (minor-version)) 8))
     (default-duplicate-binding-handler 'last))
 
+;; Turn off the scheme compiler's "possibly unbound variable" warnings.
+;; In guile 2.0 we get nearly 7500 of them loading the scheme files.
+;; This is the default value for auto-compilation-options without "unbound-variable".
+;; See module/ice-9/boot-9.scm  */
+(if (>= (string->number (major-version)) 2)
+    (set! %auto-compilation-options 
+          '(#:warnings (arity-mismatch format duplicate-case-datum bad-case-datum))))
+
 (use-modules (gnucash core-utils))
 
 ;; Load the srfis (eventually, we should see where these are needed
