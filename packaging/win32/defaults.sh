b057cf0b45e98621d515a3ac8ae0a13aff16fe17
Christian Stimming <stimming@tuhh.de>
2014-01-07 10:30:24 +0000

    Win32 build: Update libofx version to use latest bugfixes there
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@23671 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/defaults.sh
+++ packaging/win32/defaults.sh
@@ -337,7 +337,7 @@ set_default OPENSP_URL "$SF_MIRROR/openjade/OpenSP-1.5.2.tar.gz"
 set_default OPENSP_DIR $GLOBAL_DIR\\opensp
 set_default OPENSP_PATCH `pwd`/opensp-1.5.2.patch
 
-LIBOFX_VERSION="0.9.8"
+LIBOFX_VERSION="0.9.9"
 set_default LIBOFX_URL "$SF_MIRROR/libofx/libofx-${LIBOFX_VERSION}.tar.gz"
 set_default LIBOFX_DIR $GLOBAL_DIR\\libofx
 #set_default LIBOFX_PATCH `pwd`/libofx-x.x.x.patch
