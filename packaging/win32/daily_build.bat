706017a86d1797a583f6e98eab817dc54d17bd3a
Geert Janssens <janssens-geert@telenet.be>
2014-01-26 19:11:44 +0000

    Win32: update daily_build.bat rules
    Stop building svn based tags. Those were for 2.4 and older
    Prepare to do weekly 2.6 builds once we that branch is created
    
    git-svn-id: svn+ssh://svn.gnucash.org/repo/gnucash/trunk@23775 57a11ea4-9604-0410-9ed3-97b8803252fd

--- packaging/win32/daily_build.bat
+++ packaging/win32/daily_build.bat
@@ -6,9 +6,8 @@ cd c:\soft\gnucash\repos\packaging\win32\
 
 rem Development build (daily)
 c:\msys\1.0\bin\sh.exe --login c:\soft\gnucash\repos\packaging\win32\daily_build_git.sh
-rem Tags build for 2.4 and older (daily -- only tags that weren't built yet)
-c:\msys\1.0\bin\sh.exe --login c:\soft\packaging\build_tags.sh
 rem Tags build for 2.5 and newer (daily -- only tags that weren't built yet)
 c:\msys\1.0\bin\sh.exe --login c:\soft\gnucash\repos\packaging\win32\build_tags_git.sh
-rem 2.4 branch build (weekly)
-c:\msys\1.0\bin\sh.exe --login c:\soft-2.4\packaging\weekly_build.sh
+rem 2.6 branch build (weekly)
+rem There's no 2.6 branch yet. Hence the line below is commented out
+rem c:\msys\1.0\bin\sh.exe --login c:\soft-2.6\gnucash\repos\packaging\weekly_build.sh
