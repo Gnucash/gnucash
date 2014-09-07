671551585eb636554f955f4ed095d5d8106bdd8d
Christian Stimming <christian@cstimming.de>
2014-09-07 22:58:07 +0200

    Add failing unittest for aqbanking lookup of trans_retrieval date.
    The lookup of the "ab-trans-retrieval" property somehow fails to
    return the correct value. This is somewhat surprising as this
    property is already checked in the test-engine-kvp-properties.c and
    there it works fine. What's the problem here...?

--- src/import-export/aqb/test/test-kvp.c
+++ src/import-export/aqb/test/test-kvp.c
@@ -35,6 +35,7 @@ static char* get_filepath(const char* filename)
     const char *srcdir = g_getenv("SRCDIR");
     if (!srcdir)
     {
+        g_test_message("No env variable SRCDIR exists, assuming \".\"\n");
         srcdir = ".";
     }
 
@@ -154,6 +155,54 @@ test_qofsession_aqb_kvp( void )
                 g_assert_cmpstr(gnc_ab_trans_templ_get_name(templ), ==, CHANGED_NAME); // ok, the change has been saved!
                 gnc_ab_trans_templ_list_free(templ_list);
             }
+        }
+
+        {
+            // Check the kvp slots of a aqbanking-enabled account
+            QofBook *book = qof_session_get_book(new_session);
+            Account* account = gnc_book_get_root_account(book);
+            GDate retrieved_date, original_date;
+            gchar buff[MAX_DATE_LENGTH];
+
+            g_assert(account);
+
+            if (0)
+            {
+                Timespec retrieved_ts = gnc_ab_get_account_trans_retrieval(account);
+                g_test_message("retrieved_ts=%s\n", gnc_print_date(retrieved_ts));
+                printf("Time=%s\n", gnc_print_date(retrieved_ts));
+
+                retrieved_date = timespec_to_gdate(retrieved_ts);
+                g_date_set_dmy(&original_date, 29, 8, 2014);
+
+                g_assert_cmpint(g_date_compare(&retrieved_date, &original_date), ==, 0);
+            }
+
+            if (1)
+            {
+                Timespec original_ts = timespec_now(), retrieved_ts;
+
+                // Check whether the "ab-trans-retrieval" property of Account
+                // is written and read again correctly.
+                gnc_ab_set_account_trans_retrieval(account, original_ts);
+                retrieved_ts = gnc_ab_get_account_trans_retrieval(account);
+
+                printf("original_ts=%s = %d  retrieved_ts=%s = %d\n",
+                       gnc_print_date(original_ts), original_ts.tv_sec,
+                       gnc_print_date(retrieved_ts), retrieved_ts.tv_sec);
+
+                original_date = timespec_to_gdate(original_ts);
+                retrieved_date = timespec_to_gdate(retrieved_ts);
+
+                qof_print_gdate (buff, sizeof (buff), &original_date);
+                printf("original_date=%s\n", buff);
+                qof_print_gdate (buff, sizeof (buff), &retrieved_date);
+                printf("retrieved_date=%s\n", buff);
+
+                // Is the retrieved date identical to the one written
+                g_assert_cmpint(g_date_compare(&retrieved_date, &original_date), ==, 0);
+            }
+
         }
 
         g_free(newfile);
