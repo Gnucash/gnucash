79ad39095c95b3785d703d59542586337ea32eec
Mike Alexander <mta@umich.edu>
2014-11-24 01:50:08 -0500

    book.not_saved should be book.session_not_saved

--- src/optional/python-bindings/example_scripts/simple_book.py
+++ src/optional/python-bindings/example_scripts/simple_book.py
@@ -16,10 +16,10 @@ book = ses.get_book()
 
 #Call some methods that produce output to show that Book works
 book.get_root_account().SetDescription("hello, book")
-print "Book is saved:", not book.not_saved()
+print "Book is saved:", not book.session_not_saved()
 
 print "saving..."
 ses.save()
 
-print "Book is saved:", not book.not_saved()
+print "Book is saved:", not book.session_not_saved()
 ses.end()
