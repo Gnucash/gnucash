9e142124f4f1187446d3d7f7dab5e691a5f6da66
John Ralls <jralls@ceridwen.us>
2015-06-22 15:00:10 -0700

    Convert kvp-scm to C++ and to using the KVP C++ API.
    The binding remains C to simplify interaction of these two functions with
    SWIG and Scheme.

--- src/engine/kvp-scm.cpp
+++ src/engine/kvp-scm.cpp
@@ -1,14 +1,19 @@
+#include <libguile.h>
+
+extern "C"
+{
 #include "config.h"
 
 #include <qof.h>
-#include <kvp_frame.h>
-#include <libguile.h>
 #include "engine-helpers-guile.h"
 
-#include "kvp-scm.h"
 #include "guile-mappings.h"
 #include "gnc-guile-utils.h"
 #include "swig-runtime.h"
+#include "kvp-scm.h"
+}
+
+#include <kvp_frame.hpp>
 
 /* NOTE: There are some problems with this approach. Currently,
  *       guids are stored simply as strings in scheme, so some
@@ -25,43 +30,40 @@ gnc_scm_to_kvp_value_ptr(SCM val)
         /* in guile 1.8 (exact? ) only works on numbers */
         if (scm_is_exact (val) && gnc_gh_gint64_p(val))
         {
-            return kvp_value_new_gint64(scm_to_int64(val));
+            return new KvpValue{scm_to_int64(val)};
         }
         else
         {
-            return kvp_value_new_double(scm_to_double(val));
+            return new KvpValue{scm_to_double(val)};
         }
     }
     else if (gnc_numeric_p(val))
     {
-        return kvp_value_new_gnc_numeric(gnc_scm_to_numeric(val));
+        return new KvpValue{gnc_scm_to_numeric(val)};
     }
     else if (gnc_guid_p(val))
     {
-        GncGUID tmpguid = gnc_scm2guid(val);
-        return kvp_value_new_guid(&tmpguid);
+        auto guid = gnc_scm2guid(val);
+        auto tmpguid = guid_copy(&guid);
+        return new KvpValue{tmpguid};
     }
     else if (gnc_timepair_p(val))
     {
         Timespec ts = gnc_timepair2timespec(val);
-        return kvp_value_new_timespec(ts);
+        return new KvpValue{ts};
     }
     else if (scm_is_string(val))
     {
-        gchar *newstr;
-        KvpValue *ret;
-        newstr = gnc_scm_to_utf8_string (val);
-        ret = kvp_value_new_string(newstr);
-        g_free (newstr);
-        return ret;
+        return new KvpValue{gnc_scm_to_utf8_string(val)};
     }
     else if (SWIG_IsPointerOfType(val, SWIG_TypeQuery("_p_KvpFrame")))
     {
 #define FUNC_NAME G_STRFUNC
-        KvpFrame *frame = SWIG_MustGetPtr(val, SWIG_TypeQuery("_p_KvpFrame"),
-                                          1, 0);
+        auto vp_frame = SWIG_MustGetPtr(val,
+                                        SWIG_TypeQuery("_p_KvpFrame"), 1, 0);
+        KvpFrame *frame = static_cast<KvpFrame*>(vp_frame);
 #undef FUNC_NAME
-        return kvp_value_new_frame (frame);
+        return new KvpValue{frame};
     }
     /* FIXME: add list handler here */
     return NULL;
@@ -70,41 +72,42 @@ gnc_scm_to_kvp_value_ptr(SCM val)
 SCM
 gnc_kvp_value_ptr_to_scm(KvpValue* val)
 {
-    const gchar *string;
     switch (kvp_value_get_type(val))
     {
     case KVP_TYPE_GINT64:
-        return scm_from_int64(kvp_value_get_gint64(val));
+        return scm_from_int64(val->get<int64_t>());
         break;
     case KVP_TYPE_DOUBLE:
-        return scm_from_double (kvp_value_get_double(val));
+        return scm_from_double (val->get<double>());
         break;
     case KVP_TYPE_NUMERIC:
-        return gnc_numeric_to_scm(kvp_value_get_numeric(val));
+        return gnc_numeric_to_scm(val->get<gnc_numeric>());
         break;
     case KVP_TYPE_STRING:
-        string = kvp_value_get_string(val);
+    {
+        auto string = val->get<const char*>();
         return string ? scm_from_utf8_string(string) : SCM_BOOL_F;
         break;
+    }
     case KVP_TYPE_GUID:
     {
-        GncGUID *tempguid = kvp_value_get_guid(val);
+        auto tempguid = kvp_value_get_guid(val);
         return gnc_guid2scm(*tempguid);
     }
     break;
     case KVP_TYPE_TIMESPEC:
-        return gnc_timespec2timepair(kvp_value_get_timespec(val));
+        return gnc_timespec2timepair(val->get<Timespec>());
         break;
 
     case KVP_TYPE_FRAME:
     {
-        KvpFrame *frame = kvp_value_get_frame(val);
-        if (frame)
+        auto frame = val->get<KvpFrame*>();
+        if (frame != nullptr)
             return SWIG_NewPointerObj(frame, SWIG_TypeQuery("_p_KvpFrame"), 0);
     }
     break;
     case KVP_TYPE_GDATE:
-        return gnc_timespec2timepair(gdate_to_timespec(kvp_value_get_gdate(val)));
+        return gnc_timespec2timepair(gdate_to_timespec(val->get<GDate>()));
 
         /* FIXME: handle types below */
     case KVP_TYPE_GLIST:
--- src/engine/kvp-scm.h
+++ src/engine/kvp-scm.h
@@ -1,11 +1,18 @@
 #ifndef KVP_SCM_H
 #define KVP_SCM_H
 
-#include "qof.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <qof.h>
 #include <libguile.h>
 
 KvpValue* gnc_scm_to_kvp_value_ptr(SCM kvpval);
 SCM gnc_kvp_value_ptr_to_scm(KvpValue* val);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* KVP_SCM_H */
 
