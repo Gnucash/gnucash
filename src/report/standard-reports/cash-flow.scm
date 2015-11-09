8dfea02da79ecefd290be928c5b25a9e765764d1
Peter Broadbery <p.broadbery@gmail.com>
2015-11-09 22:47:57 +0000

    reports: Add account and split module, plus tests.
    Remove common functions from the budget and cashflow reports.
    Add into separate modules, plus some tests for these newly exposed
    functions.

--- src/report/standard-reports/cash-flow.scm
+++ src/report/standard-reports/cash-flow.scm
@@ -31,6 +31,8 @@
 (use-modules (gnucash main)) ;; FIXME: delete after we finish modularizing.
 (use-modules (gnucash gnc-module))
 (use-modules (gnucash gettext))
+(use-modules (gnucash report report-system account))
+(use-modules (gnucash report report-system split))
 
 (use-modules (gnucash printf))
 
@@ -157,47 +159,6 @@
          (doc (gnc:make-html-document))
          (table (gnc:make-html-table))
          (txt (gnc:make-html-text)))
-
-    ;; is account in list of accounts?
-    (define (same-account? a1 a2)
-      (string=? (gncAccountGetGUID a1) (gncAccountGetGUID a2)))
-
-    (define (same-split? s1 s2) 
-      (string=? (gncSplitGetGUID s1) (gncSplitGetGUID s2)))
-
-    (define account-in-list?
-      (lambda (account accounts)
-        (cond
-          ((null? accounts) #f)
-          ((same-account? (car accounts) account) #t)
-          (else (account-in-list? account (cdr accounts))))))
-
-    (define split-in-list? 
-      (lambda (split splits)
-	(cond 
-	 ((null? splits) #f)
-	 ((same-split? (car splits) split) #t)
-	 (else (split-in-list? split (cdr splits))))))
-
-    (define account-in-alist
-      (lambda (account alist)
-        (cond
-	   ((null? alist) #f)
-           ((same-account? (caar alist) account) (car alist))
-           (else (account-in-alist account (cdr alist))))))
-
-    ;; helper for sorting of account list
-    (define (account-full-name<? a b)
-      (string<? (gnc-account-get-full-name a) (gnc-account-get-full-name b)))
-
-    ;; return maximum depth over accounts and their children, if any
-    (define (accounts-get-children-depth accounts)
-      (apply max
-	     (map (lambda (acct)
-		    (let ((acct-depth (gnc-account-get-current-depth acct)))
-		      (+ acct-depth (- (gnc-account-get-tree-depth acct) 1))))
-		  accounts)))
-
 
     (gnc:html-document-set-title! 
      doc (string-append
