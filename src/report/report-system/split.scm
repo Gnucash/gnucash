6a3c8f481a5ce18b5e79863a3497fd17c92e4ead
Peter Broadbery <p.broadbery@gmail.com>
2015-11-09 20:51:57 +0000

    split.scm: Add a specialised hashtable.

--- src/report/report-system/split.scm
+++ src/report/report-system/split.scm
@@ -4,9 +4,12 @@
 (gnc:module-begin-syntax (gnc:module-load "gnucash/engine" 0))
 
 (use-modules (sw_engine))
+(use-modules (srfi srfi-1))
 
 (export split-same?)
 (export split-in-list?)
+(export split-hashtable-ref)
+(export split-hashtable-set!)
 
 (define (split-same? s1 s2) 
   (string=? (gncSplitGetGUID s1) (gncSplitGetGUID s2)))
@@ -17,4 +20,20 @@
      ((null? splits) #f)
      ((split-same? (car splits) split) #t)
      (else (split-in-list? split (cdr splits))))))
+
+;; Split hashtable. Because we do gncSplitGetGUID so often, it
+;; turns out to be a bit quicker to store a (hash, split) pair
+;; instead of just the split.
+(define (split-assoc split alist)
+  (find (lambda (pair) (split-same? (cdr split) (cdr (car pair)))) alist))
+(define (split-hash split size)
+  (remainder (car split) size))
+
+(define (split-hashtable-ref table split)
+  (hashx-ref split-hash split-assoc table
+	     (cons (string-hash (gncSplitGetGUID split)) split)))
+
+(define (split-hashtable-set! table split value)
+  (hashx-set! split-hash split-assoc table
+	      (cons (string-hash (gncSplitGetGUID split)) split) value))
 
