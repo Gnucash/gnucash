837b25830d0530d10e0af85507cb91462b6ca253
John Ralls <jralls@ceridwen.us>
2016-01-10 15:39:59 -0800

    Update copyright and ChangeLog for the new year.

--- src/gnome-utils/gnc-main-window.c
+++ src/gnome-utils/gnc-main-window.c
@@ -4366,7 +4366,7 @@ gnc_main_window_cmd_help_about (GtkAction *action, GncMainWindow *window)
     {
 	const gchar *fixed_message = _("The GnuCash personal finance manager. "
                                    "The GNU way to manage your money!");
-	const gchar *copyright = _("© 1997-2015 Contributors");
+	const gchar *copyright = _("© 1997-2016 Contributors");
 	gchar **authors = get_file_strsplit("AUTHORS");
 	gchar **documenters = get_file_strsplit("DOCUMENTERS");
 	gchar *license = get_file("LICENSE");
