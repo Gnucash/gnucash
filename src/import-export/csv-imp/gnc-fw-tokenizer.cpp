41f5fb5f9a29c85f5af3b920d09a694cdbf68136
Geert Janssens <janssens-geert@telenet.be>
2016-06-12 12:27:08 +0200

    Add functions to manipulate fixed width column positions

--- src/import-export/csv-imp/gnc-fw-tokenizer.cpp
+++ src/import-export/csv-imp/gnc-fw-tokenizer.cpp
@@ -17,6 +17,165 @@ GncFwTokenizer::columns(const std::vector<uint>& cols)
 }
 
 
+bool GncFwTokenizer::col_can_add (uint col_end)
+{
+    if (col_end < 0 || col_end > longest_line)
+        return false;
+    else
+        return true;
+}
+
+void GncFwTokenizer::col_add (uint col_end)
+{
+    if (col_can_add (col_end))
+    {
+        for (auto col_it = col_vec.begin(); col_it != col_vec.end(); col_it++)
+        {
+            if (*col_it == col_end)
+                return; // don't add same column end twice in the column list
+            if (*col_it > col_end)
+                col_vec.insert (col_it, col_end);
+        }
+
+        // If we got here that means the requested col_end is beyond the currently
+        // inserted columns, so append it
+        col_vec.push_back (col_end);
+    }
+}
+
+bool GncFwTokenizer::col_can_delete (uint col_num)
+{
+    auto last_col = col_vec.size() - 1;
+    if (col_num < 0 || col_num > last_col)
+        return false;
+    else
+        return true;
+}
+
+void GncFwTokenizer::col_delete (uint col_num)
+{
+    if (col_can_delete (col_num))
+        col_vec.erase (col_vec.begin() + col_num);
+}
+
+bool GncFwTokenizer::col_can_narrow (uint col_num)
+{
+    auto last_col = col_vec.size() - 1;
+    int col_start, next_col_start;
+
+    if (col_num > last_col)
+        return false;
+
+    col_start = (col_num == 0) ? 0 : col_vec[col_num - 1];
+    next_col_start = col_vec[col_num];
+
+    if (next_col_start - 1 <= col_start)
+        return false;
+    else
+        return true;
+}
+
+void GncFwTokenizer::col_narrow (uint col_num)
+{
+    if (col_can_narrow (col_num))
+        col_vec[col_num]--;
+}
+
+bool GncFwTokenizer::col_can_widen (uint col_num)
+{
+    auto last_col = col_vec.size() - 1;
+    int col_end, next_col_end;
+
+    if (col_num > last_col)
+        return false;
+
+    col_end = col_vec[col_num];
+    next_col_end = (col_num == last_col - 1)
+                    ? longest_line
+                    : col_vec[col_num + 1];
+
+    if (col_end + 1 >= next_col_end)
+        return false;
+    else
+        return true;
+}
+
+void GncFwTokenizer::col_widen (uint col_num)
+{
+    if (col_can_widen (col_num))
+        col_vec[col_num]++;
+}
+
+bool GncFwTokenizer::col_can_split (uint col_num, uint position)
+{
+    auto last_col = col_vec.size() - 1;
+    if (col_num > last_col)
+        return false;
+
+    uint col_start = (col_num == 0) ? 0 : col_vec[col_num - 1];
+    uint col_end = col_vec[col_num];
+    if (position <= col_start || position >= col_end)
+        return false;
+    else
+        return true;
+}
+
+void GncFwTokenizer::col_split (uint col_num, uint position)
+{
+    if (col_can_split (col_num, position))
+    {
+        uint col_start = (col_num == 0) ? 0 : col_vec[col_num - 1];;
+        col_vec.insert (col_vec.begin() + col_num, col_start + position);
+    }
+}
+
+
+std::string GncFwTokenizer::cols_to_string()
+{
+    std::ostringstream colstream;
+    for (auto col_end : col_vec)
+        colstream<<col_end<<",";
+    std::string colstr = colstream.str();
+    if (!colstr.empty())
+        colstr.pop_back(); // Drop last ","
+    return colstr;
+}
+
+void GncFwTokenizer::cols_from_string(const std::string& col_str)
+{
+    // Clear existing columns first
+    columns();
+
+    std::istringstream in_stream(col_str);
+    std::string col_end_str;
+    while (std::getline (in_stream, col_end_str, ','))
+    {
+        if (col_end_str.empty())
+            continue;  // Skip empty column positions
+
+        uint charindex = std::stoi (col_end_str);
+        col_add (charindex);
+    }
+}
+
+
+void GncFwTokenizer::load_file(const std::string& path)
+{
+    GncTokenizer::load_file(path);
+
+    std::string line;
+    longest_line = 0;
+    std::istringstream in_stream(utf8_contents);
+    while (std::getline (in_stream, line))
+    {
+        if (line.size() > longest_line)
+            longest_line = line.size();
+
+        line.clear();
+    }
+}
+
+
 int GncFwTokenizer::tokenize()
 {
     typedef boost::tokenizer< boost::offset_separator > Tokenizer;
