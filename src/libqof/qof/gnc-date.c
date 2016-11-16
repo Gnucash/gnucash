5ce8878764a47f0ff9f98f5bc7cc50858229fd72
Robert Fewell <14uBobIT@gmail.com>
2016-11-16 19:13:50 +0000

    Add an unset option to date-format
    Added an option of "No Fancy Date Format" to date-format so it can be
    the default. In this position, the format returned is that of what ever
    the preference date is set to.

--- src/libqof/qof/gnc-date.c
+++ src/libqof/qof/gnc-date.c
@@ -456,6 +456,8 @@ gnc_date_dateformat_to_string(QofDateFormat format)
         return "locale";
     case QOF_DATE_FORMAT_CUSTOM:
         return "custom";
+    case QOF_DATE_FORMAT_UNSET:
+        return "unset";
     default:
         return NULL;
     }
@@ -481,6 +483,8 @@ gnc_date_string_to_dateformat(const char* fmt_str, QofDateFormat *format)
         *format = QOF_DATE_FORMAT_LOCALE;
     else if (!strcmp(fmt_str, "custom"))
         *format = QOF_DATE_FORMAT_CUSTOM;
+    else if (!strcmp(fmt_str, "unset"))
+        *format = QOF_DATE_FORMAT_UNSET;
     else
         return TRUE;
 
@@ -769,6 +773,8 @@ const gchar *qof_date_format_get_string(QofDateFormat df)
         return "%Y-%m-%dT%H:%M:%SZ";
     case QOF_DATE_FORMAT_ISO:
         return "%Y-%m-%d";
+    case QOF_DATE_FORMAT_UNSET: // use global
+        return qof_date_format_get_string (dateFormat);
     case QOF_DATE_FORMAT_LOCALE:
     default:
         break;
@@ -798,6 +804,8 @@ const gchar *qof_date_text_format_get_string(QofDateFormat df)
         return "%Y-%m-%dT%H:%M:%SZ";
     case QOF_DATE_FORMAT_ISO:
         return "%Y-%b-%d";
+    case QOF_DATE_FORMAT_UNSET: // use global
+        return qof_date_text_format_get_string (dateFormat);
     case QOF_DATE_FORMAT_LOCALE:
     default:
         break;
