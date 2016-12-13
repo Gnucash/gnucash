7fdf135a5417db8fd6174a147066e330b60017b7
Geert Janssens <janssens-geert@telenet.be>
2016-12-13 19:59:45 +0100

    Revisit csv trans import settings handling
    - move all gtk related stuff to the assistant code
    - move all checks to the settings object, the assistant should query that
    - handle sensitivity of the save and delete button more intelligently

--- src/import-export/csv-imp/gnc-csv-trans-settings.hpp
+++ src/import-export/csv-imp/gnc-csv-trans-settings.hpp
@@ -27,10 +27,6 @@
 #ifndef GNC_CSV_TRANS_SETTINGS_H
 #define GNC_CSV_TRANS_SETTINGS_H
 
-extern "C" {
-#include <gtk/gtk.h>
-}
-
 #include <string>
 #include <vector>
 #include "gnc-trans-props.hpp"
@@ -48,41 +44,46 @@ struct CsvTransSettings
 {
     CsvTransSettings() : header_rows{0}, footer_rows{0}, csv_format (true),
                     skip_alt_rows (false), multi_split (false),
-                    encoding {"UTF-8"}, custom {false}, custom_entry {nullptr},
-                    date_active {0}, currency_active {0}
+                    encoding {"UTF-8"}, custom {false}, custom_entry {""},
+                    date_active {0}, currency_active {0}, load_error {false},
+                    internal {false}
                     {
                         for (uint i = 0; i < SEP_NUM_OF_TYPES; i++)
                         {
                             separator[i] = false;
                         }
+                        separator [SEP_COMMA] = true;
                     }
-
-/** Finds CSV settings entries in the state key file and populates the
- *  tree model.
- *
- *  @param settings_store The liststore that is used for the combo's
- *  which holds the key name and visual text.
- */
-void find (GtkTreeModel *settings_store);
 
 /** Save the gathered widget properties to a key File.
  *
- *  @param settings_name The name the settings will be stored under.
- *
  *  @return true if there was a problem in saving.
  */
-bool save (const std::string& settings_name);
+bool save (void);
 
 /** Load the widget properties from a key File.
+ *
+ *  @return true if there was a problem.
+ */
+bool load (void);
+
+/** Remove the preset from the state file.
+ */
+void remove (void);
+
+/** Check whether the user is allowed to save (over) or delete this preset or not.
+ *  The internally generated presets are read-only. The others
+ *  can be saved to the state file or deleted.
  *
  *  @param group The group name where the settings are stored in the
  *  key file.
  *
  *  @return true if there was a problem.
  */
-bool load (const std::string& group);
+bool read_only (void);
 
 
+std::string   name;                         // Name given to this preset by the user
 int           header_rows;                  // Number of header rows
 int           footer_rows;                  // Number of footer rows
 bool          csv_format;                   // CSV import Format
@@ -101,6 +102,23 @@ int           currency_active;              // Currency Active id
 std::vector<GncTransPropType>  column_types;// The Column types in order
 std::vector<uint> column_widths;            // The Column widths
 
+bool          load_error;                   // Was there an error while parsing the state file ?
+bool          internal;                     // true for internally generated presets
 };
+
+using preset_vec = std::vector<std::shared_ptr<CsvTransSettings>>;
+/** Creates a vector of CsvTransSettings which combines
+ *  - one or more internally defined presets
+ *  - all preset found in the state key file.
+ *
+ *  @return a reference to the populated vector.
+ */
+const preset_vec& get_trans_presets (void);
+
+/** Check whether name can be used as a preset name.
+ *  The names of the internal presets are considered reserved.
+ *  A preset with such a name should not be saved or deleted.
+ */
+bool trans_preset_is_reserved_name (const std::string& name);
 
 #endif
