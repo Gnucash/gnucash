Gnucash/gnucash

commit b3eb9d0d8a48791261915f1dffc5ede558e0c8b4

Use qof_instance_set/get instead of g_object_set/get.

src/app-utils/gnc-sx-instance-model.c
@@ -83,7 +83,7 @@ scrub_sx_split_numeric (Split* split, const char *debcred)
     char *error_loc;
     gnc_numeric amount = gnc_numeric_zero ();
     gboolean parse_result = FALSE;
-    g_object_get (G_OBJECT (split),
+    qof_instance_get (QOF_INSTANCE (split),
 		  formula, &formval,
 		  numeric, &numval,
 		  NULL);
@@ -95,7 +95,7 @@ scrub_sx_split_numeric (Split* split, const char *debcred)
     g_hash_table_unref (parser_vars);
     if (gnc_numeric_eq (amount, numval))
         return FALSE;
-    g_object_set (G_OBJECT (split),
+    qof_instance_set (QOF_INSTANCE (split),
 		  numeric, amount,
 		  NULL);
     return TRUE;

commit d18fce775f6023c936c7516cf4fdf28564a9d238

Fix a crash in scrub_sx_split_numeric introduced in b3eb9d0.

src/app-utils/gnc-sx-instance-model.c
@@ -96,7 +96,7 @@ scrub_sx_split_numeric (Split* split, const char *debcred)
     if (gnc_numeric_eq (amount, numval))
         return FALSE;
     qof_instance_set (QOF_INSTANCE (split),
-		  numeric, amount,
+		  numeric, &amount,
 		  NULL);
     return TRUE;
 }