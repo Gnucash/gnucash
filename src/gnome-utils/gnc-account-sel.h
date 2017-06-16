d29bb5b8cd16a822d59a1e30390f017abd2dd14a
Robert Fewell <14uBobIT@gmail.com>
2017-06-16 16:50:45 +0100

    Correct corrupted line in gnc-account-sel.h

--- src/gnome-utils/gnc-account-sel.h
+++ src/gnome-utils/gnc-account-sel.h
@@ -106,4 +106,4 @@ void gnc_account_sel_set_new_account_modal( GNCAccountSel *gas,
 gint gnc_account_sel_get_num_account( GNCAccountSel *gas );
 void gnc_account_sel_purge_account( GNCAccountSel *gas, Account *acc, gboolean recursive);
 
-#endif /* !ndef GNC_ACCOUNT_SEL_H */
+#endif /* GNC_ACCOUNT_SEL_H */
