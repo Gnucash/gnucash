acd7475ba471923eabb5b52244df52822a4beca4
Robert Fewell <14uBobIT@gmail.com>
2017-06-18 15:15:03 +0100

    Files linked to dialog-transfer.glade

--- src/gnome-utils/dialog-transfer.c
+++ src/gnome-utils/dialog-transfer.c
@@ -1862,9 +1862,9 @@ gnc_xfer_dialog_create(GtkWidget *parent, XferDialog *xferData)
 
     ENTER(" ");
     builder = gtk_builder_new();
-    gnc_builder_add_from_file (builder, "dialog-transfer.glade", "Transfer Dialog");
+    gnc_builder_add_from_file (builder, "dialog-transfer.glade", "transfer_dialog");
 
-    xferData->dialog = GTK_WIDGET(gtk_builder_get_object (builder, "Transfer Dialog"));
+    xferData->dialog = GTK_WIDGET(gtk_builder_get_object (builder, "transfer_dialog"));
     g_object_set_data_full (G_OBJECT (xferData->dialog), "builder", builder, g_object_unref);
 
     /* parent */
