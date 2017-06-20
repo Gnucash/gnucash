82e11de8dcac21dd70616a3d45f497d717126996
Robert Fewell <14uBobIT@gmail.com>
2017-06-20 14:14:53 +0100

    Files linked to dialog-bi-import-gui.glade

--- src/plugins/bi_import/dialog-bi-import-gui.c
+++ src/plugins/bi_import/dialog-bi-import-gui.c
@@ -103,8 +103,8 @@ gnc_plugin_bi_import_showGUI(void)
     gui->open_mode = "ALL";
 
     builder = gtk_builder_new();
-    gnc_builder_add_from_file (builder, "dialog-bi-import-gui.glade", "bi-import Dialog");
-    gui->dialog = GTK_WIDGET(gtk_builder_get_object (builder, "bi-import Dialog"));
+    gnc_builder_add_from_file (builder, "dialog-bi-import-gui.glade", "bi_import_dialog");
+    gui->dialog = GTK_WIDGET(gtk_builder_get_object (builder, "bi_import_dialog"));
     gui->tree_view = GTK_WIDGET(gtk_builder_get_object (builder, "treeview1"));
     gui->entryFilename = GTK_WIDGET(gtk_builder_get_object (builder, "entryFilename"));
 
