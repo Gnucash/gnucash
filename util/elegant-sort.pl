16b98cc5a461726fdb2f34cd7ddd8b12c782ca54
Geert Janssens <geert@kobaltwit.be>
2017-08-03 12:02:49 +0200

    Tweak cmake/autotools so they produce the same output for POTFILES.in

---
+++ util/elegant-sort.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+# -*- perl -*-
+#
+# Sort filenames in POTFILES.in in a consistent way
+# This reduces the amount of clutter in our version manangement system
+# The files will be sorted
+# * per directory
+# * case-insensitive
+# * ignoring punctuation (-,_,.)
+#
+sub sort_func
+{
+    my $stripped_a = $a;
+    my $stripped_b = $b;
+    $stripped_a =~ s/[-_.]//g;
+    $stripped_b =~ s/[-_.]//g;
+
+    lc ($stripped_a) cmp lc ($stripped_b)
+      ||
+    lc ($a) cmp lc ($b)
+}
+
+#open my $fh, '<', $ARGV[0];
+#my @raw_files = <$fh>;
+my @raw_files = <STDIN>;
+my @possible_files = sort sort_func @raw_files;
+print @possible_files;
