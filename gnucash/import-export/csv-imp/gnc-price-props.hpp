3410a03b2291f6a095040dca442df1bf903a284c
Robert Fewell <14uBobIT@gmail.com>
2017-12-01 10:44:15 +0000

    Add property files for the csv price importer
    These files are largely based on the csv transaction importer

---
+++ gnucash/import-export/csv-imp/gnc-price-props.hpp
@@ -0,0 +1,110 @@
+/********************************************************************\
+ * gnc-price-props.hpp - encapsulate price properties for use       *
+ *                       in the csv importer                        *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
+ *                                                                  *
+\********************************************************************/
+
+#ifndef GNC_PRICE_PROPS_HPP
+#define GNC_PRICE_PROPS_HPP
+
+extern "C" {
+#include <platform.h>
+#if PLATFORM(WINDOWS)
+#include <windows.h>
+#endif
+
+#include <glib/gi18n.h>
+
+#include "gnc-commodity.h"
+}
+
+#include <string>
+#include <map>
+#include <memory>
+#include <boost/optional.hpp>
+#include <gnc-numeric.hpp>
+
+/** Enumeration for column types. These are the different types of
+ * columns that can exist in a CSV/Fixed-Width file. There should be
+ * no two columns with the same type except for the GncPricePropType::NONE
+ * type. */
+enum class GncPricePropType {
+    NONE,
+    DATE,
+    AMOUNT,
+    CURRENCY_FROM,
+    CURRENCY_TO,
+    SYMBOL_FROM,
+    PRICE_PROPS = SYMBOL_FROM
+};
+
+/** Maps all column types to a string representation.
+ *  The actual definition is in gnc-csv-imp-prices.cpp.
+ *  Attention: that definition should be adjusted for any
+ *  changes to enum class GncPricePropType ! */
+extern std::map<GncPricePropType, const char*> gnc_csv_price_col_type_strs;
+
+/** Functor to check if the above map has an element of which
+ *  the value equals name. To be used with std::find_if.
+ */
+struct test_price_prop_type_str
+{
+    test_price_prop_type_str( const char* name ) : m_name(name) {}
+    bool operator()( const std::pair<GncPricePropType, const char*>& v ) const
+    {
+        return !g_strcmp0(v.second, m_name);
+    }
+private:
+    const char *m_name;
+};
+
+time64 parse_date_price (const std::string &date_str, int format);
+gnc_commodity* parse_commodity_price_comm (const std::string& comm_str);
+gnc_commodity* parse_commodity_price_sym (const std::string& comm_str, bool is_currency);
+GncNumeric parse_amount_price (const std::string &str, int currency_format);
+
+struct GncImportPrice
+{
+public:
+    GncImportPrice (int date_format, int currency_format) : m_date_format{date_format},
+        m_currency_format{currency_format}{};
+
+    void set (GncPricePropType prop_type, const std::string& value);
+    void set_date_format (int date_format) { m_date_format = date_format ;}
+    void set_currency_format (int currency_format) { m_currency_format = currency_format ;}
+    void reset (GncPricePropType prop_type);
+    std::string verify_essentials (void);
+    bool create_price (QofBook* book, GNCPriceDB *pdb, bool over);
+    std::string errors();
+
+private:
+    int m_date_format;
+    int m_currency_format;
+    boost::optional<time64> m_date;
+    boost::optional<GncNumeric> m_amount;
+    boost::optional<gnc_commodity*> m_currency_from;
+    boost::optional<gnc_commodity*> m_currency_to;
+    boost::optional<gnc_commodity*> m_symbol_from;
+    bool created = false;
+
+    std::map<GncPricePropType, std::string> m_errors;
+};
+
+#endif
