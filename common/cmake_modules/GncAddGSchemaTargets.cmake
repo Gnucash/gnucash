8407320794a258e72684b728442dd19ae5843868
Geert Janssens <geert@kobaltwit.be>
2017-12-21 17:54:49 +0100

    Replace GNC_CONFIGURE(2) with configure_file

--- common/cmake_modules/GncAddGSchemaTargets.cmake
+++ common/cmake_modules/GncAddGSchemaTargets.cmake
@@ -12,26 +12,19 @@ MACRO(ADD_GSCHEMA_TARGETS _TARGET _gschema_INPUTS)
     SET(CMAKE_COMMAND_TMP ${CMAKE_COMMAND} -E env)
   ENDIF()
   FOREACH(file ${_gschema_INPUTS})
-    GNC_CONFIGURE2(${file}-target ${file}.in.in ${file}.in)
+    configure_file(${file}.in.in ${file}.in @ONLY)
     STRING(REPLACE ".xml" ".valid" file_no_xml ${file})
-    SET(_OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file})
-    SET(_BUILD_FILE ${DATADIR_BUILD}/glib-2.0/schemas/${file})
+    SET(_OUTPUT_FILE ${DATADIR_BUILD}/glib-2.0/schemas/${file})
     SET(_VALID_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file_no_xml})
     LIST(APPEND _gschema_OUTPUTS ${_OUTPUT_FILE})
     LIST(APPEND _gschema_VALIDS ${_VALID_FILE})
-    LIST(APPEND _gschema_BUILDS ${_BUILD_FILE})
     ADD_CUSTOM_COMMAND(
         OUTPUT ${_OUTPUT_FILE}
         COMMAND ${CMAKE_COMMAND_TMP}
           LC_ALL=C
-          ${PERL_EXECUTABLE} ${INTLTOOL_MERGE} -x -u ${INITTOOL_OPTIONS} ${CMAKE_CURRENT_BINARY_DIR}/${file}.in ${CMAKE_CURRENT_BINARY_DIR}/${file}
+          ${PERL_EXECUTABLE} ${INTLTOOL_MERGE} -x -u ${INITTOOL_OPTIONS} ${CMAKE_CURRENT_BINARY_DIR}/${file}.in ${_OUTPUT_FILE}
         DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${file}.in
         MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${file}.in.in
-    )
-    ADD_CUSTOM_COMMAND(
-      OUTPUT ${_BUILD_FILE}
-      COMMAND ${CMAKE_COMMAND} -E copy ${_OUTPUT_FILE} ${_BUILD_FILE}
-      DEPENDS ${_OUTPUT_FILE}
     )
     ADD_CUSTOM_COMMAND(
         OUTPUT ${_VALID_FILE}
@@ -40,9 +33,10 @@ MACRO(ADD_GSCHEMA_TARGETS _TARGET _gschema_INPUTS)
         COMMAND ${CMAKE_COMMAND} -E touch ${_VALID_FILE}
         DEPENDS ${_OUTPUT_FILE}
     )
+
   ENDFOREACH(file)
 
-  ADD_CUSTOM_TARGET(${_TARGET} DEPENDS ${_gschema_OUTPUTS} ${_gschema_VALIDS} ${_gschema_BUILDS})
+  ADD_CUSTOM_TARGET(${_TARGET} DEPENDS ${_gschema_VALIDS} ${_gschema_OUTPUTS})
 
   INSTALL(FILES ${_gschema_OUTPUTS} DESTINATION share/glib-2.0/schemas)
 
