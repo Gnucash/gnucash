f11eab36d9a2e9d844a549e8a64cf356c8b4858f
Geert Janssens <geert@kobaltwit.be>
2017-12-21 17:54:49 +0100

    Replace GNC_CONFIGURE(2) with configure_file

--- common/cmake_modules/GncAddGSchemaTargets.cmake
+++ common/cmake_modules/GncAddGSchemaTargets.cmake
@@ -1,7 +1,6 @@
-MACRO(ADD_GSCHEMA_TARGETS _TARGET _gschema_INPUTS)
+macro(add_gschema_targets _gschema_INPUTS)
   SET(_gschema_OUTPUTS "")
-  SET(_gschema_VALIDS "")
-  SET(_gschema_BUILDS "")
+  set(local_depends ${gschema_depends})
   # FIXME: I have no idea of I'm using the right options here for intltool-merge for Windows.
   SET(INITTOOL_OPTIONS "--no-translations")
   IF(WIN32)
@@ -12,27 +11,22 @@ MACRO(ADD_GSCHEMA_TARGETS _TARGET _gschema_INPUTS)
     SET(CMAKE_COMMAND_TMP ${CMAKE_COMMAND} -E env)
   ENDIF()
   FOREACH(file ${_gschema_INPUTS})
-    GNC_CONFIGURE2(${file}-target ${file}.in.in ${file}.in)
-    STRING(REPLACE ".xml" ".valid" file_no_xml ${file})
-    SET(_OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file})
-    SET(_BUILD_FILE ${DATADIR_BUILD}/glib-2.0/schemas/${file})
-    SET(_VALID_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file_no_xml})
-    LIST(APPEND _gschema_OUTPUTS ${_OUTPUT_FILE})
-    LIST(APPEND _gschema_VALIDS ${_VALID_FILE})
-    LIST(APPEND _gschema_BUILDS ${_BUILD_FILE})
+    configure_file(${file}.in.in ${file}.in @ONLY)
+
+    set(_OUTPUT_FILE ${DATADIR_BUILD}/glib-2.0/schemas/${file})
     ADD_CUSTOM_COMMAND(
         OUTPUT ${_OUTPUT_FILE}
         COMMAND ${CMAKE_COMMAND_TMP}
           LC_ALL=C
-          ${PERL_EXECUTABLE} ${INTLTOOL_MERGE} -x -u ${INITTOOL_OPTIONS} ${CMAKE_CURRENT_BINARY_DIR}/${file}.in ${CMAKE_CURRENT_BINARY_DIR}/${file}
+          ${PERL_EXECUTABLE} ${INTLTOOL_MERGE} -x -u ${INITTOOL_OPTIONS} ${CMAKE_CURRENT_BINARY_DIR}/${file}.in ${_OUTPUT_FILE}
         DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${file}.in
         MAIN_DEPENDENCY ${CMAKE_CURRENT_SOURCE_DIR}/${file}.in.in
     )
-    ADD_CUSTOM_COMMAND(
-      OUTPUT ${_BUILD_FILE}
-      COMMAND ${CMAKE_COMMAND} -E copy ${_OUTPUT_FILE} ${_BUILD_FILE}
-      DEPENDS ${_OUTPUT_FILE}
-    )
+    list(APPEND _gschema_OUTPUTS ${_OUTPUT_FILE})
+
+    string(REPLACE ".xml" ".valid" file_no_xml ${file})
+    set(_VALID_FILE ${CMAKE_CURRENT_BINARY_DIR}/${file_no_xml})
+    list(APPEND _gschema_VALIDS ${_VALID_FILE})
     ADD_CUSTOM_COMMAND(
         OUTPUT ${_VALID_FILE}
         COMMAND ${CMAKE_COMMAND_TMP}
@@ -40,10 +34,18 @@ MACRO(ADD_GSCHEMA_TARGETS _TARGET _gschema_INPUTS)
         COMMAND ${CMAKE_COMMAND} -E touch ${_VALID_FILE}
         DEPENDS ${_OUTPUT_FILE}
     )
+    add_custom_target(${file_no_xml}-target DEPENDS ${_VALID_FILE})
+
+    # Add both a target and a file level dependency for the valid target/file
+    # to the gschemas.compiled target (local_depends will be propagated to that target)
+    # The target level one is to ensure a link between two targets in different directories (required for the make build chain)
+    # The file level one is to ensure gschemas.compiled will be rebuilt if any of the
+    # dependencies changes.
+    list(APPEND local_depends ${file_no_xml}-target ${_VALID_FILE})
   ENDFOREACH(file)
 
-  ADD_CUSTOM_TARGET(${_TARGET} DEPENDS ${_gschema_OUTPUTS} ${_gschema_VALIDS} ${_gschema_BUILDS})
+  set(gschema_depends ${local_depends} CACHE INTERNAL "gschemas.compiled dependencies")
 
   INSTALL(FILES ${_gschema_OUTPUTS} DESTINATION share/glib-2.0/schemas)
 
-ENDMACRO()
+endmacro()
