03e862ec47e6f27e38e261c1b71356a3d7a28be8
Christopher Lam <christopher.lck@gmail.com>
2018-02-19 23:31:46 +0800

    TR: (minor change) eq? -> eqv?
    According to the guide, eqv? better than eq? for chars

--- gnucash/report/standard-reports/transaction.scm
+++ gnucash/report/standard-reports/transaction.scm
@@ -987,7 +987,7 @@ tags within description, notes or memo. ")
                                (lambda (split transaction-row?)
                                  (gnc:make-html-table-cell/markup
                                   "date-cell"
-                                  (if (eq? (xaccSplitGetReconcile split) #\y)
+                                  (if (eqv? (xaccSplitGetReconcile split) #\y)
                                       (qof-print-date (xaccSplitGetDateReconciled split))
                                       "")))))
 
