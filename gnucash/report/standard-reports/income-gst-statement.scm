1ee2c08306af8b480b5a706854c4557139400c76
Christopher Lam <christopher.lck@gmail.com>
2018-04-14 22:13:21 +0800

    GSTR: close br tag; add  empty lines to empty-report-message
    This will be useful for unit testing, and for displaying
    gnc:render-options-changed.

--- gnucash/report/standard-reports/income-gst-statement.scm
+++ gnucash/report/standard-reports/income-gst-statement.scm
@@ -43,14 +43,15 @@
  authorities. From <i>Edit report options</i> above, choose your Business Income and Business Expense accounts.
  Each transaction may contain, in addition to the accounts payable/receivable or bank accounts,
  a split to a tax account, e.g. Income:Sales -$1000, Liability:GST on Sales -$100, Asset:Bank $1100.")
-   "<br><br>"
+   "<br/><br/>"
    (_ "These tax accounts can either be populated using the standard register, or from Business Invoices and Bills
  which will require Business > Sales Tax Tables to be set up correctly. Please see the documentation.")
-   "<br><br>"
+   "<br/><br/>"
    (_ "From the Report Options, you will need to select the accounts which will \
 hold the GST/VAT taxes collected or paid. These accounts must contain splits which document the \
 monies which are wholly sent or claimed from tax authorities during periodic GST/VAT returns. These \
-accounts must be of type ASSET for taxes paid on expenses, and type LIABILITY for taxes collected on sales.")))
+accounts must be of type ASSET for taxes paid on expenses, and type LIABILITY for taxes collected on sales.")
+   "<br/><br/>"))
 
 (define (income-gst-statement-renderer rpt)
   (trep-renderer rpt
