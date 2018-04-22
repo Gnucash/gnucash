8e9b13461ef841d0179308e5b6d37a8c486e05aa
John Ralls <jralls@ceridwen.us>
2018-04-22 16:09:03 -0700

    Reverse Swig "off-by-one" error.
    See
    https://github.com/swig/swig/commit/46ab0c252da9ab846265a60517ca462e5256cd24
    
    Fixes the rest of Bug 795134.
    Seems to fix Bug 794965 - Non-English Characters don't display properly
    in reports.

--- common/guile-mappings.h
+++ common/guile-mappings.h
@@ -21,6 +21,10 @@
 
 /* Convenience macros */
 
+#if defined(scm_to_utf8_string) && SCM_MAJOR_VERSION >= 2
+#undef scm_to_utf8_string
+#undef scm_from_utf8_string
+#endif
 #define scm_is_equal(obj1,obj2)	scm_is_true(scm_equal_p(obj1,obj2))
 #define scm_is_exact(obj)	scm_is_true(scm_exact_p(obj))
 #define scm_is_list(obj)	scm_is_true(scm_list_p(obj))
