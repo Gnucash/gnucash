f144a8deb7cd4f4595719d1902c857953e6978ff
John Ralls <jralls@ceridwen.us>
2018-06-22 10:14:21 -0700

    Revert "Bug 796248 - Editing Scheduled Transaction"
    This reverts commit ffe6044cd66edf39ae22875ef475d8e655b93526.
    It breaks loading splits on transactions that have already
    been loaded.

--- libgnucash/backend/sql/gnc-transaction-sql.cpp
+++ libgnucash/backend/sql/gnc-transaction-sql.cpp
@@ -292,12 +292,12 @@ load_single_tx (GncSqlBackend* sql_be, GncSqlRow& row)
     if (guid == NULL) return NULL;
     tx_guid = *guid;
 
-    /* Don't overwrite the transaction if it's already been loaded (and possibly
-     * modified).
-     */
+    // Don't overwrite the transaction if it's already been loaded (and possibly modified).
+    // However increase the edit level, it may be modified while loading its splits
     pTx = xaccTransLookup (&tx_guid, sql_be->book());
     if (pTx != NULL)
     {
+        xaccTransBeginEdit (pTx);
         return NULL;
     }
 
