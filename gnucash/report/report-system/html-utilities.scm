d865b149580df44c15c19819a227c25dd1172562
Geert Janssens <geert@kobaltwit.be>
2018-06-28 13:20:34 +0200

    Revert "html-utilities.scm: simplify"
    This reverts commit 4c55141d963452a2381a5bd5b3d4fe31bde2cd2c.
    and should fix the regression it caused as reported in
    https://bugzilla.gnome.org/show_bug.cgi?id=796696

--- gnucash/report/report-system/html-utilities.scm
+++ gnucash/report/report-system/html-utilities.scm
@@ -27,7 +27,9 @@
 ;; returns a list with n #f (empty cell) values 
 (define (gnc:html-make-empty-cell) #f)
 (define (gnc:html-make-empty-cells n)
-  (make-list n #f))
+  (if (> n 0)
+      (cons #f (gnc:html-make-empty-cells (- n 1)))
+      (list)))
 
 (define (gnc:register-guid type guid)
   (gnc-build-url URL-TYPE-REGISTER (string-append type guid) ""))
@@ -814,8 +816,8 @@
     (gnc:html-markup-p
      (gnc:html-markup-anchor
       (gnc-build-url URL-TYPE-OPTIONS
-                     (format #f "report-id=~a" report-id)
-                     "")
+       (string-append "report-id=" (format #f "~a" report-id))
+       "")
       (_ "Edit report options")))))
 
 (define* (gnc:html-render-options-changed options #:optional plaintext?)
