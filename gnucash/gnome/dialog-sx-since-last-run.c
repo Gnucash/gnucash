9e104ebc4034efdc00d8b673c2cba32413c4cb9e
Robert Fewell <14uBobIT@gmail.com>
2018-12-02 14:12:44 +0000

    Bug 796948 - Scheduled Transactions Entered Since Last Run Are not Visible
    Add a call to gnc_gui_refresh_all after the transactions have been
    created so the registers are updated.

--- gnucash/gnome/dialog-sx-since-last-run.c
+++ gnucash/gnome/dialog-sx-since-last-run.c
@@ -1154,6 +1154,7 @@ dialog_response_cb(GtkDialog *dialog, gint response_id, GncSxSinceLastRunDialog 
     gnc_suspend_gui_refresh();
     gnc_sx_slr_model_effect_change(app_dialog->editing_model, FALSE, &app_dialog->created_txns, &creation_errors);
     gnc_resume_gui_refresh();
+    gnc_gui_refresh_all (); // force a refresh of all registers
     if (creation_errors)
         creation_error_dialog(&creation_errors);
 
