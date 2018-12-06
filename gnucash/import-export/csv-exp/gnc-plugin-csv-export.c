016f352944f17c579e650a21809d42c80f4935a4
Christoph Holtermann <c.holtermann@gmx.de>
2018-12-06 21:51:16 +0100

    use G_DEFINE_TYPE_WITH_PRIVATE to replace g_type_class_add_private
    gnc_plugin_csv_export_get_type is being replaced by that provided by G_DEFINE_TYPE_WITH_PRIVATE

--- gnucash/import-export/csv-exp/gnc-plugin-csv-export.c
+++ gnucash/import-export/csv-exp/gnc-plugin-csv-export.c
@@ -78,39 +78,13 @@ typedef struct GncPluginCsvExportPrivate
 
 static GObjectClass *parent_class = NULL;
 
-GType
-gnc_plugin_csv_export_get_type (void)
-{
-    static GType gnc_plugin_csv_export_type = 0;
-
-    if (gnc_plugin_csv_export_type == 0)
-    {
-        static const GTypeInfo our_info =
-        {
-            sizeof (GncPluginCsvExportClass),
-            NULL,		/* base_init */
-            NULL,		/* base_finalize */
-            (GClassInitFunc) gnc_plugin_csv_export_class_init,
-            NULL,		/* class_finalize */
-            NULL,		/* class_data */
-            sizeof (GncPluginCsvExport),
-            0,		/* n_preallocs */
-            (GInstanceInitFunc) gnc_plugin_csv_export_init,
-        };
-
-        gnc_plugin_csv_export_type = g_type_register_static (GNC_TYPE_PLUGIN,
-                                     "GncPluginCsvExport",
-                                     &our_info, 0);
-    }
-
-    return gnc_plugin_csv_export_type;
-}
-
 GncPlugin *
 gnc_plugin_csv_export_new (void)
 {
     return GNC_PLUGIN (g_object_new (GNC_TYPE_PLUGIN_CSV_EXPORT, NULL));
 }
+
+G_DEFINE_TYPE_WITH_PRIVATE(GncPluginCsvExport, gnc_plugin_csv_export, GNC_TYPE_PLUGIN)
 
 static void
 gnc_plugin_csv_export_class_init (GncPluginCsvExportClass *klass)
@@ -130,8 +104,6 @@ gnc_plugin_csv_export_class_init (GncPluginCsvExportClass *klass)
     plugin_class->actions      = gnc_plugin_actions;
     plugin_class->n_actions    = gnc_plugin_n_actions;
     plugin_class->ui_filename  = PLUGIN_UI_FILENAME;
-
-    g_type_class_add_private(klass, sizeof(GncPluginCsvExportPrivate));
 }
 
 static void
