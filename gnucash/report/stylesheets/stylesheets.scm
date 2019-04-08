a1c496f8932b15b7a1cb6769df9091756ece7be7
Christopher Lam <christopher.lck@gmail.com>
2019-04-09 06:57:45 +0800

    [stylesheets] *reindent/delete-trailing-whitespace/untabify*

--- gnucash/report/stylesheets/stylesheets.scm
+++ gnucash/report/stylesheets/stylesheets.scm
@@ -2,7 +2,7 @@
 ;;  stylesheets.scm
 ;;  load the standard stylesheet definitions
 ;;
-;;  Copyright (c) 2001 Linux Developers Group, Inc. 
+;;  Copyright (c) 2001 Linux Developers Group, Inc.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; This program is free software; you can redistribute it and/or
@@ -26,7 +26,7 @@
 
 (define-module (gnucash report stylesheets))
 
-(use-modules (gnucash utilities)) 
+(use-modules (gnucash utilities))
 (use-modules (gnucash report stylesheet-plain))
 (use-modules (gnucash report stylesheet-fancy))
 (use-modules (gnucash report stylesheet-footer))
