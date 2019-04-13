d93d4efd82df063a3baa868774c1c6f35da10aed
c-holtermann <mail@c-holtermann.net>
2019-04-13 18:13:19 +0200

    Import necessary module gdk

--- gnucash/python/pycons/console.py
+++ gnucash/python/pycons/console.py
@@ -35,6 +35,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import GObject
 from gi.repository import Gtk
+from gi.repository import Gdk
 from gi.repository import Pango
 import io
 import pycons.shell as shell
