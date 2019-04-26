82a2fca27ac589de30086c970bf429ff11e61858
Frank H. Ellenberger <frank.h.ellenberger@gmail.com>
2019-04-26 02:30:48 +0200

    [I18N] Mask line endings of continuous text in income-gst-statement.scm
    While the HTML renderer ignored them, translators got confused.

--- gnucash/report/standard-reports/income-gst-statement.scm
+++ gnucash/report/standard-reports/income-gst-statement.scm
@@ -40,14 +40,14 @@
 (define pagename-filter (N_ "Filter"))
 (define TAX-SETUP-DESC
   (gnc:make-html-text
-   (_ "This report is useful to calculate periodic business tax payable/receivable from
- authorities. From 'Edit report options' above, choose your Business Income and Business Expense accounts.
- Each transaction may contain, in addition to the accounts payable/receivable or bank accounts,
- a split to a tax account, e.g. Income:Sales -$1000, Liability:GST on Sales -$100, Asset:Bank $1100.")
+   (_ "This report is useful to calculate periodic business tax payable/receivable from \
+authorities. From 'Edit report options' above, choose your Business Income and Business Expense accounts. \
+Each transaction may contain, in addition to the accounts payable/receivable or bank accounts, \
+a split to a tax account, e.g. Income:Sales -$1000, Liability:GST on Sales -$100, Asset:Bank $1100.")
    (gnc:html-markup-br)
    (gnc:html-markup-br)
-   (_ "These tax accounts can either be populated using the standard register, or from Business Invoices and Bills
- which will require Tax Tables to be set up correctly. Please see the documentation.")
+   (_ "These tax accounts can either be populated using the standard register, or from Business Invoices and Bills \
+which will require Tax Tables to be set up correctly. Please see the documentation.")
    (gnc:html-markup-br)
    (gnc:html-markup-br)
    (_ "From the Report Options, you will need to select the accounts which will \
