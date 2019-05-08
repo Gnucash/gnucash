62f5436d15def097b800caba9d0d1c7d86b600ff
c-holtermann <mail@c-holtermann.net>
2019-05-08 10:15:28 +0200

    reuse 28877:msgid 'Have a nice day\!'

--- gnucash/python/init.py
+++ gnucash/python/init.py
@@ -99,7 +99,7 @@ class Console (cons.Console):
     def quit (self):
         """ quit """
 
-        self.write(_("\nHave a nice day !\n"))
+        self.write("\n"+_("Have a nice day!")+"\n")
         return super(Console, self).quit()
 
 
