c19eb7587e3982d2910b5d66a75a6a5b0e6687cc
Robert Fewell <14uBobIT@gmail.com>
2019-06-28 15:56:56 +0100

    Check for Root Currency in preference dialogue
    Prior to 5.0 if the Qif import was used from the New User dialogue the
    Currency of the Root Account was not set. When the preference dialogue
    is used a check is made for the Root Account Currency and if not
    present the locale currency is used to set it.

--- gnucash/gnome-utils/dialog-preferences.c
+++ gnucash/gnome-utils/dialog-preferences.c
@@ -79,6 +79,7 @@
 #include "gnc-component-manager.h"
 #include "dialog-preferences.h"
 #include "dialog-doclink-utils.h"
+#include "qofinstance.h"
 
 #define DIALOG_PREFERENCES_CM_CLASS "dialog-newpreferences"
 #define GNC_PREFS_GROUP             "dialogs.preferences"
@@ -1498,7 +1499,21 @@ gnc_preferences_dialog_create (GtkWindow *parent)
     gtk_label_set_label (GTK_LABEL(label), currency_name);
 
     root_currency = xaccAccountGetCommodity (gnc_get_current_root_account ());
-    currency_name = gnc_commodity_get_printname (root_currency);
+
+    /* Prior to version 5.0, if the qif importer was used from 'New User'
+       dialog the Root Account currency was not set */
+    if (!root_currency)
+    {
+        QofBook *book = gnc_get_current_book();
+        Account *root = gnc_get_current_root_account ();
+        xaccAccountBeginEdit (root);
+        xaccAccountSetCommodity (root, locale_currency);
+        xaccAccountCommitEdit (root);
+        qof_book_mark_session_dirty (book);
+        currency_name = gnc_commodity_get_printname (locale_currency);
+    }
+    else
+        currency_name = gnc_commodity_get_printname (root_currency);
     label = GTK_WIDGET(gtk_builder_get_object (builder, "root_currency"));
     gtk_label_set_label (GTK_LABEL(label), currency_name);
     label = GTK_WIDGET(gtk_builder_get_object (builder, "root_currency2"));
