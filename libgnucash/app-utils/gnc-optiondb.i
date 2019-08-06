ade7fc8b6e29a43d7b47db246066c57e6c6af309
John Ralls <jralls@ceridwen.us>
2019-08-06 15:31:44 -0700

    Initial SWIG of GncOptionDB and Scheme tests.
    This class will be heavily used by reports so we need to ensure SWIG and
    Scheme compatibility from the start.

---
+++ libgnucash/app-utils/gnc-optiondb.i
@@ -0,0 +1,18 @@
+/*
+ * Temporary swig interface file while developing C++ options.
+ */
+
+%module sw_gnc_optiondb
+%{
+#include <libguile.h>
+#include "gnc-optiondb.hpp"
+extern "C" SCM scm_init_sw_gnc_optiondb_module(void);
+%}
+
+%include <std_string.i>
+
+%ignore OptionClassifier;
+%ignore GncOption;
+%ignore GncOptionWrapper;
+
+%include "gnc-optiondb.hpp"
---
+++ libgnucash/app-utils/test/test-gnc-optiondb.scm
@@ -0,0 +1,46 @@
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ; test-gnc-option.scm -- unit tests for GncOption class.           ;
+ ; Copyright (C) 2019 John Ralls <jralls@ceridwen.us>               ;
+ ;                                                                  ;
+ ; This program is free software; you can redistribute it and/or    ;
+ ; modify it under the terms of the GNU General Public License as   ;
+ ; published by the Free Software Foundation; either version 2 of   ;
+ ; the License, or (at your option) any later version.              ;
+ ;                                                                  ;
+ ; This program is distributed in the hope that it will be useful,  ;
+ ; but WITHOUT ANY WARRANTY; without even the implied warranty of   ;
+ ; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    ;
+ ; GNU General Public License for more details.                     ;
+ ;                                                                  ;
+ ; You should have received a copy of the GNU General Public License;
+ ; along with this program; if not, contact:                        ;
+ ;                                                                  ;
+ ; Free Software Foundation           Voice:  +1-617-542-5942       ;
+ ; 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       ;
+ ; Boston, MA  02110-1301,  USA       gnu@gnu.org                   ;
+ ;                                                                  ;
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-modules (srfi srfi-64))
+(use-modules (tests srfi64-extras))
+
+(eval-when
+ (compile load eval expand)
+ (load-extension "libswig-gnc-optiondb" "scm_init_sw_gnc_optiondb_module"))
+(use-modules (sw_gnc_optiondb))
+
+(define (run-test)
+  (test-runner-factory gnc:test-runner)
+  (test-begin "test-gnc-optiondb-scheme")
+  (test-gnc-make-text-option)
+  (test-end "test-gnc-optiondb-scheme"))
+
+(define (test-gnc-make-text-option)
+  (test-begin "test-gnc-test-string-option")
+  (let* ((option-db (new-GncOptionDB))
+         (string-opt (gnc-register-string-option option-db "foo" "bar" "baz"
+                                                 "Phony Option" "waldo")))
+    (test-equal (GncOptionDB-lookup-option option-db "foo" "bar") "waldo")
+    (delete-GncOptionDB option-db))
+
+  (test-end "test-gnc-make-string-option"))
