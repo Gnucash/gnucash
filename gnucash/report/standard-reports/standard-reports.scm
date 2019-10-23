d881130a194a4d14bc1036a23408e51161720ec9
Christopher Lam <christopher.lck@gmail.com>
2019-10-23 19:27:20 +0800

    [standard-reports] convert to srfi-9
    because 2-arg record-constructor is being deprecated.
    
    http://git.savannah.gnu.org/cgit/guile.git/commit/?id=f7b4055b

--- gnucash/report/standard-reports/standard-reports.scm
+++ gnucash/report/standard-reports/standard-reports.scm
@@ -25,6 +25,7 @@
 
 
 (define-module (gnucash report standard-reports))
+(use-modules (srfi srfi-9))
 (use-modules (srfi srfi-13))
 (use-modules (gnucash utilities))
 (use-modules (gnucash core-utils))
@@ -39,25 +40,14 @@
 ;; or without split.  If no function is found, then run the 'default'
 ;; function
 
-(define acct-type-info (make-record-type "AcctTypeInfo" '(split non-split)))
-
-(define make-acct-type-private
-  (record-constructor acct-type-info '(split non-split)))
+(define-record-type :acct-type-info
+  (make-acct-type-private split non-split)
+  acct-type?
+  (split get-split set-split)
+  (non-split get-non-split set-non-split))
 
 (define (make-acct-type)
   (make-acct-type-private #f #f))
-
-(define get-split
-  (record-accessor acct-type-info 'split))
-
-(define set-split
-  (record-modifier acct-type-info 'split))
-
-(define get-non-split
-  (record-accessor acct-type-info 'non-split))
-
-(define set-non-split
-  (record-modifier acct-type-info 'non-split))
 
 (define (gnc:register-report-hook acct-type split? create-fcn)
   (let ((type-info (hash-ref gnc:*register-report-hash* acct-type (make-acct-type))))
