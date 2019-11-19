6eb0ccfa1172a20074c03e1b1fdf1c730edd09ad
Geert Janssens <geert@kobaltwit.be>
2019-11-19 16:40:41 +0100

    gnc-module - move guile wrappers to bindings/guile

--- bindings/guile/engine-utilities.scm
+++ bindings/guile/engine-utilities.scm
@@ -20,8 +20,6 @@
 ;; Boston, MA  02110-1301,  USA       gnu@gnu.org
 
 ;; Copyright 2000 Rob Browning <rlb@cs.utexas.edu>
-(use-modules (gnucash gnc-module))
-
 (eval-when
       (compile load eval expand)
       (load-extension "libgnucash-guile" "gnc_guile_bindings_init"))
