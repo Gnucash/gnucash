043c6367a5352f9be861eacd70ba2a8d0aac01df
Geert Janssens <geert@kobaltwit.be>
2019-11-21 16:55:17 +0100

    gnc-module test updates
    - move test modules into a subdirectory on Windows as well
    - move the futuremod module into its own subdirectory
      to avoid its load warnings each time gnc_module_init is called
      That also tends to happen when building guile modules.
    - remove the log handlers filtering out the futuremodsys warnings
      They didn't match the actual warning signature anyway and
      they're no longer emitted during testing

--- bindings/guile/test/test-gnc-module-load-deps.scm
+++ bindings/guile/test/test-gnc-module-load-deps.scm
@@ -1,12 +1,3 @@
-(use-modules (tests unittest-support))
-(define log-domain "gnc.module")
-(define check (new-TestErrorStruct))
-(define log-level (G-LOG-LEVEL-WARNING))
-(define msg "Module '../../../libgnucash/gnc-module/test/misc-mods/.libs/libgncmod_futuremodsys.so' requires newer module system\n")
-(TestErrorStruct-log-domain-set check log-domain)
-(TestErrorStruct-log-level-set check log-level)
-(TestErrorStruct-msg-set check msg)
-(define handler (test-set-checked-handler log-domain log-level check))
 (use-modules (gnucash gnc-module))
 
 (gnc:module-system-init)
@@ -16,7 +7,5 @@
 (foo-hello)
 (baz:scheme-hello)
 (foo:scheme-hello)
-
-(g-log-remove-handler log-domain handler)
 
 (exit 0)
