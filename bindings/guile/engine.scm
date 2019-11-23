ee722b85c1337cb41aa79423e5db15a78bffb226
Geert Janssens <geert@kobaltwit.be>
2019-11-23 15:51:20 +0100

    bindings/guile cleanup - fold engine-utilities.scm into engine.scm
    It was only exposed via engine.scm anyway and combining them
    in one file eliminates one explicit load_extension and sw_engine call.

--- bindings/guile/engine.scm
+++ bindings/guile/engine.scm
@@ -19,6 +19,9 @@
 
 (define-module (gnucash engine))
 
+(use-modules (srfi srfi-1)
+             (srfi srfi-13))
+
 (eval-when
       (compile load eval expand)
       (load-extension "libgnucash-guile" "gnc_guile_bindings_init"))
@@ -63,13 +66,6 @@
 (export GNC_COMMODITY_NS_EUREX)
 (export GNC_COMMODITY_NS_MUTUAL)
 
-;; engine-utilities.scm
-(export gnc:account-map-descendants)
-(export gnc:account-map-children)
-
-(export account-full-name<?)
-(export accounts-get-children-depth)
-
 ; business-core.scm
 (export gnc:owner-get-address)
 (export gnc:owner-get-name-dep)
@@ -80,5 +76,31 @@
 
 (load-from-path "gnucash/engine/gnc-numeric")
 (load-from-path "gnucash/engine/commodity-table")
-(load-from-path "gnucash/engine/engine-utilities")
 (load-from-path "gnucash/engine/business-core")
+
+;; A few account related utility functions which used to be in engine-utilities.scm
+(define (gnc:account-map-descendants thunk account)
+        (let ((descendants (or (gnc-account-get-descendants-sorted account) '())))
+             (map thunk descendants)))
+
+(define (gnc:account-map-children thunk account)
+        (let ((children (or (gnc-account-get-children-sorted account) '())))
+             (map thunk children)))
+
+;; account related functions
+;; helper for sorting of account list
+(define (account-full-name<? a b)
+        (string<? (gnc-account-get-full-name a) (gnc-account-get-full-name b)))
+
+;; return maximum depth over accounts and their children, if any
+(define (accounts-get-children-depth accounts)
+        (apply max
+            (map (lambda (acct)
+                         (let ((acct-depth (gnc-account-get-current-depth acct)))
+                              (+ acct-depth (- (gnc-account-get-tree-depth acct) 1))))
+                 accounts)))
+
+(export gnc:account-map-descendants)
+(export gnc:account-map-children)
+(export account-full-name<?)
+(export accounts-get-children-depth)
