44ad75a356efc3a3c5032a59ff16f2e93d13e947
Josh Soref <jsoref@users.noreply.github.com>
2020-01-02 04:21:06 -0500

    spelling: nonsensical

--- gnucash/register/ledger-core/split-register-control.c
+++ gnucash/register/ledger-core/split-register-control.c
@@ -128,7 +128,7 @@ gnc_split_register_balance_trans (SplitRegister *reg, Transaction *trans)
         other_account = xaccSplitGetAccount (split);
 
     /*  If the two pointers are still the same, we have two splits, but
-     *  they both refer to the same account. While non-sensical, we don't
+     *  they both refer to the same account. While nonsensical, we don't
      *  object.   */
 
     if (default_account == other_account)
