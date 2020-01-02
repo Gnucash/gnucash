4ecbc6f674ced986658799dade286c3ff0d04fc7
Josh Soref <jsoref@users.noreply.github.com>
2020-01-01 23:19:53 -0500

    spelling: permissible

--- libgnucash/engine/SchedXaction.c
+++ libgnucash/engine/SchedXaction.c
@@ -661,7 +661,7 @@ xaccSchedXactionGetEndDate(const SchedXaction *sx )
 void
 xaccSchedXactionSetEndDate( SchedXaction *sx, const GDate *newEnd )
 {
-/* Note that an invalid GDate IS a permissable value: It means that
+/* Note that an invalid GDate IS a permissible value: It means that
  * the SX is to run "forever". See gnc_sxed_save_sx() and
  * schedXact_editor_populate() in dialog-sx-editor.c.
  */
