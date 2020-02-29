f27215d5399b9e3e3cb19594926b8ffd23b453c9
c-holtermann <mail@c-holtermann.net>
2020-02-29 15:46:08 +0100

    improve output and explain code for python shell

--- gnucash/python/init.py
+++ gnucash/python/init.py
@@ -7,9 +7,12 @@ require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import os
 sys.path.append(os.path.dirname(__file__))
+# output extra debug information if gnucash has been started with
+# gnucash --debug --extra
 noisy = gnc_prefs_is_extra_enabled()
 if noisy:
-    print("woop", os.path.dirname(__file__))
+    print("Hello from python!")
+    print("Python file: %s" % (__file__))
 # Importing the console class causes SIGTTOU to be thrown if GnuCash is
 # started in the background.  This causes a hang if it is not handled, 
 # so ignore it for the duration
@@ -22,14 +25,13 @@ import pycons.console as cons
 signal.signal(signal.SIGTTOU, old_sigttou)
 
 if noisy:
-    print("Hello from python!")
-    print("test", sys.modules.keys())
-    print("test2", dir(_sw_app_utils))
+    print("\nsys.modules.keys(): ", sys.modules.keys())
+    print("\ndir(_sw_app_utils): ", dir(_sw_app_utils))
 
    #root = _sw_app_utils.gnc_get_current_root_account()
 
    #print("test", dir(root), root.__class__)
-    print("test3", dir(gnucash_core_c))
+    print("\ndir(gnucash_core_c): ", dir(gnucash_core_c))
 
    #acct = Account(instance = root)
 
