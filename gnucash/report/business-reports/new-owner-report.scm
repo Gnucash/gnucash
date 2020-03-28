63ce37e2e577c49e52e5f88e2ecffa0b348220c1
Frank H. Ellenberger <frank.h.ellenberger@gmail.com>
2020-03-28 03:42:38 +0100

    I18N: *owner-reports: Unify 'a' vs. 'the' in tooltips

--- gnucash/report/business-reports/new-owner-report.scm
+++ gnucash/report/business-reports/new-owner-report.scm
@@ -891,7 +891,7 @@
   (gnc:register-inv-option
    (gnc:make-simple-boolean-option
     (N_ "Display Columns") credit-header
-    "had" (N_ "Display a period credits column?") #t))
+    "had" (N_ "Display the period credits column?") #t))
 
   (gnc:register-inv-option
    (gnc:make-simple-boolean-option
