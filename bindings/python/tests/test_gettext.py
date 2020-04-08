749457bafb56e82d3187cba9597c2e6d8c2d7828
c-holtermann <mail@c-holtermann.net>
2020-04-08 07:33:15 +0200

    add test for gettext

---
+++ bindings/python/tests/test_gettext.py
@@ -0,0 +1,22 @@
+# test gettext
+#
+# @date 2020-04-08
+# @author Christoph Holtermann <mail@c-holtermann.net>
+
+from unittest import TestCase, main
+import gettext
+import gnucash
+from gnucash import _sw_core_utils
+
+class TestGettext(TestCase):
+    def test_import_gettext(self):
+        import gettext
+
+    def test_get_localedir(self):
+        _localedir = _sw_core_utils.gnc_path_get_localedir()
+
+    def test_translation(self):
+        self.assertTrue("Project-Id-Version: GnuCash" in _(""))
+
+if __name__ == '__main__':
+    main()
