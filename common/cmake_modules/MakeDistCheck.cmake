1365a19e2ea8095c9a18e424daa3502ada1329a5
John Ralls <jralls@ceridwen.us>
2020-04-12 15:51:05 -0700

    Force WITH_PYTHON=YES for distcheck.
    To ensure that it works from the tarball.

--- common/cmake_modules/MakeDistCheck.cmake
+++ common/cmake_modules/MakeDistCheck.cmake
@@ -47,6 +47,7 @@ function(run_dist_check PACKAGE_PREFIX EXT)
               -D CMAKE_INSTALL_PREFIX=../${INSTALL_DIR}
               -D GTEST_ROOT=${GTEST_ROOT}
               -D GMOCK_ROOT=${GMOCK_ROOT}
+              -D WITH_PYTHON=YES
               ../${PACKAGE_PREFIX}
             WORKING_DIRECTORY ${BUILD_DIR}
             ERROR_MSG "CMake configure command failed."
