2c09fe4f44fa928112439f0123779df1546a25a9
c-holtermann <mail@c-holtermann.net>
2020-04-27 22:08:15 +0200

    debug on travis

--- bindings/python/tests/test_gettext.py
+++ bindings/python/tests/test_gettext.py
@@ -8,6 +8,9 @@ import gettext
 import gnucash
 from gnucash import _sw_core_utils
 
+class MyException(Exception):
+    pass
+
 class TestGettext(TestCase):
     def test_import_gettext(self):
         import gettext
@@ -16,6 +19,7 @@ class TestGettext(TestCase):
         _localedir = _sw_core_utils.gnc_path_get_localedir()
 
     def test_translation(self):
+        raise MyException({'_(""): ':_(""), '_sw_core_utils.gnc_path_get_localedir(): ':_sw_core_utils.gnc_path_get_localedir()})
         self.assertTrue("Project-Id-Version: GnuCash" in _(""))
 
 if __name__ == '__main__':
