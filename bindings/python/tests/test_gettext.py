f788e593185ef3bcf460c330ac81bfe200bd8aba
c-holtermann <mail@c-holtermann.net>
2020-04-28 20:57:58 +0200

    remove debug stuff (temporarily)

--- bindings/python/tests/test_gettext.py
+++ bindings/python/tests/test_gettext.py
@@ -4,7 +4,7 @@
 # @author Christoph Holtermann <mail@c-holtermann.net>
 
 from unittest import TestCase, main
-import gettext
+# import gettext
 import gnucash
 from gnucash import _sw_core_utils
 
@@ -18,11 +18,11 @@ class TestGettext(TestCase):
     def test_get_localedir(self):
         _localedir = _sw_core_utils.gnc_path_get_localedir()
 
-    def test_translation(self):
-        import inspect
-        import locale
-        raise MyException({'_(""): ':_(""), '_sw_core_utils.gnc_path_get_localedir(): ':_sw_core_utils.gnc_path_get_localedir(),'_.__doc__': _.__doc__, 'inspect.getsource(_): ': inspect.getsource(_), '_("Welcome to GnuCash")':_("Welcome to GnuCash"), 'locale.getlocale(): ': locale.getlocale()})
-        self.assertTrue("Project-Id-Version: GnuCash" in _(""))
+    #def test_translation(self):
+    #    import inspect
+    #    import locale
+    #    raise MyException({'_(""): ':_(""), '_sw_core_utils.gnc_path_get_localedir(): ':_sw_core_utils.gnc_path_get_localedir(),'_.__doc__': _.__doc__, 'inspect.getsource(_): ': inspect.getsource(_), '_("Welcome to GnuCash")':_("Welcome to GnuCash"), 'locale.getlocale(): ': locale.getlocale()})
+    #    self.assertTrue("Project-Id-Version: GnuCash" in _(""))
 
 if __name__ == '__main__':
     main()
