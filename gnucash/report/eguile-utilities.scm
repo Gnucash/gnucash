0c1b6c5a4faf7070fe06db4aebbec67528828e4d
Christopher Lam <christopher.lck@gmail.com>
2020-05-09 21:34:38 +0800

    [eguile-utilities] deprecate single-use function
    only used by balsheet-eg.scm

--- gnucash/report/eguile-utilities.scm
+++ gnucash/report/eguile-utilities.scm
@@ -39,7 +39,8 @@
 (define-public fmtnumeric fmtnumber)
 
 (define-public (gnc-monetary-neg? monetary)
-  ; return true if the monetary value is negative
+  ;; return true if the monetary value is negative
+  (issue-deprecated-warning "gnc-monetary-neg? is deprecated")
   (negative? (gnc:gnc-monetary-amount monetary)))
 
 ;; 'Safe' versions of cdr and cadr that don't crash
