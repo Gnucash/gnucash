159ceb64908932d8b64fc2148535b37e3a0e1316
Mike Alexander <mta@umich.edu>
2020-05-25 02:40:51 -0400

    Don't call gnc_path_get_localedir vefore binreloc is initialized.

--- gnucash/gnucash-bin.c
+++ gnucash/gnucash-bin.c
@@ -846,7 +846,7 @@ redirect_stdout (void)
 int
 main(int argc, char ** argv)
 {
-    gchar *localedir = gnc_path_get_localedir();
+    gchar *localedir;
 #if !defined(G_THREADS_ENABLED) || defined(G_THREADS_IMPL_NONE)
 #    error "No GLib thread implementation available!"
 #endif
@@ -882,6 +882,7 @@ main(int argc, char ** argv)
         setlocale (LC_ALL, "C");
       }
 #endif
+    localedir = gnc_path_get_localedir();
     bindtextdomain(PROJECT_NAME, localedir);
     bindtextdomain("iso_4217", localedir); // For win32 to find currency name translations
     bind_textdomain_codeset("iso_4217", "UTF-8");
