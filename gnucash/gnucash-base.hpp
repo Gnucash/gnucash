c58cfdb87dfaa90bbb0d3b4710e3198d651819aa
Geert Janssens <geert@kobaltwit.be>
2020-05-29 14:06:21 +0200

    Extract common setup bits from gnucash and gnucash-cli into a separate class Gnucash::Base
    This is just a first start, more refactoring will follow in later commits.
    The idea is to have an application class that provides the basic framework
    gnucash and gnucash-cli will become specializations of this class adding their
    specific functionality. I'm splitting this over several commits to be able to
    keep track of all the many changes.

---
+++ gnucash/gnucash-base.hpp
@@ -0,0 +1,59 @@
+/*
+ * gnucash-base.hpp -- Basic application object for gnucash binaries
+ *
+ * Copyright (C) 2020 Geert Janssens <geert@kobaltwit.be>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact:
+ *
+ * Free Software Foundation           Voice:  +1-617-542-5942
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org
+ */
+
+#ifndef GNUCASH_BASE_HPP
+#define GNUCASH_BASE_HPP
+
+namespace Gnucash {
+
+class Base
+{
+public:
+    Base ();
+
+    void parse_command_line (int *argc, char ***argv);
+    void start (void);
+
+    const char *get_file_to_load (void);
+    int get_no_file (void);
+    const char *get_quotes_file (void);
+
+private:
+
+    /* Command-line option variables */
+    int gnucash_show_version = 0;
+    int debugging = 0;
+    int extra = 0;
+    gchar **log_flags = NULL;
+    char *log_to_filename = NULL;
+    int nofile = 0;
+    const gchar *gsettings_prefix = NULL;
+    const char *add_quotes_file = NULL;
+    char *namespace_regexp = NULL;
+    const char *file_to_load = NULL;
+    gchar **args_remaining = NULL;
+    gchar *sys_locale = NULL;
+};
+
+}
+#endif
