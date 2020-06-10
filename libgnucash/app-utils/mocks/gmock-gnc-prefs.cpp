8425af784e1ef9c5ab62cff82f76f999b9012bb4
Christian Gruber <christian.gruber@posteo.de>
2020-06-10 22:04:30 +0200

    Check pointer to preferences backend before using it

--- libgnucash/app-utils/mocks/gmock-gnc-prefs.cpp
+++ libgnucash/app-utils/mocks/gmock-gnc-prefs.cpp
@@ -13,41 +13,53 @@ gmock_gnc_prefs_set_backend(MockPrefsBackend *backend)
 gboolean
 gnc_prefs_get_bool (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getBool(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getBool(group, pref_name) : FALSE;
 }
 
 gint
 gnc_prefs_get_int (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getInt(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getInt(group, pref_name) : 0;
 }
 
 gint64
 gnc_prefs_get_int64 (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getInt64(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getInt64(group, pref_name) : 0;
 }
 
 gdouble
 gnc_prefs_get_float (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getFloat(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getFloat(group, pref_name) : 0.0;
 }
 
 gchar *
 gnc_prefs_get_string (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getString(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getString(group, pref_name) : NULL;
 }
 
 gint
 gnc_prefs_get_enum (const gchar *group, const gchar *pref_name)
 {
-    return prefsbackend->getEnum(group, pref_name);
+    EXPECT_NE(prefsbackend, nullptr);
+    return prefsbackend ? prefsbackend->getEnum(group, pref_name) : 0;
 }
 
 void
 gnc_prefs_get_coords (const gchar *group, const gchar *pref_name, gdouble *x, gdouble *y)
 {
-    prefsbackend->getCoords(group, pref_name, x, y);
+    EXPECT_NE(prefsbackend, nullptr);
+
+    *x = 0.0;
+    *y = 0.0;
+
+    if (prefsbackend != nullptr)
+        prefsbackend->getCoords(group, pref_name, x, y);
 }
