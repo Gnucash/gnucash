94c61e15f337d222d249a68ffcfb6b362faf2fd2
Christopher Lam <christopher.lck@gmail.com>
2020-08-16 21:51:07 +0800

    [trep-engine] export gnc:lists->csv
    converts a list of lists to CSV string.

--- gnucash/report/trep-engine.scm
+++ gnucash/report/trep-engine.scm
@@ -465,6 +465,7 @@ Credit Card, and Income accounts."))
   (and (keylist-get-info (sortkey-list split-action?) sortkey 'split-sortvalue)
        (not (keylist-get-info (sortkey-list split-action?) sortkey 'sortkey))))
 
+(define-public gnc:lists->csv lists->csv)
 (define (lists->csv lst)
   ;; converts a list of lists into CSV
   ;; this function aims to follow RFC4180, and will pad lists to
