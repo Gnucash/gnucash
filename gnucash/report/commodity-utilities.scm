Gnucash/gnucash

commit e7165507c8497bfbcf7d3b107ca4e9ed2912a9d3

[commodity-utils.scm][api] gnc:html-make-rates-table with price-fn
better precision than gnc:html-make-exchangerates

gnucash/report/commodity-utilities.scm
@@ -21,6 +21,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-modules (ice-9 match))
+(use-modules (srfi srfi-26))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Functions to get splits with interesting data from accounts.
@@ -763,6 +764,18 @@
        (lambda (foreign domestic)
          (gnc:exchange-by-pricedb-nearest
           foreign domestic to-date-tp))))))
+
+(define (gnc:case-price-fn source target-curr date)
+  (define pdb (gnc-pricedb-get-db (gnc-get-current-book)))
+  (case source
+    ((pricedb-nearest) (cut gnc-pricedb-get-nearest-price pdb <> target-curr date))
+    ((pricedb-latest)  (cut gnc-pricedb-get-latest-price pdb <> target-curr))
+    (else
+     (lambda (commodity)
+       (let* ((exchange-fn (gnc:case-exchange-fn source target-curr date))
+              (foreign-mon (gnc:make-gnc-monetary commodity 1))
+              (domestic-mon (exchange-fn foreign-mon target-curr)))
+         (gnc:gnc-monetary-amount domestic-mon))))))
 
 ;; Return a ready-to-use function. Which one to use is determined by
 ;; the value of 'source-option', whose possible values are set in

gnucash/report/html-utilities.scm
@@ -228,6 +228,30 @@
                             (G_ "Exchange rates"))))))
     table))
 
+;; Create a html-table of all prices. The report-currency is
+;; 'currency', The prices are given through the function 'price-fn'
+;; and the 'accounts' determine which commodities to show. Returns a
+;; html-object, a <html-table>. price-fn is easily obtained from
+;; gnc:case-price-fn
+(define (gnc:html-make-rates-table currency price-fn accounts)
+  (define (cell c) (gnc:make-html-table-cell/markup "number-cell" c))
+  (define table (gnc:make-html-table))
+  (let lp ((comm-list (gnc:accounts-get-commodities accounts currency)) (entries 0))
+    (match comm-list
+      (()
+       (unless (zero? entries)
+         (gnc:html-table-set-col-headers!
+          table (list (gnc:make-html-table-header-cell/size
+                       1 2 (if (= entries 1) (G_ "Exchange rate")
+                               (G_ "Exchange rates"))))))
+       table)
+      ((comm . rest)
+       (gnc:html-table-append-row!
+        table
+        (list (cell (gnc:make-gnc-monetary comm 1))
+              (cell (gnc:default-price-renderer currency (price-fn comm)))))
+       (lp rest (1+ entries))))))
+
 
 (define (gnc:html-make-generic-budget-warning report-title-string)
   (gnc:html-make-generic-simple-warning

gnucash/report/report.scm
@@ -58,6 +58,7 @@
 (export gnc:exchange-by-pricealist-nearest)
 (export gnc:case-exchange-fn)
 (export gnc:case-exchange-time-fn)
+(export gnc:case-price-fn)
 (export gnc:sum-collector-commodity)
 
 ;; options-utilities.scm
@@ -104,6 +105,7 @@
 (export gnc:assign-colors)
 (export gnc:html-table-append-ruler!)
 (export gnc:html-make-exchangerates)
+(export gnc:html-make-rates-table)
 (export gnc:html-render-options-changed)
 (export gnc:html-make-generic-warning)
 (export gnc:html-make-no-account-warning)

commit a6c8a806f37f1ba182e954942324fff5cb95dc2b

Bug 797982 - exchange rates' decimal places (bis)
Bug introduced in e7165507c. We need to normalize date for
pricedb-nearest to avoid getting next day price.

gnucash/report/commodity-utilities.scm
@@ -768,7 +768,8 @@
 (define (gnc:case-price-fn source target-curr date)
   (define pdb (gnc-pricedb-get-db (gnc-get-current-book)))
   (case source
-    ((pricedb-nearest) (cut gnc-pricedb-get-nearest-price pdb <> target-curr date))
+    ((pricedb-nearest) (cut gnc-pricedb-get-nearest-price pdb <> target-curr
+                            (time64CanonicalDayTime date)))
     ((pricedb-latest)  (cut gnc-pricedb-get-latest-price pdb <> target-curr))
     (else
      (lambda (commodity)