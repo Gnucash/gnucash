ffbd09089c0e33f29a4324fd7804da53f84c435f
Christopher Lam <christopher.lck@gmail.com>
2020-12-05 00:33:27 +0800

    [modularise] (gnucash eguile)

--- gnucash/report/eguile-utilities.scm
+++ gnucash/report/eguile-utilities.scm
@@ -22,6 +22,8 @@
 ;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 ;; 02111-1307 USA
 
+(define-module (gnucash eguile eguile-utilities))
+
 (use-modules (ice-9 match))
 ; using all of these seems like overkill -- 
 ; not sure which are really required
