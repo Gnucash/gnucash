8b772384cd30cac87f1f2bb942e9ce42ad229571
Geert Janssens <geert@kobaltwit.be>
2021-01-29 11:57:28 +0100

    Various performance fixes based on feedback - const correctnes - avoid unnecessary copying - avoid need to reverse GList

--- gnucash/gnucash-cli.cpp
+++ gnucash/gnucash-cli.cpp
@@ -132,7 +132,7 @@ Gnucash::GnucashCli::start ([[maybe_unused]] int argc, [[maybe_unused]] char **a
         if (*m_quotes_cmd == "info")
         {
             auto quotes = gnc_get_quotes_instance();
-            if (quotes.check())
+            if (quotes.cmd_result() == 0)
             {
                 std::cout << bl::format (bl::translate ("Found Finance::Quote version {1}.")) % quotes.version() << std::endl;
                 std::cout << bl::translate ("Finance::Quote sources: ");
