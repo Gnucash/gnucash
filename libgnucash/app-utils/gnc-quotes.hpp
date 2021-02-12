6ecc1ef73f134b0045a7df78245bb2a9544f15fc
Geert Janssens <geert@kobaltwit.be>
2021-02-12 18:18:20 +0100

    GncQuotes - switch to Pimpl idiom
    That allows the private implementation to pass a number of variables
    based on various boost libraries. It's better to not have them in
    the public interface to keep compilation times down.

--- libgnucash/app-utils/gnc-quotes.hpp
+++ libgnucash/app-utils/gnc-quotes.hpp
@@ -37,6 +37,7 @@ using CmdOutput = std::pair <StrVec, StrVec>;
 
 const std::string not_found = std::string ("Not Found");
 
+class GncQuotesImpl;
 
 class GncQuotes
 {
@@ -44,35 +45,19 @@ public:
     // Constructor - checks for presence of Finance::Quote and import version and quote sources
     GncQuotes ();
     GncQuotes (QofBook *book);
-    ~GncQuotes () = default;
+    ~GncQuotes ();
 
     void fetch_all ();
     void fetch (const CommVec& commodities);
 
-    const int cmd_result() noexcept { return m_cmd_result; }
-    const std::string& error_msg() noexcept { return m_error_msg; }
-    const std::string& version() noexcept { return m_version.empty() ? not_found : m_version; }
-    const QuoteSources& sources() noexcept { return m_sources; }
+    const int cmd_result() noexcept;
+    const std::string& error_msg() noexcept;
+    const std::string& version() noexcept;
+    const QuoteSources& sources() noexcept;
     GList* sources_as_glist ();
 
 private:
-    GncQuotes () = delete;
-    // Check if Finance::Quote is properly installed
-    void check (QofBook *book);
-    // Run the command specified. Returns two vectors for further processing by the caller
-    // - one with the contents of stdout
-    // - one with the contents of stderr
-    // Will also set m_cmd_result
-    template <typename BufferT> CmdOutput run_cmd (std::string cmd_name, StrVec args, BufferT input_vec);
-
-    void parse_quotes (std::string);
-
-
-    std::string m_version;
-    QuoteSources m_sources;
-    int m_cmd_result;
-    std::string m_error_msg;
-    QofBook *m_book;
+    std::unique_ptr<GncQuotesImpl> m_impl;
 };
 
 #endif /* GNC_QUOTES_HPP */
