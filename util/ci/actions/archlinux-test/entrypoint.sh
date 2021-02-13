ba0a21c7154d168e3ad18a65bf452bc70b74ea7d
John Ralls <jralls@ceridwen.us>
2021-02-13 11:42:52 -0800

    Add /usr/bin/core_perl to the PATH for ArchLinux Dockers.
    Apparently perl's PKGCONFIB doesn't do so any more.

--- util/ci/actions/archlinux-test/entrypoint.sh
+++ util/ci/actions/archlinux-test/entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p "$HOME"/.local/share
 mkdir build
 cd build
 export TZ="America/Los_Angeles"
-
+export PATH="$PATH:/usr/bin/core_perl"
 cmake /github/workspace -DWITH_PYTHON=ON -DCMAKE_BUILD_TYPE=debug -G Ninja
 ninja
 ninja check
