6537135e7072b39c90f065d04eec7b61810382c5
Geert Janssens <geert@kobaltwit.be>
2021-03-18 14:41:40 +0100

    Restructure early locale initialization
    - Move Macos specific bits to its own source file. As this is objective-c use .mm extension
    - Use common function signature for Macos and Windows init function and include via common header file

---
+++ gnucash/gnucash-locale-platform.h
@@ -0,0 +1,30 @@
+/*
+ * gnucash-locale-platform.h -- Common header for platform specific locale handling
+ *
+ * Copyright (C) 2021 Geert Janssens <geert@kobaltwit.be>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, contact:
+ *
+ * Free Software Foundation           Voice:  +1-617-542-5942
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org
+ */
+
+
+#ifndef GNUCASH_LOCALE_PLATFORM_H
+#define GNUCASH_LOCALE_PLATFORM_H
+
+char *set_platform_locale();
+
+#endif
