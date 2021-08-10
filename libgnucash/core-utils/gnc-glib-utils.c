e84549926bbeaf6f19428f1aeac1b96eeb860b6e
Christopher Lam <christopher.lck@gmail.com>
2021-08-10 12:55:47 +0800

    [gnc-glib-utils] gnc_g_list_stringjoin to join a GList of strings
    It traverses the GList twice (once to calculate the length) but
    allocates only once.
    
    Includes snippet from
    https://www.joelonsoftware.com/2001/12/11/back-to-basics/

--- libgnucash/core-utils/gnc-glib-utils.c
+++ libgnucash/core-utils/gnc-glib-utils.c
@@ -327,3 +327,35 @@ void gnc_gpid_kill(GPid pid)
     }
 #endif /* G_OS_WIN32 */
 }
+
+static inline char*
+gnc_strcat (char* dest, const char* src)
+{
+    while (*dest) dest++;
+    while ((*dest++ = *src++));
+    return --dest;
+}
+
+gchar *
+gnc_g_list_stringjoin (GList *list_of_strings, const gchar *sep)
+{
+    gint seplen = sep ? strlen(sep) : 0;
+    gint length = -seplen;
+    gchar *retval, *p;
+
+    if (!list_of_strings)
+        return NULL;
+
+    for (GList *n = list_of_strings; n; n = n->next)
+        length += strlen ((gchar*)n->data) + seplen;
+
+    p = retval = (gchar*) g_malloc0 (length * sizeof (gchar) + 1);
+    for (GList *n = list_of_strings; n; n = n->next)
+    {
+        p = gnc_strcat (p, (gchar*)n->data);
+        if (n->next && sep)
+            p = gnc_strcat (p, sep);
+    }
+
+    return retval;
+}
