37895bfdad7abbab705925dd947f876cdaa1a878
Christopher Lam <christopher.lck@gmail.com>
2021-08-12 22:30:39 +0800

    [reports] remove deprecated owner-report creation functions

--- gnucash/report/reports/standard/new-owner-report.scm
+++ gnucash/report/reports/standard/new-owner-report.scm
@@ -1240,10 +1240,6 @@ and do not match the transaction."))))))))
              (guid (assv-ref guid-alist type)))
     (owner-report-create-internal guid owner type enddate)))
 
-(define (owner-report-create owner account)
-  (issue-deprecation-warning "owner-report-create is not used anymore. call owner-report-create-with-enddate instead")
-  (owner-report-create-with-enddate owner account #f))
-
 (define (gnc:owner-report-create-internal
          account split query journal? double? title debit-string credit-string)
 
@@ -1256,5 +1252,4 @@ and do not match the transaction."))))))))
 
 (gnc:register-report-hook ACCT-TYPE-RECEIVABLE #t gnc:owner-report-create-internal)
 (gnc:register-report-hook ACCT-TYPE-PAYABLE #t gnc:owner-report-create-internal)
-(export owner-report-create)            ;deprecate
 (export owner-report-create-with-enddate)
