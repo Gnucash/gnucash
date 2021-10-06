0bce6a1d56857436af6060da8555e8696fed7796
Christopher Lam <christopher.lck@gmail.com>
2021-10-06 09:19:14 +0800

    [core-utils] introduce gnc:format
    (gnc:format str [binding value]...)
    
    str will contain ${binding} which will be replaced to value.

--- bindings/guile/test/test-core-utils.scm
+++ bindings/guile/test/test-core-utils.scm
@@ -13,8 +13,37 @@
     "foobar"
     (N_ "foobar")))
 
+(define (gnc-format-tests)
+  (test-equal "null"
+    ""
+    (gnc:format ""))
+
+  (test-equal "basic"
+    "basic"
+    (gnc:format "basic"))
+
+  (test-equal "basic with unused symbols"
+    "basic"
+    (gnc:format "basic" 'task "testing"))
+
+  (test-equal "one substitution"
+    "basic test"
+    (gnc:format "basic ${job}" 'job "test"))
+
+  (test-equal "two substitutions out of order"
+    "basic test"
+    (gnc:format "${difficulty} ${job}" 'job "test" 'difficulty "basic"))
+
+  (test-equal "trying to reference invalid symbol"
+    "${symbol} does not exist"
+    (gnc:format "${symbol} does not exist" 'existence "none"))
+
+  (test-error "gnc:format syntax error"
+    (gnc:format "${symbol} does not exist" 'existence)))
+
 (define (run-test)
   (test-runner-factory gnc:test-runner)
   (test-begin "test-core-utils")
   (N_-tests)
+  (gnc-format-tests)
   (test-end "test-core-utils"))
