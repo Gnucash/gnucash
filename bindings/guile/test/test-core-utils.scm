34a4b652c1c2849624713e9f70bf7b8460146797
Christopher Lam <christopher.lck@gmail.com>
2021-10-07 19:20:32 +0800

    [test-core-utils.scm] fix - N_ is no longer a macro

--- bindings/guile/test/test-core-utils.scm
+++ bindings/guile/test/test-core-utils.scm
@@ -2,16 +2,16 @@
 (setenv "GNC_UNINSTALLED" "1")
 (use-modules (gnucash core-utils))
 
-(if (macro? (module-ref (current-module) 'N_))
-    (display "Macro N_ defined\n")
+(if (procedure? (module-ref (current-module) 'N_))
+    (display "N_ defined\n")
     (begin
-      (display "Failed - macro N_ not defined\n")
+      (display "Failed - N_ not defined\n")
       (set! exit-code -1)))
 
 (if (string=? (N_ "foobar") "foobar")
-    (display "Macro N_ works properly\n")
+    (display "N_ works properly\n")
     (begin
-      (display "Failed - macro N_ doesn't work\n")
+      (display "Failed - N_ doesn't work\n")
       (set! exit-code -1)))
 
 (exit exit-code)
