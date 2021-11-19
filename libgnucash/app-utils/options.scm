dd7feb99887278982f5dfc898fac44feb770672c
John Ralls <jralls@ceridwen.us>
2021-11-19 11:45:54 -0800

    options.scm: Remove second license comment.

--- libgnucash/app-utils/options.scm
+++ libgnucash/app-utils/options.scm
@@ -356,23 +356,6 @@
 ;;
 ;; Created by:	Derek Atkins <derek@ihtfp.com>
 ;;
-;; This program is free software; you can redistribute it and/or    
-;; modify it under the terms of the GNU General Public License as   
-;; published by the Free Software Foundation; either version 2 of   
-;; the License, or (at your option) any later version.              
-;;                                                                  
-;; This program is distributed in the hope that it will be useful,  
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of   
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
-;; GNU General Public License for more details.                     
-;;                                                                  
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, contact:
-;;
-;; Free Software Foundation           Voice:  +1-617-542-5942
-;; 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
-;; Boston, MA  02110-1301,  USA       gnu@gnu.org
-
 
 ;; Internally, values are always a guid. Externally, both guids and
 ;; invoice pointers may be used to set the value of the option. The
