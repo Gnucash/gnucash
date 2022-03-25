7d94ade599abee4ea90389bb7bdd57ae3734f825
John Ralls <jralls@ceridwen.us>
2022-03-25 13:35:31 -0700

    [python]Remove non-namespace "All non-currency" fom namespace test.

--- bindings/python/tests/test_commodity.py
+++ bindings/python/tests/test_commodity.py
@@ -21,12 +21,12 @@ class TestCommodityNamespace(CommoditySession):
         #print(self.table.__class__)
         namespace_names = self.table.get_namespaces()
         #print(namespace_names)
-        self.assertEqual(namespace_names, ['All non-currency', 'template', 'CURRENCY'])
+        self.assertEqual(namespace_names, ['template', 'CURRENCY'])
 
     def test_namespaces_list(self):
         namespaces = self.table.get_namespaces_list()
         namespace_names = [ns.get_name() for ns in namespaces]
-        self.assertEqual(namespace_names, ['All non-currency', 'template', 'CURRENCY'])
+        self.assertEqual(namespace_names, ['template', 'CURRENCY'])
 
 if __name__ == '__main__':
     main()
