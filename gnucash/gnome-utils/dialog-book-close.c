c256818eb6a6ef412b73f91a1091a8a19e936b41
Christopher Lam <christopher.lck@gmail.com>
2022-04-18 22:41:10 +0800

    [dialog-book-close] Close Book: hide Placeholder accts

--- gnucash/gnome-utils/dialog-book-close.c
+++ gnucash/gnome-utils/dialog-book-close.c
@@ -352,6 +352,7 @@ void gnc_ui_close_book (QofBook* book, GtkWindow *parent)
     box = GTK_WIDGET(gtk_builder_get_object (builder, "income_acct_box"));
     cbw->income_acct_widget = gnc_account_sel_new();
     gnc_account_sel_set_hexpand (GNC_ACCOUNT_SEL(cbw->income_acct_widget), TRUE);
+    gnc_account_sel_filter_placeholder (GNC_ACCOUNT_SEL(cbw->income_acct_widget), TRUE);
     gnc_account_sel_set_acct_filters(GNC_ACCOUNT_SEL(cbw->income_acct_widget),
                                      equity_list, NULL);
     gnc_account_sel_set_new_account_ability(GNC_ACCOUNT_SEL(cbw->income_acct_widget), TRUE);
@@ -361,6 +362,7 @@ void gnc_ui_close_book (QofBook* book, GtkWindow *parent)
     box = GTK_WIDGET(gtk_builder_get_object (builder, "expense_acct_box"));
     cbw->expense_acct_widget = gnc_account_sel_new();
     gnc_account_sel_set_hexpand (GNC_ACCOUNT_SEL(cbw->expense_acct_widget), TRUE);
+    gnc_account_sel_filter_placeholder (GNC_ACCOUNT_SEL(cbw->expense_acct_widget), TRUE);
     gnc_account_sel_set_acct_filters(GNC_ACCOUNT_SEL(cbw->expense_acct_widget),
                                      equity_list, NULL);
     gnc_account_sel_set_new_account_ability(GNC_ACCOUNT_SEL(cbw->expense_acct_widget), TRUE);
