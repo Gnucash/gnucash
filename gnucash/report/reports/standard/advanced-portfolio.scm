327b0af71cffcbde6774a6d9da7cdbb85e624a25
Christopher Lam <christopher.lck@gmail.com>
2022-08-08 15:17:59 +0800

    reports should not gnc-price-ref/unref prices
    IMHO reports must access prices passively, therefore
    gnc-price-ref/unref shouldn't be allowed.

--- gnucash/report/reports/standard/advanced-portfolio.scm
+++ gnucash/report/reports/standard/advanced-portfolio.scm
@@ -356,7 +356,6 @@ by preventing negative stock balances.<br/>")
             (if (gnc-commodity-equiv currency (gnc-price-get-commodity p))
                 (set! price (gnc-price-invert p))))
           price-list)
-        (gnc-price-ref price)
         (gnc-price-list-destroy price-list)
         price)))
 
@@ -495,7 +494,6 @@ by preventing negative stock balances.<br/>")
                                 (if (not (gnc-numeric-zero-p (gnc:gnc-monetary-amount trans-price)))
                                   ;; We can exchange the price from this transaction into the report currency
                                   (begin
-                                    (if price (gnc-price-unref price))
                                     (set! pricing-txn trans)
                                     (set! price trans-price)
                                     (gnc:debug "Transaction price is " (gnc:monetary->string price))
@@ -954,11 +952,9 @@ by preventing negative stock balances.<br/>")
 	       row-style
 	       activecols)
 
-              (if (and (not use-txn) price) (gnc-price-unref price))
 	      (table-add-stock-rows-internal rest (not odd-row?))
 	      )
 	    (begin
-	      (if (and (not use-txn) price) (gnc-price-unref price))
 	      (table-add-stock-rows-internal rest odd-row?)
 	      )
             )
