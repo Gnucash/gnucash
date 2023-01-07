8a755239284f3efbcea182c4983076ab6c06d6a6
Frank H. Ellenberger <frank.h.ellenberger@gmail.com>
2023-01-07 01:48:01 +0100

    Translate "Accounts" in "Filter %s by..."

--- gnucash/gnome-utils/gnc-tree-view-account.c
+++ gnucash/gnome-utils/gnc-tree-view-account.c
@@ -2328,7 +2328,7 @@ account_filter_dialog_create(AccountFilterDialog *fd, GncPluginPage *page)
                                  GTK_WINDOW(GNC_PLUGIN_PAGE(page)->window));
     /* Translators: The %s is the name of the plugin page */
     title = g_strdup_printf(_("Filter %s by..."),
-                            gnc_plugin_page_get_page_name(GNC_PLUGIN_PAGE(page)));
+                            _(gnc_plugin_page_get_page_name(GNC_PLUGIN_PAGE(page))));
     gtk_window_set_title(GTK_WINDOW(dialog), title);
     g_free(title);
 
