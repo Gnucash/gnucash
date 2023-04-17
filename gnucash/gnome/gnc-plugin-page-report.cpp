b1cf200ad0bf14b5ec8387ac634f1a5d0f5ae3e5
Robert Fewell <14uBobIT@gmail.com>
2023-04-17 14:31:19 +0100

    Bug 798872 - Reload inoperative for reports
    Missed a adding the refresh action to the view menu when a report is in
    focus.

--- gnucash/gnome/gnc-plugin-page-report.cpp
+++ gnucash/gnome/gnc-plugin-page-report.cpp
@@ -212,7 +212,10 @@ static const gchar *gnc_plugin_load_ui_items [] =
     "FilePlaceholder3",
     "FilePlaceholder4",
     "FilePlaceholder5",
+    "EditPlaceholder3",
+    "EditPlaceholder5",
     "EditPlaceholder6",
+    "ViewPlaceholder4",
     "ReportsPlaceholder1",
     NULL,
 };
