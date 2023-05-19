a6dcdb6a68c475162d35bc6ecda8007c7cd97bee
Richard Cohen <richard@daijobu.co.uk>
2023-05-19 16:24:35 +0100

    Refactor: DECLARE_FINAL GNCSearchAccount < GNCSearchCoreType

--- gnucash/gnome-search/search-account.h
+++ gnucash/gnome-search/search-account.h
@@ -26,12 +26,7 @@
 #include "qof.h"
 
 #define GNC_TYPE_SEARCH_ACCOUNT 	(gnc_search_account_get_type ())
-#define GNCSEARCH_ACCOUNT(obj)		G_TYPE_CHECK_INSTANCE_CAST (obj, GNC_TYPE_SEARCH_ACCOUNT, GNCSearchAccount)
-#define GNCSEARCH_ACCOUNT_CLASS(klass)	G_TYPE_CHECK_CLASS_CAST (klass, GNC_TYPE_SEARCH_ACCOUNT, GNCSearchAccountClass)
-#define IS_GNCSEARCH_ACCOUNT(obj)	G_TYPE_CHECK_INSTANCE_TYPE (obj, GNC_TYPE_SEARCH_ACCOUNT)
-
-typedef struct _GNCSearchAccount	GNCSearchAccount;
-typedef struct _GNCSearchAccountClass	GNCSearchAccountClass;
+G_DECLARE_FINAL_TYPE (GNCSearchAccount, gnc_search_account, GNC, SEARCH_ACCOUNT, GNCSearchCoreType)
 
 struct _GNCSearchAccount
 {
@@ -40,16 +35,6 @@ struct _GNCSearchAccount
     QofGuidMatch	how;
 };
 
-struct _GNCSearchAccountClass
-{
-    GNCSearchCoreTypeClass parent_class;
-
-    /* virtual methods */
-
-    /* signals */
-};
-
-GType		gnc_search_account_get_type	(void);
 GNCSearchAccount	*gnc_search_account_new	(void);
 GNCSearchAccount	*gnc_search_account_matchall_new	(void);
 
