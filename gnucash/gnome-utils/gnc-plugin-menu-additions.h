b715211b24ac1b5edeb3eec77a232877fd2742d3
Richard Cohen <richard@daijobu.co.uk>
2023-06-12 19:37:23 +0100

    Refactor: DECLARE_FINAL GncPluginMenuAdditions < GncPlugin

--- gnucash/gnome-utils/gnc-plugin-menu-additions.h
+++ gnucash/gnome-utils/gnc-plugin-menu-additions.h
@@ -44,34 +44,17 @@ G_BEGIN_DECLS
 
 /* type macros */
 #define GNC_TYPE_PLUGIN_MENU_ADDITIONS            (gnc_plugin_menu_additions_get_type ())
-#define GNC_PLUGIN_MENU_ADDITIONS(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), GNC_TYPE_PLUGIN_MENU_ADDITIONS, GncPluginMenuAdditions))
-#define GNC_PLUGIN_MENU_ADDITIONS_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), GNC_TYPE_PLUGIN_MENU_ADDITIONS, GncPluginMenuAdditionsClass))
-#define GNC_IS_PLUGIN_MENU_ADDITIONS(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GNC_TYPE_PLUGIN_MENU_ADDITIONS))
-#define GNC_IS_PLUGIN_MENU_ADDITIONS_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GNC_TYPE_PLUGIN_MENU_ADDITIONS))
-#define GNC_PLUGIN_MENU_ADDITIONS_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GNC_TYPE_PLUGIN_MENU_ADDITIONS, GncPluginMenuAdditionsClass))
+G_DECLARE_FINAL_TYPE (GncPluginMenuAdditions, gnc_plugin_menu_additions, GNC, PLUGIN_MENU_ADDITIONS, GncPlugin)
 
 #define GNC_PLUGIN_MENU_ADDITIONS_NAME "gnc-plugin-menu-additions"
 
 /* typedefs & structures */
-typedef struct
+struct _GncPluginMenuAdditions
 {
     GncPlugin gnc_plugin;
-} GncPluginMenuAdditions;
-
-typedef struct
-{
-    GncPluginClass gnc_plugin;
-} GncPluginMenuAdditionsClass;
+};
 
 /* function prototypes */
-
-
-/** Get the type of an extensions plugin.
- *
- *  @return A GType.
- */
-GType gnc_plugin_menu_additions_get_type (void);
-
 
 /** Create a new menu_additions plugin.  This plugin attaches the menu
  *  items from Scheme code to any window that is opened.
