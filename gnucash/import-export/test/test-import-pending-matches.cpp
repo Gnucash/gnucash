907667df01adb538d7e8e2a07438418e33168759
BLR <blrnh94@gmail.com>
2024-02-13 20:03:21 -0500

    Adding missing file headers in import source files.

--- gnucash/import-export/test/test-import-pending-matches.cpp
+++ gnucash/import-export/test/test-import-pending-matches.cpp
@@ -1,3 +1,27 @@
+/********************************************************************\
+ * test-import-pending-matches.cpp - Tests for                      *
+ *                                   import-pending-matches         *
+ *                                                                  *
+ * Copyright (c) 2017 Jesse Olmer                                   *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
+\********************************************************************/
+
 #include <glib.h>
 #include <gtk/gtk.h> /* for references in import-backend.h */
 
