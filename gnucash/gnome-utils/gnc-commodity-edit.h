9456475095e14cce4b9e994c7cf670970b9137c6
Robert Fewell <14uBobIT@gmail.com>
2025-04-16 10:52:02 +0100

    Fix up the gnc-commodity-edit source files

--- gnucash/gnome-utils/gnc-commodity-edit.h
+++ gnucash/gnome-utils/gnc-commodity-edit.h
@@ -1,30 +1,26 @@
-/*
- * gnc-commodity-edit.h -- Commodity editing widget
- *
- * Copyright (C) 2000 Free Software Foundation
- * All rights reserved.
- *
- * Dave Peticolas <dave@krondo.com>
- * Derek Atkins <warlord@MIT.EDU>
- *
- * GnuCash is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Library General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * Gnucash is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, contact:
- *
- * Free Software Foundation           Voice:  +1-617-542-5942
- * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652
- * Boston, MA  02110-1301,  USA       gnu@gnu.org
- *
- */
+/********************************************************************
+ * gnc-commodity-edit.h -- Commodity editing widget                 *
+ *                                                                  *
+ * Copyright (C) Dave Peticolas <dave@krondo.com>                   *
+ * Copyright (C) Derek Atkins <warlord@MIT.EDU>                     *
+ *                                                                  *
+ * This program is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as   *
+ * published by the Free Software Foundation; either version 2 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU General Public License for more details.                     *
+ *                                                                  *
+ * You should have received a copy of the GNU General Public License*
+ * along with this program; if not, contact:                        *
+ *                                                                  *
+ * Free Software Foundation           Voice:  +1-617-542-5942       *
+ * 51 Franklin Street, Fifth Floor    Fax:    +1-617-542-2652       *
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org                   *
+ *******************************************************************/
 /*
   @NOTATION@
  */
