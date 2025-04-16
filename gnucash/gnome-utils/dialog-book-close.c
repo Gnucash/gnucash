eb2f3fa2ddf07cf4d6983cd80b63981bcfdcd7ee
Robert Fewell <14uBobIT@gmail.com>
2025-04-16 10:55:47 +0100

    Start of book close changes

--- gnucash/gnome-utils/dialog-book-close.c
+++ gnucash/gnome-utils/dialog-book-close.c
@@ -45,8 +45,6 @@
 
 #define DIALOG_BOOK_CLOSE_CM_CLASS "dialog-book-close"
 
-void gnc_book_close_response_cb(GtkDialog *, gint, GtkDialog *);
-
 /* This static indicates the debugging module that this .o belongs to.  */
 static QofLogModule log_module = GNC_MOD_GUI;
 
@@ -56,7 +54,7 @@ struct CloseBookWindow
     QofBook* book;
 
     /* Parts of the dialog */
-    GtkWidget* dialog;
+    GtkWidget* window;
     GtkWidget* close_date_widget;
     GtkWidget* income_acct_widget;
     GtkWidget* expense_acct_widget;
@@ -241,78 +239,92 @@ static void close_accounts_of_type(struct CloseBookWindow* cbw,
     g_hash_table_destroy(cacb.txns);
 }
 
-static void close_handler(gpointer data)
+static void
+close_handler (gpointer user_data)
 {
-    GtkWidget *dialog = data;
+    struct CloseBookWindow *cbw = user_data;
 
-//FIXME gtk4    gtk_window_destroy (GTK_WINDOW(dialog));
+    gtk_window_destroy (GTK_WINDOW(cbw->window));
 }
 
-static void destroy_cb(GObject *object, gpointer data)
+static void
+destroy_cb (GObject *object, gpointer user_data)
 {
-    struct CloseBookWindow *cbw;
-
-    cbw = g_object_get_data(G_OBJECT(object), "CloseBookWindow");
+    struct CloseBookWindow *cbw = user_data;
 
     if (cbw->component_manager_id)
     {
         gnc_unregister_gui_component(cbw->component_manager_id);
         cbw->component_manager_id = 0;
     }
+    g_free (cbw);
 }
 
-
-void
-gnc_book_close_response_cb(GtkDialog *dialog, gint response, GtkDialog *unused)
+static void
+help_button_cb (GtkWidget *widget, gpointer user_data)
 {
-    struct CloseBookWindow* cbw;
+    struct CloseBookWindow *cbw = user_data;
+    gnc_gnome_help (GTK_WINDOW(cbw->window), DF_MANUAL, DL_CLOSE_BOOK);
+}
+
+static void
+cancel_button_cb (GtkWidget *widget, gpointer user_data)
+{
+    struct CloseBookWindow *cbw = user_data;
+    gtk_window_destroy (GTK_WINDOW(cbw->window));
+}
+
+static void
+ok_button_cb (GtkWidget *widget, gpointer user_data)
+{
+    struct CloseBookWindow *cbw = user_data;
     Account* income_acct;
     Account* expense_acct;
 
-    ENTER("dialog %p, response %d, unused %p", dialog, response, unused);
+    cbw->close_date = gnc_date_edit_get_date (GNC_DATE_EDIT(cbw->close_date_widget));
+    cbw->desc = gnc_entry_get_text (GTK_ENTRY(cbw->desc_widget));
 
-    g_return_if_fail(dialog);
+    income_acct = gnc_account_sel_get_account (GNC_ACCOUNT_SEL(cbw->income_acct_widget));
+    expense_acct = gnc_account_sel_get_account (GNC_ACCOUNT_SEL(cbw->expense_acct_widget));
 
-    cbw = g_object_get_data(G_OBJECT(dialog), "CloseBookWindow");
-    g_return_if_fail(cbw);
-
-    switch (response)
+    if (!income_acct)
     {
-    case GTK_RESPONSE_HELP:
-        gnc_gnome_help (GTK_WINDOW(dialog), DF_MANUAL, DL_CLOSE_BOOK);
-        break;
-    case GTK_RESPONSE_OK:
-        cbw->close_date = gnc_date_edit_get_date(GNC_DATE_EDIT(cbw->close_date_widget));
-        cbw->desc = gnc_entry_get_text(GTK_ENTRY(cbw->desc_widget));
-
-        income_acct = gnc_account_sel_get_account(GNC_ACCOUNT_SEL(cbw->income_acct_widget));
-        expense_acct = gnc_account_sel_get_account(GNC_ACCOUNT_SEL(cbw->expense_acct_widget));
-
-        if (!income_acct)
-        {
-            gnc_error_dialog(GTK_WINDOW (cbw->dialog), "%s",
-                             _("Please select an Equity account to hold the total Period Income."));
-            break;
-        }
-
-        if (!expense_acct)
-        {
-            gnc_error_dialog(GTK_WINDOW (cbw->dialog), "%s",
-                             _("Please select an Equity account to hold the total Period Expense."));
-            break;
-        }
-
-        gnc_suspend_gui_refresh();
-        close_accounts_of_type(cbw, income_acct, ACCT_TYPE_INCOME);
-        close_accounts_of_type(cbw, expense_acct, ACCT_TYPE_EXPENSE);
-        gnc_resume_gui_refresh();
-
-        /* FALL THROUGH */
-    default:
-//FIXME gtk4        gtk_window_destroy (GTK_WINDOW(dialog));
-        break;
+        gnc_error_dialog (GTK_WINDOW (cbw->window), "%s",
+                          _("Please select an Equity account to hold the total Period Income."));
+        return;
     }
-    LEAVE("");
+
+    if (!expense_acct)
+    {
+        gnc_error_dialog (GTK_WINDOW (cbw->window), "%s",
+                          _("Please select an Equity account to hold the total Period Expense."));
+        return;
+    }
+
+    gnc_suspend_gui_refresh();
+    close_accounts_of_type (cbw, income_acct, ACCT_TYPE_INCOME);
+    close_accounts_of_type (cbw, expense_acct, ACCT_TYPE_EXPENSE);
+    gnc_resume_gui_refresh();
+
+    gtk_window_destroy (GTK_WINDOW(cbw->window));
+}
+
+static gboolean
+dialog_key_press_cb (GtkEventControllerKey *key, guint keyval,
+                     guint keycode, GdkModifierType state,
+                     gpointer user_data)
+{
+    struct CloseBookWindow *cbw = user_data;
+
+    if (keyval == GDK_KEY_Escape)
+    {
+        if (gnc_ok_to_close_window (GTK_WIDGET(cbw->window)))
+            close_handler (cbw);
+
+        return TRUE;
+    }
+    else
+        return FALSE;
 }
 
 void gnc_ui_close_book (QofBook* book, GtkWindow *parent)
@@ -321,27 +333,27 @@ void gnc_ui_close_book (QofBook* book, GtkWindow *parent)
     GtkBuilder* builder;
     GtkWidget* box;
     GList* equity_list = NULL;
+    GtkWidget *button;
 
     g_return_if_fail(book);
 
-    cbw = g_new0(struct CloseBookWindow, 1);
+    cbw = g_new0 (struct CloseBookWindow, 1);
     g_return_if_fail(cbw);
     cbw->book = book;
 
     /* Open the dialog */
     builder = gtk_builder_new();
-    gtk_builder_set_current_object (builder, G_OBJECT(cbw->dialog));
-    gnc_builder_add_from_file (builder, "dialog-book-close.glade", "close_book_dialog");
-    cbw->dialog = GTK_WIDGET(gtk_builder_get_object (builder,  "close_book_dialog"));
+    gnc_builder_add_from_file (builder, "dialog-book-close.ui", "close_book_window");
+    cbw->window = GTK_WIDGET(gtk_builder_get_object (builder,  "close_book_window"));
 
     // Set the name for this dialog so it can be easily manipulated with css
-    gtk_widget_set_name (GTK_WIDGET(cbw->dialog), "gnc-id-book-close");
+    gtk_widget_set_name (GTK_WIDGET(cbw->window), "gnc-id-book-close");
 
     /* parent */
     if (parent != NULL)
-        gtk_window_set_transient_for (GTK_WINDOW(cbw->dialog), GTK_WINDOW(parent));
+        gtk_window_set_transient_for (GTK_WINDOW(cbw->window), GTK_WINDOW(parent));
 
-    PINFO("Closed Book Window is %p, Dialog is %p", cbw, cbw->dialog);
+    PINFO("Closed Book Window is %p, Dialog is %p", cbw, cbw->window);
 
     /* close date */
     box = GTK_WIDGET(gtk_builder_get_object (builder,  "date_box"));
@@ -368,25 +380,41 @@ void gnc_ui_close_book (QofBook* book, GtkWindow *parent)
     /* desc */
     cbw->desc_widget = GTK_WIDGET(gtk_builder_get_object (builder, "desc_entry"));
 
-    /* Autoconnect signals */
-//FIXME gtk4    gtk_builder_connect_signals_full (builder, gnc_builder_connect_full_func, cbw->dialog);
+    button = GTK_WIDGET(gtk_builder_get_object (builder, "helpbutton"));
+    g_signal_connect (G_OBJECT(button), "clicked",
+                      G_CALLBACK(help_button_cb), cbw);
+    button = GTK_WIDGET(gtk_builder_get_object (builder, "cancelbutton"));
+    g_signal_connect (G_OBJECT(button), "clicked",
+                      G_CALLBACK(cancel_button_cb), cbw);
+
+    /* default to 'cancel' button */
+    gtk_window_set_default_widget (GTK_WINDOW(cbw->window),
+                                   GTK_WIDGET(button)); //FIXME gtk4, may not work
+
+    button = GTK_WIDGET(gtk_builder_get_object (builder, "okbutton"));
+    g_signal_connect (G_OBJECT(button), "clicked",
+                      G_CALLBACK(ok_button_cb), cbw);
+
+    GtkEventController *event_controller_window = gtk_event_controller_key_new ();
+    gtk_widget_add_controller (GTK_WIDGET(cbw->window), event_controller_window);
+    g_signal_connect (G_OBJECT(event_controller_window),
+                      "key-pressed",
+                      G_CALLBACK(dialog_key_press_cb), cbw);
 
     /* Register dialog with component manager */
     cbw->component_manager_id =
         gnc_register_gui_component(DIALOG_BOOK_CLOSE_CM_CLASS, NULL, close_handler,
-                                   cbw->dialog);
+                                   cbw->window);
     gnc_gui_component_set_session(cbw->component_manager_id,
                                   gnc_get_current_session());
-    g_signal_connect(cbw->dialog, "destroy", G_CALLBACK(destroy_cb), NULL);
 
-    /* Clean up the data structure when the dialog is destroyed */
-    g_object_set_data_full(G_OBJECT(cbw->dialog), "CloseBookWindow", cbw, g_free);
+    g_signal_connect (G_OBJECT(cbw->window), "destroy",
+                      G_CALLBACK(destroy_cb), cbw);
 
-    g_object_unref(G_OBJECT(builder));
+    g_object_unref (G_OBJECT(builder));
 
-    /* Run the dialog */
-//FIXME gtk4    gtk_widget_show_all(cbw->dialog);
+    gtk_widget_set_visible (GTK_WIDGET(cbw->window), TRUE);
 
-    g_list_free(equity_list);
+    g_list_free (equity_list);
 }
 
