75dbadd9abcb84088c34decd9001d778d7f3a841
John Ralls <jralls@ceridwen.us>
2026-01-31 16:36:45 -0800

    Work around Github runner adding Windows line endings to test files.

--- libgnucash/backend/xml/test/gtest-load-save-files.cpp
+++ libgnucash/backend/xml/test/gtest-load-save-files.cpp
@@ -32,6 +32,8 @@
 
 #include <cstdlib>
 #include <functional>
+#include <iostream>
+#include <iomanip>
 #include <memory>
 #include <string>
 #include <vector>
@@ -65,7 +67,6 @@ static std::vector<unsigned char> read_file (std::string filename)
 
         memcpy (data.data (), contents, length);
         g_free (contents);
-        std::cerr << filename << " contents length " << length << " vector size " << data.size() << std::endl;
         return data;
     }
     else
@@ -74,14 +75,30 @@ static std::vector<unsigned char> read_file (std::string filename)
     }
 }
 
+#ifdef _WIN32
+static inline void
+fix_line_endings(std::vector<uint8_t>& contents)
+{
+    // On some systems (github actions runners in particular)
+    // something is changing some line endings to Windows,
+    // i.e. 0xODOA. Change them back to 0x0A.
+    auto new_end{std::remove(contents.begin(), contents.end(), 0x0D)};
+    contents.erase(new_end, contents.end());
+}
+#endif
+
 static bool
 compare_files (std::string filename1, std::string filename2)
 {
     auto contents1 = read_file (filename1);
     auto contents2 = read_file (filename2);
 
+#ifdef _WIN32
+    fix_line_endings(contents1);
+#endif
+
     if (contents1.size () > 0 && contents1.size () == contents2.size ()
-            && !memcmp(contents1.data (), contents2.data (), contents1.size ())) {
+        && !memcmp(contents1.data (), contents2.data (), contents1.size ())) {
         return true;
     } else {
         ADD_FAILURE() << "compare_files: " << filename1 << " and " << filename2 << " are different";
@@ -116,7 +133,7 @@ decompress_file (std::string filename, const std::vector<unsigned char> &in, std
         return false;
     }
 
-    ret = inflate (&stream, Z_NO_FLUSH);
+    ret = inflate (&stream, Z_FINISH);
     if (ret != Z_STREAM_END)
     {
         ADD_FAILURE() << "decompress_file: " << filename << " could not be uncompressed (inflate "
@@ -139,7 +156,7 @@ decompress_file (std::string filename, const std::vector<unsigned char> &in, std
         return false;
     }
 
-    out.resize (stream.avail_out);
+    out.resize (stream.total_out);
 
     return true;
 }
@@ -149,12 +166,14 @@ compare_compressed_files (std::string uncompressed_filename1, std::string compre
 {
     auto uncompressed_contents1 = read_file (uncompressed_filename1);
     auto compressed_contents2 = read_file (compressed_filename2);
+#ifdef _WIN32
+    fix_line_endings(uncompressed_contents1);
+#endif
     /* Allow some space to grow beyond the expected size */
     std::vector<unsigned char> uncompressed_contents2(uncompressed_contents1.size () * 2);
 
     if (!decompress_file (compressed_filename2, compressed_contents2, uncompressed_contents2))
         return false;
-
     if (uncompressed_contents1.size () > 0
         && uncompressed_contents1.size () != uncompressed_contents2.size ())
     {
@@ -164,11 +183,6 @@ compare_compressed_files (std::string uncompressed_filename1, std::string compre
             << uncompressed_contents2.size () << ")";
         return false;
     }
-    else
-    {
-        std::cout << uncompressed_filename1 << " sizes match: " << uncompressed_contents1.size() << std::endl;
-    }
-
 
     if (!memcmp(uncompressed_contents1.data (), uncompressed_contents2.data (), uncompressed_contents1.size ())) {
         return true;
