f819d19fa21d5a44d0da077e1d5a476748eee15a
Quazgar <quazgar@posteo.de>
2026-04-13 20:17:47 +0200

    DOCS[py]: Extended Python examples.

--- bindings/python/example_scripts/simple_book.py
+++ bindings/python/example_scripts/simple_book.py
@@ -9,17 +9,19 @@ from gnucash import Session, SessionOpenMode
 
 # We need to tell GnuCash the data format to create the new file as (xml://)
 uri = "xml:///tmp/simple_book.gnucash"
+# Alternatively, use "sqlite3://..." instead to save as sqlite file.
 
 print("uri:", uri)
 with Session(uri, SessionOpenMode.SESSION_NEW_STORE) as ses:
     book = ses.get_book()
 
-    #Call some methods that produce output to show that Book works
+    # Call some methods that produce output to show that Book works and to mark it as "something
+    # needs to be saved".
     book.get_root_account().SetDescription("hello, book")
     print("Book is saved:", not book.session_not_saved())
 
-    #As long as there's no exceptions, book is automatically saved
-    #when session ends.
+    # As long as there's no exception, book is automatically saved
+    # when session ends.
     print("saving...")
 
 print("Book is saved:", not book.session_not_saved())
--- bindings/python/example_scripts/simple_sqlite_create.py
+++ bindings/python/example_scripts/simple_sqlite_create.py
@@ -3,12 +3,13 @@
 #   @brief Example Script simple sqlite create 
 #   @ingroup python_bindings_examples
 
+import os
 from gnucash import Session, Account, SessionOpenMode
 from os.path import abspath
 from gnucash.gnucash_core_c import ACCT_TYPE_ASSET
 
 s = Session('sqlite3://%s' % abspath('test.blob'), SessionOpenMode.SESSION_NEW_STORE)
-# this seems to make a difference in more complex cases
+# This seems to make a difference in more complex cases
 s.save()
 
 book = s.book
@@ -23,3 +24,21 @@ a.SetCommodity( commod_table.lookup('CURRENCY', 'CAD') )
 s.save()
 
 s.end()
+
+########################################
+# Similar, but create empty file first.
+
+targetpath = os.path.abspath("./test.gnucash")
+targetfile = f"sqlite3://{targetpath}"
+
+# Remove if it existst.
+if os.path.exists(targetpath):
+    os.remove(targetpath)
+
+with Session(targetfile, SessionOpenMode.SESSION_NEW_STORE) as session:
+    session.book.get_root_account()  # Seems necessary to trigger creation of tables.
+print("test.gnucash created")
+
+with Session(targetfile, SessionOpenMode.SESSION_NORMAL_OPEN) as session:
+    print("Opened again")
+print("Closed again.")
